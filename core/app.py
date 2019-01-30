34e891a76294013c52f6e9a8bcf721616467bff0
mysitioc <mysitioc@mysitio.cl>
2019-01-30 12:37:37 -0600

    --- Auto-Git Commit ---

--- core/app.py
+++ core/app.py
@@ -9,13 +9,12 @@ def init(environ):
     data_return={}
     data_return['status']="200 OK"
     data_return['content_type']='text/html'
-    #data_return['url'] = parse_qs(environ['QUERY_STRING'])
     data_return['url'] = parse_qs(environ['QUERY_STRING'])
 
     for i in range(10):
         view.add('hola'+str(i),'hello world')
-        
-    view.add('url',str(data_return['url']))
+
+    view.add('url_data',str(parse_qs(environ['QUERY_STRING'])))
     
     data_return['response_body']=view.render()
     

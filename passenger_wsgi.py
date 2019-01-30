6f4bcfeca7b2c169dead3e9decb4685940fd708c
mysitioc <mysitioc@mysitio.cl>
2019-01-30 13:31:38 -0600

    --- Auto-Git Commit ---

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -16,7 +16,7 @@ def application(environ, start_response):
         ]
 
         start_response(main_data['status'], response_headers)
-        return [main_data['response_body']]
+        return [str(main_data['response_body'])]
     except:                                   # Error output starts here
         exc_type, exc_obj, tb = sys.exc_info()
         f = tb.tb_frame

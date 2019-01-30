ee1874e5a4df075a6e9c466bff4648418096d80b
mysitioc <mysitioc@mysitio.cl>
2019-01-30 14:38:37 -0600

    --- Auto-Git Commit ---

--- core/view.py
+++ core/view.py
@@ -10,18 +10,18 @@ class view:
     data = {}
 
     @staticmethod
-    def add(key,value):
-        view.data[key]=value
+    def add(key, value):
+        view.data[key] = value
 
     @staticmethod
     def render():
-        data_return=[]
+        data_return = []
         for key, value in view.data.items():
-            a='<div>'+key+':'+value+'</div>'
+            a = '<div>'+key+':'+value+'</div>'
             data_return.append(a)
-        body='<br/>'.join(data_return)
-        
-        body=view.html % { # Fill the above html template in
+        body = '<br/>'.join(data_return)
+
+        body = view.html % {  # Fill the above html template in
             'body': body
         }
 

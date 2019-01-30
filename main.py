6800eef7d00162e559c6ab3612c70bd237034e48
mysitioc <mysitioc@mysitio.cl>
2019-01-30 11:28:56 -0600

    --- Auto-Git Commit ---

--- main.py
+++ main.py
@@ -1,24 +1,9 @@
 #import cgitb
 #cgitb.enable()
+import view
 def init():
     view.add('hola','hello world')
     view.add('hola2','hello world')
     view.add('hola3','hello world')
     view.add('hola4','hello world')
     return view.render()
-
-class view:
-    data = {}
-    @staticmethod
-    def add(key,value):
-        view.data[key]=value
-    @staticmethod
-    def render():
-        data_return=[]
-        for key, value in view.data.items():
-            a='<div>'+key+':'+value+'</div>'
-            data_return.append(a)
-        else:
-            pass
-        body='<br/>'.join(data_return)
-        return body
---
+++ view.py
@@ -0,0 +1,16 @@
+
+class view:
+    data = {}
+    @staticmethod
+    def add(key,value):
+        view.data[key]=value
+    @staticmethod
+    def render():
+        data_return=[]
+        for key, value in view.data.items():
+            a='<div>'+key+':'+value+'</div>'
+            data_return.append(a)
+        else:
+            pass
+        body='<br/>'.join(data_return)
+        return body

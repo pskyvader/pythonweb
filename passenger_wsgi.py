be3905837af524e6b281606a54297ed8f3ce76b7
mysitioc <mysitioc@mysitio.cl>
2019-01-30 12:05:21 -0600

    --- Auto-Git Commit ---

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -11,13 +11,12 @@ sys.path.insert(0, os.getcwd())
 def application(environ, start_response):
     try:
         main_data=app.init(environ)
-        status = main_data.status
         response_headers = [
             ('Content-Type', main_data.content_type),
             ('Content-Length', str(len(main_data.response_body)))
         ]
 
-        start_response(status, response_headers)
+        start_response(main_data.status, response_headers)
         return [main_data.response_body]
     except:                                   # Error output starts here
         exc_type, exc_obj, tb = sys.exc_info()

a922c1e3fc78d93cc86610f5f4fcd1f9ef10e934
mysitioc <mysitioc@mysitio.cl>
2019-01-30 14:38:52 -0600

    --- Auto-Git Commit ---

--- core/app.py
+++ core/app.py
@@ -1,39 +1,43 @@
 #import cgitb
-#cgitb.enable()
+# cgitb.enable()
 import sys
 import os
 from core.view import view
 from cgi import parse_qs
 
+
 def init(environ):
-    data_return={}
-    data_return['status']="200 OK"
-    data_return['content_type']='text/html; charset=utf-8'
+    data_return = {}
+    data_return['status'] = "200 OK"
+    data_return['content_type'] = 'text/html; charset=utf-8'
     data_return['extra'] = parse_extra(parse_qs(environ['QUERY_STRING']))
     data_return['url'] = parse_url(environ['PATH_INFO'])
 
     for i in range(5):
-        view.add('hola'+str(i),'hello world ááá bbbaa')
+        view.add('hola'+str(i), 'hello world ááá bbbaa')
 
-    view.add('url_data',str(data_return['url']))
-    view.add('url_extra',str(data_return['extra']))
-    
-    data_return['response_body']=view.render()
-    
+    view.add('url_data', str(data_return['url']))
+    view.add('url_extra', str(data_return['extra']))
+
+    data_return['response_body'] = view.render()
+
     return data_return
 
+
 def parse_url(url):
-    url=url.lstrip('/')
-    if url !='':
-        url=url.split('/')
+    url = url.lstrip('/')
+    if url != '':
+        url = url.split('/')
         if url[0] == 'manifest.js':
             url[0] = 'manifest'
         else:
             if url[0] == 'sw.js':
                 url[0] = 'sw'
     else:
-        url=['home']
+        url = ['home']
     return url
+
+
 def parse_extra(url):
     if 'url' in url:
         del url['url']
--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -6,16 +6,17 @@ from wsgiref.simple_server import make_server
 
 sys.path.insert(0, os.path.dirname(__file__))
 
+
 def application(environ, start_response):
     try:
-        main_data=app.init(environ)
+        main_data = app.init(environ)
         response_headers = [
             ('Content-Type', main_data['content_type']),
             ('Content-Length', str(len(main_data['response_body'])))
         ]
 
         start_response(main_data['status'], response_headers)
-        ret =main_data['response_body']
+        ret = main_data['response_body']
     except:                                   # Error output starts here
         exc_type, exc_obj, tb = sys.exc_info()
         f = tb.tb_frame
@@ -23,8 +24,10 @@ def application(environ, start_response):
         filename = f.f_code.co_filename
         linecache.checkcache(filename)
         line = linecache.getline(filename, lineno, f.f_globals)
-        es = '''Error in {}, Line {} "{}": {}'''.format(filename, lineno, line.strip(), exc_obj)
-        start_response('500 Internal Server Error', [('Content-type', 'text/html; charset=utf-8'),])
-        ret=es
+        es = '''Error in {}, Line {} "{}": {}'''.format(
+            filename, lineno, line.strip(), exc_obj)
+        start_response('500 Internal Server Error', [
+                       ('Content-type', 'text/html; charset=utf-8'), ])
+        ret = es
 
     return [bytes(ret, 'utf-8')]

ba55bbd9da9024f4336c4a79923ebb097c1e9157
mysitioc <mysitioc@mysitio.cl>
2019-01-30 14:56:07 -0600

    --- Auto-Git Commit ---

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,6 +1,6 @@
 import sys
 import os
-from core import app
+import core
 import linecache
 from wsgiref.simple_server import make_server
 
@@ -9,7 +9,7 @@ sys.path.insert(0, os.path.dirname(__file__))
 
 def application(environ, start_response):
     try:
-        app_web=app.app()
+        app_web=app()
         main_data = app_web.init(environ)
         response_headers = [
             ('Content-Type', main_data['content_type']),

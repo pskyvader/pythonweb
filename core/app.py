e41ad9c3806451f24b43d2563f1b93745e60f8ae
mysitioc <mysitioc@mysitio.cl>
2019-01-30 15:42:38 -0600

    --- Auto-Git Commit ---

--- core/app.py
+++ core/app.py
@@ -50,7 +50,7 @@ class app:
             self.controller_dir += 'back/'+config['theme_back']+'/'
             self.view_dir += 'back/'+config['theme_back']+'/'
 
-        my_file = Path(self.controller_dir+data_return['url'][0]+'.py')
+        my_file = Path('../'+self.controller_dir+data_return['url'][0]+'.py')
         if my_file.is_file():
             view.add('existe', 'si')
             sys.path.insert(0, self.controller_dir)

ddd670029b27f5516c9341ce554e14981ed700b7
mysitioc <mysitioc@mysitio.cl>
2019-01-30 15:11:53 -0600

    --- Auto-Git Commit ---

--- core/app.py
+++ core/app.py
@@ -19,6 +19,7 @@ class app:
         data_return['status'] = "200 OK"
         data_return['content_type'] = 'text/html; charset=utf-8'
         data_return['extra'] = self.parse_extra(parse_qs(environ['QUERY_STRING']))
+        data_return['extra'] = environ
         data_return['url'] = self.parse_url(environ['PATH_INFO'])
         config=self.get_config()
         if(data_return['url'][0]==config['admin']):

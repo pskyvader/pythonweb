ca4b37a590ee4082834cb06605636cfdf22f535f
mysitioc <mysitioc@mysitio.cl>
2019-01-30 12:53:38 -0600

    --- Auto-Git Commit ---

--- core/app.py
+++ core/app.py
@@ -4,19 +4,17 @@ import sys
 import os
 from core.view import view
 from cgi import parse_qs
-from urllib.parse import urlparse
 
 def init(environ):
     data_return={}
     data_return['status']="200 OK"
     data_return['content_type']='text/html'
-    data_return['url'] = (urlparse(environ['QUERY_STRING']))
+    data_return['url'] = parse_url(parse_qs(environ['QUERY_STRING']))
 
     for i in range(10):
         view.add('hola'+str(i),'hello world')
 
     view.add('url_data',str(data_return['url']))
-    view.add('environ',str(environ))
     
     data_return['response_body']=view.render()
     

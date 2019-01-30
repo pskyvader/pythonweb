144271c0f51a1e0deba501392baf3b2768333004
mysitioc <mysitioc@mysitio.cl>
2019-01-30 11:28:04 -0600

    a

--- main.py
+++ main.py
@@ -2,6 +2,9 @@
 #cgitb.enable()
 def init():
     view.add('hola','hello world')
+    view.add('hola2','hello world')
+    view.add('hola3','hello world')
+    view.add('hola4','hello world')
     return view.render()
 
 class view:

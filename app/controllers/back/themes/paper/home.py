d0a7c6b621ae7b79f8921443b0c38d3ba6fa850b
mysitioc <mysitioc@mysitio.cl>
2019-01-30 16:33:10 -0600

    --- Auto-Git Commit ---

--- app/controllers/back/themes/paper/home.py
+++ app/controllers/back/themes/paper/home.py
@@ -3,5 +3,5 @@ from core.view import view
 
 def init():
     for i in range(10):
-        view.add('hola'+str(i), 'hello world ááá bbbaa')
+        view.add('hola-- '+str(i), 'hello world ááá bbbaa')
     return True

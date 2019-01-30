36ef39f2338739e5542eeb727186f73af0b6036c
mysitioc <mysitioc@mysitio.cl>
2019-01-30 15:45:09 -0600

    --- Auto-Git Commit ---

--- core/app.py
+++ core/app.py
@@ -57,8 +57,7 @@ class app:
             module = importlib.import_module(data_return['url'][0]+'.py')
         else:
             view.add('existe', 'no')
-            view.add('ruta', '../'+self.controller_dir +
-                     data_return['url'][0]+'.py')
+            view.add('ruta', '../'+self.controller_dir + data_return['url'][0]+'.py')
 
         for i in range(5):
             view.add('hola'+str(i), 'hello world ááá bbbaa')

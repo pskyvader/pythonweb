fdec80d777bd28788cd2f5f465dfb0f8b6e93231
mysitioc <mysitioc@mysitio.cl>
2019-02-01 12:19:05 -0600

    --- Auto-Git Commit ---

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -3,27 +3,32 @@ import os
 from core import app
 import linecache
 from wsgiref.simple_server import make_server
+import pprint
 
 sys.path.insert(0, os.path.dirname(__file__))
 
+
+
+application = LoggingMiddleware(passenger_wsgi.application)
 
 def application(environ, start_response):
     app_web = app.app(os.path.dirname(__file__))
     main_data = app_web.init(environ)
     start_response(main_data['status'], main_data['headers'])
-    try:
-        ret = main_data['response_body']
-    except:                                   # Error output starts here
-        exc_type, exc_obj, tb = sys.exc_info()
-        f = tb.tb_frame
-        lineno = tb.tb_lineno
-        filename = f.f_code.co_filename
-        linecache.checkcache(filename)
-        line = linecache.getline(filename, lineno, f.f_globals)
-        es = '''Error in {}, Line {} "{}": {}'''.format(
-            filename, lineno, line.strip(), exc_obj)
-        start_response('500 Internal Server Error', [
-                       ('Content-type', 'text/html; charset=utf-8'), ])
-        ret = es
-
     return [bytes(ret, 'utf-8')]
+
+
+class LoggingMiddleware:
+    
+    def __init__(self, application):
+        self.__application = application
+
+    def __call__(self, environ, start_response):
+        errors = environ['wsgi.errors']
+        pprint.pprint(('REQUEST', environ), stream=errors)
+
+        def _start_response(status, headers, *args):
+            pprint.pprint(('RESPONSE', status, headers), stream=errors)
+            return start_response(status, headers, *args)
+
+        return self.__application(environ, _start_response)

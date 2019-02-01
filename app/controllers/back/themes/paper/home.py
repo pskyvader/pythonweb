a949593156dc8d0600555e06ef542231341d7fe1
mysitioc <mysitioc@mysitio.cl>
2019-02-01 11:57:50 -0600

    --- Auto-Git Commit ---

--- app/controllers/back/themes/paper/home.py
+++ app/controllers/back/themes/paper/home.py
@@ -6,21 +6,14 @@ metadata = {'title': 'Home', 'modulo': 'home'}
 
 def init(var):
     if 0 in var:
-        if hasattr(home, var[0]) and callable(getattr(home, var[0])):
+        if hasattr(self, var[0]) and callable(getattr(self, var[0])):
             fun = var[0]
             del var[0]
             ret = fun(var)
         else:
             ret = {
-                'body': '',
-                'status': '404 Not Found',
-                'header': [
-                    ('Content-Type', 'text/html; charset=utf-8'),
-                    ('Content-Length', 0)
-                ]
+                'error': 404,
             }
-
-            ret = []
     else:
         ret = index()
     return ret

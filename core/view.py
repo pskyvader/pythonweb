0f5ad33fd0624cd652dd30a71f092205dceedeab
mysitioc <mysitioc@mysitio.cl>
2019-02-01 13:44:22 -0600

    --- Auto-Git Commit ---

--- core/view.py
+++ core/view.py
@@ -22,7 +22,7 @@ class view:
         view.data = {}
 
     @staticmethod
-    def render(template, minify=True):
+    def render(template, minify=True, return_body=False):
         theme = view.get_theme()
         template_url = theme + template + "." + view.extension
         my_file = Path(template_url)
@@ -44,7 +44,12 @@ class view:
         #    body = mini.html(body)
 
         view.reset()
-        return body
+        if return_body:
+            return body
+        else:
+            return html % {  # Fill the above html template in
+                'content': body
+            }
 
     @staticmethod
     def render_template(content):
@@ -61,49 +66,11 @@ class view:
             res = view.template_if(content, key, d)
             content = res[0]
             if not res[1]:  # no es bloque if
-                content = str_replace('{' + key + '}', d, content)
+                s = Template(content)
+                dic = dict(key=d)
+                content = s.substitute(dic)
+        return content
 
-        data_return = []
-        for key, value in view.data.items():
-            a = '<div>'+key+':'+value+'</div>'
-            data_return.append(a)
-        body = '<br/>'.join(data_return)
-
-        str_content = view.html % {  # Fill the above html template in
-            'content': body
-        }
-        return str_content
-
-
-public static function render_template(array $data, string $content)
- {$data2 = array()
-   foreach ($data as $key=> $d) {
-            if (is_array($d)) {// arrray de elementos foreach en vista
-                               $array_open = "{foreach " . $key . "}"
-                               $array_close = "{/foreach " . $key . "}"
-
-                               $pos_open = strpos($content, $array_open)
-                                $pos_close = strpos($content, $array_close)
-
-                if ($pos_open != = false & & $pos_close != = false) { // existe el codigo foreach en vista?
-                                                                      $subcontent1 = substr($content, $pos_open, ($pos_close - $pos_open));
-                                                                      $subcontent = str_replace($array_open, "", $subcontent1);
-                                                                      $sub = "";
-                    foreach ($d as $k = > $s) { // rellenar recursivamente los elementos dentro del foreach
-                                                $sub . = self:: render_template($s, $subcontent);                                                }
-                    $content = str_replace($subcontent1, $sub, $content);
-                    $content = str_replace($array_close, "", $content);} elseif(error_reporting()) {
-                    throw new \Exception("Array no encontrado {$array_open}, o Tag mal cerrado", 1);}
-
-            } else {// si no es array, se procesa despues para evitar conflictos de nombres repetidos dentro y fuera del bloque foreach en template
-                    $data2[$key] = $d;}
-      }
-      foreach ($data2 as $key = > $d) {$res     = self:: template_if($content, $key, $d);
-                                        $content = $res[0];
-            if (!$res[1]) {// no es bloque if
-                           $content = str_replace('{' . $key . '}', $d, $content);}
-      }
-      return $content;}
 
   @staticmethod
    def set_theme(theme):

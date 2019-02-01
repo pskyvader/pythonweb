14ca0d8174d3f0178d14430f9e76539f10f022c8
mysitioc <mysitioc@mysitio.cl>
2019-02-01 11:20:03 -0600

    --- Auto-Git Commit ---

--- app/controllers/back/themes/paper/home.py
+++ app/controllers/back/themes/paper/home.py
@@ -4,7 +4,17 @@ url = ['home']
 metadata = {'title': 'Home', 'modulo': 'home'}
 
 
-def init():
+def init(var):
+    if 1 in var:
+        exist=hasattr(home, key) and callable(getattr(home, key))
+    else:
+        
     for i in range(10):
         view.add('hola-- '+str(i), 'hello world ááá bbbaa')
     return True
+
+def index():
+    for i in range(10):
+        view.add('hola-- '+str(i), 'hello world ááá bbbaa')
+    ret=view.render();
+    return ret

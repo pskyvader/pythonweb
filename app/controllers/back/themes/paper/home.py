f4ac5646bc8baed33cdce1aa93e50e38ea0a0caf
mysitioc <mysitioc@mysitio.cl>
2019-02-01 14:39:24 -0600

    --- Auto-Git Commit ---

--- app/controllers/back/themes/paper/home.py
+++ app/controllers/back/themes/paper/home.py
@@ -2,30 +2,29 @@ from core.view import view
 import os
 
 
-def init(url):
+def init(var):
     h = home()
-    h.init(url)
+    if len(var) > 0:
+        if hasattr(home, var[0]) and callable(getattr(home, var[0])):
+            return False
+        if var[0] in dir(os):
+            fun = var[0]
+            del var[0]
+            ret = fun(var)
+        else:
+            ret = {
+                'error': 404,
+            }
+    else:
+        ret = home.index()
+    return ret
+
+
 
 
 class home:
     url = ['home']
     metadata = {'title': 'Home', 'modulo': 'home'}
-
-    def init(self, var):
-        if len(var) > 0:
-            if hasattr(self.__class__, var[0]) and callable(getattr(self.__class__, var[0])):
-                return False
-            if var[0] in dir(os):
-                fun = var[0]
-                del var[0]
-                ret = fun(var)
-            else:
-                ret = {
-                    'error': 404,
-                }
-        else:
-            ret = self.index()
-        return ret
 
     def index(self):
         ret = {}

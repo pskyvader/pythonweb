3f54eab3a6c6121c4d7f2a67ad1384568e12397f
mysitioc <mysitioc@mysitio.cl>
2019-02-01 13:30:52 -0600

    --- Auto-Git Commit ---

--- core/view.py
+++ core/view.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from mako.template import Template
 class view:
     extension = 'html'
     content_url = {}
@@ -45,6 +46,45 @@ class view:
 
     @staticmethod
     def render_template(template_url):
+        data2={}
+        for key, d in view.data.items():
+            if isinstance(d,dict): # es dictionary de elementos en vista
+                $array_open  = "{foreach " . $key . "}";
+                $array_close = "{/foreach " . $key . "}";
+
+                return False
+            else:
+                data2[key]=d
+
+
+foreach ($data as $key => $d) {
+            if (is_array($d)) { //arrray de elementos foreach en vista
+                $array_open  = "{foreach " . $key . "}";
+                $array_close = "{/foreach " . $key . "}";
+
+                $pos_open  = strpos($content, $array_open);
+                $pos_close = strpos($content, $array_close);
+
+                if ($pos_open !== false && $pos_close !== false) { //existe el codigo foreach en vista?
+                    $subcontent1 = substr($content, $pos_open, ($pos_close - $pos_open));
+                    $subcontent  = str_replace($array_open, "", $subcontent1);
+                    $sub         = "";
+                    foreach ($d as $k => $s) { //rellenar recursivamente los elementos dentro del foreach
+                        $sub .= self::render_template($s, $subcontent);
+                    }
+                    $content = str_replace($subcontent1, $sub, $content);
+                    $content = str_replace($array_close, "", $content);
+                } elseif (error_reporting()) {
+                    throw new \Exception("Array no encontrado {$array_open}, o Tag mal cerrado", 1);
+                }
+
+            } else { //si no es array, se procesa despues para evitar conflictos de nombres repetidos dentro y fuera del bloque foreach en template
+                $data2[$key] = $d;
+            }
+        }
+
+
+
         data_return = []
         for key, value in view.data.items():
             a = '<div>'+key+':'+value+'</div>'
@@ -55,6 +95,55 @@ class view:
             'content': body
         }
         return str_content
+
+
+
+public static function render_template(array $data, string $content)
+    {
+        $data2 = array();
+        foreach ($data as $key => $d) {
+            if (is_array($d)) { //arrray de elementos foreach en vista
+                $array_open  = "{foreach " . $key . "}";
+                $array_close = "{/foreach " . $key . "}";
+
+                $pos_open  = strpos($content, $array_open);
+                $pos_close = strpos($content, $array_close);
+
+                if ($pos_open !== false && $pos_close !== false) { //existe el codigo foreach en vista?
+                    $subcontent1 = substr($content, $pos_open, ($pos_close - $pos_open));
+                    $subcontent  = str_replace($array_open, "", $subcontent1);
+                    $sub         = "";
+                    foreach ($d as $k => $s) { //rellenar recursivamente los elementos dentro del foreach
+                        $sub .= self::render_template($s, $subcontent);
+                    }
+                    $content = str_replace($subcontent1, $sub, $content);
+                    $content = str_replace($array_close, "", $content);
+                } elseif (error_reporting()) {
+                    throw new \Exception("Array no encontrado {$array_open}, o Tag mal cerrado", 1);
+                }
+
+            } else { //si no es array, se procesa despues para evitar conflictos de nombres repetidos dentro y fuera del bloque foreach en template
+                $data2[$key] = $d;
+            }
+        }
+        foreach ($data2 as $key => $d) {
+            $res     = self::template_if($content, $key, $d);
+            $content = $res[0];
+            if (!$res[1]) { //no es bloque if
+                $content = str_replace('{' . $key . '}', $d, $content);
+            }
+        }
+        return $content;
+    }
+
+
+
+
+
+
+
+
+
 
     @staticmethod
     def set_theme(theme):

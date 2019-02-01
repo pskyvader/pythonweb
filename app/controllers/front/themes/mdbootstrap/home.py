62a0efd891dc20f776a6a93ff9d4e1b3ee785515
mysitioc <mysitioc@mysitio.cl>
2019-02-01 12:24:05 -0600

    --- Auto-Git Commit ---

---
+++ app/controllers/front/themes/mdbootstrap/home.py
@@ -0,0 +1,29 @@
+from core.view import view
+import os
+
+url = ['home']
+metadata = {'title': 'Home', 'modulo': 'home'}
+
+
+def init(var):
+    if 0 in var:
+        if var[0] in dir(os):
+            fun = var[0]
+            del var[0]
+            ret = fun(var)
+        else:
+            ret = {
+                'error': 404,
+            }
+    else:
+        ret = index()
+    return ret
+
+
+def index():
+    ret={}
+    for i in range(10):
+        view.add('hola-- '+str(i), 'hello world ááá bbbaa')
+    ret['body'] = view.render()
+    ret['headers'] = [('Content-Type', 'text/html; charset=utf-8')]
+    return ret

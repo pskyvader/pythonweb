98a97ffeb3e3f0738920d4055d83fcc555dd5386
mysitioc <mysitioc@mysitio.cl>
2019-02-01 12:28:50 -0600

    --- Auto-Git Commit ---

--- core/view.py
+++ core/view.py
@@ -14,7 +14,7 @@ class view:
         view.data[key] = value
 
     @staticmethod
-    def render():
+    def render(template,minify=False,return=False):
         data_return = []
         for key, value in view.data.items():
             a = '<div>'+key+':'+value+'</div>'
@@ -26,3 +26,35 @@ class view:
         }
 
         return body
+
+public static function render($template, $minify = true, $return = false)
+    {
+        $theme        = self::get_theme();
+        $template_url = $theme . $template . "." . self::EXTENSION_TEMPLATES;
+        if (!file_exists($template_url)) {
+            throw new \Exception("Error: El archivo " . $template_url . " no existe", 1);
+        }
+
+        if (isset(self::$content_url[$template_url])) {
+            $content = self::$content_url[$template_url];
+        } else {
+            $content                          = file_get_contents($template_url);
+            self::$content_url[$template_url] = $content;
+        }
+        $str = self::render_template(self::$data, $content);
+        if ($minify && !$return && cache::is_cacheable()) {
+            $str = mini::html($str,array('collapse_whitespace'=>true));
+        }
+
+        self::reset();
+        if ($return) {
+            return $str;
+        } else {
+            /*ob_start();
+            echo $str;
+            $str = ob_get_contents();
+            ob_end_clean();*/
+            echo $str;
+            cache::add_cache($str);
+        }
+    }

ce7463885c6409c26d323f7295e4e26bef6c8a65
mysitioc <mysitioc@mysitio.cl>
2019-02-01 14:37:54 -0600

    --- Auto-Git Commit ---

--- app/controllers/back/themes/paper/home.py
+++ app/controllers/back/themes/paper/home.py
@@ -1,12 +1,18 @@
 from core.view import view
 import os
 
+
+def init():
+    h = home()
+    h.init()
+
+
 class home:
     url = ['home']
     metadata = {'title': 'Home', 'modulo': 'home'}
 
-    def init(self,var):
-        if len(var)>0:
+    def init(self, var):
+        if len(var) > 0:
             if hasattr(self.__class__, var[0]) and callable(getattr(self.__class__, var[0])):
                 return False
             if var[0] in dir(os):
@@ -21,19 +27,16 @@ class home:
             ret = self.index()
         return ret
 
-
     def index(self):
-        ret={}
-        view.add('title','index');
+        ret = {}
+        view.add('title', 'index')
         ret['body'] = view.render('home')
         ret['headers'] = [('Content-Type', 'text/html; charset=utf-8')]
         return ret
 
-
-
     def ver(self):
-        ret={}
-        view.add('title','ver');
+        ret = {}
+        view.add('title', 'ver')
         ret['body'] = view.render('home')
         ret['headers'] = [('Content-Type', 'text/html; charset=utf-8')]
         return ret

9b62185168438c26c5e7c62007cd0e81a424c711
mysitioc <mysitioc@mysitio.cl>
2019-02-04 11:37:25 -0600

    --- Auto-Git Commit ---

--- core/app.py
+++ core/app.py
@@ -123,7 +123,7 @@ class app:
 
     @staticmethod
     def get_url(front=False):
-        if (app.front or front):
+        if (front):
             return app.url['base']
         else:
             return app.url['admin']

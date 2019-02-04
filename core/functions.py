e93a0df26a6d2b3d71a2d88ea89de8f75372b9d5
mysitioc <mysitioc@mysitio.cl>
2019-02-04 10:15:11 -0600

    --- Auto-Git Commit ---

--- core/functions.py
+++ core/functions.py
@@ -1,19 +1,22 @@
 
-def current_url():
-    url = environ['wsgi.url_scheme']+'://'
-    if environ.get('HTTP_HOST'):
-        url += environ['HTTP_HOST']
-    else:
-        url += environ['SERVER_NAME']
+class functions:
 
-        if environ['wsgi.url_scheme'] == 'https':
-            if environ['SERVER_PORT'] != '443':
-                url += ':' + environ['SERVER_PORT']
+
+    def current_url():
+        url = environ['wsgi.url_scheme']+'://'
+        if environ.get('HTTP_HOST'):
+            url += environ['HTTP_HOST']
         else:
-            if environ['SERVER_PORT'] != '80':
-                url += ':' + environ['SERVER_PORT']
-    url += environ['SCRIPT_NAME']
-    url += environ['PATH_INFO']
-    if environ['QUERY_STRING']:
-        url += '?' + environ['QUERY_STRING']
-    return url
+            url += environ['SERVER_NAME']
+
+            if environ['wsgi.url_scheme'] == 'https':
+                if environ['SERVER_PORT'] != '443':
+                    url += ':' + environ['SERVER_PORT']
+            else:
+                if environ['SERVER_PORT'] != '80':
+                    url += ':' + environ['SERVER_PORT']
+        url += environ['SCRIPT_NAME']
+        url += environ['PATH_INFO']
+        if environ['QUERY_STRING']:
+            url += '?' + environ['QUERY_STRING']
+        return url

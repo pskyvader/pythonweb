cd7fc0f80d27f0aac2cdc37e24660c6e8d725c2c
mysitioc <mysitioc@mysitio.cl>
2019-02-04 12:14:36 -0600

    --- Auto-Git Commit ---

--- core/functions.py
+++ core/functions.py
@@ -23,7 +23,8 @@ class functions():
                     url = url+"?" + urlencode(app.extra)
 
         url = (app.get_url() if front_auto else app.get_url(front)) + url
-        return urlretrieve(url)
+        #url= urlretrieve(url)
+        return url
 
     @staticmethod
     def current_url():

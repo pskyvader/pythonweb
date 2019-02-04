79e5dc607d5cca9e2e6199930ab15ac767aef5f7
mysitioc <mysitioc@mysitio.cl>
2019-02-04 09:58:11 -0600

    --- Auto-Git Commit ---

--- core/functions.py
+++ core/functions.py
@@ -1,4 +1,19 @@
 def current_url(environ):
-    #url = environ['HTTP_HOST']
-    #uri = environ['REQUEST_URI']
-    return environ
+    url = environ['wsgi.url_scheme']+'://'
+    if environ.get('HTTP_HOST'):
+        url += environ['HTTP_HOST']
+    else:
+        url += environ['SERVER_NAME']
+
+        if environ['wsgi.url_scheme'] == 'https':
+            if environ['SERVER_PORT'] != '443':
+            url += ':' + environ['SERVER_PORT']
+        else:
+            if environ['SERVER_PORT'] != '80':
+            url += ':' + environ['SERVER_PORT']
+
+    url += quote(environ.get('SCRIPT_NAME', ''))
+    url += quote(environ.get('PATH_INFO', ''))
+    if environ.get('QUERY_STRING'):
+        url += '?' + environ['QUERY_STRING']
+    return url

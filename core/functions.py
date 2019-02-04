b3082342a9a5b4d1d81f4ec8b9bee4361daa6de7
mysitioc <mysitioc@mysitio.cl>
2019-02-04 09:55:41 -0600

    --- Auto-Git Commit ---

--- core/functions.py
+++ core/functions.py
@@ -1,5 +1,4 @@
-import os
-def current_url():
-    url = os.environ['HTTP_HOST']
-    uri = os.environ['REQUEST_URI']
+def current_url(environ):
+    url = environ['HTTP_HOST']
+    uri = environ['REQUEST_URI']
     return url + uri

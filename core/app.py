b60f98b7560c193692abbb51843f9f1ef5328b42
mysitioc <mysitioc@mysitio.cl>
2019-02-04 14:25:01 -0600

    --- Auto-Git Commit ---

--- core/app.py
+++ core/app.py
@@ -83,7 +83,7 @@ class app:
                 response['body'] = ''
             else:
                 data_return['status'] = '404 Not Found'
-                response['body'] = '<html><body>No encontrado'+str(controller)+'</body></html>'
+                response['body'] = '<html><body>No encontrado '+str(app.root)+'</body></html>'
         else:
             data_return['status'] = '200 OK'
 

1634fddf3151b54832d51f83080a6dc211dbfa50
mysitioc <mysitioc@mysitio.cl>
2019-02-06 13:31:04 -0600

    --- Auto-Git Commit ---

--- app/controllers/back/themes/paper/header.py
+++ app/controllers/back/themes/paper/header.py
@@ -1,5 +1,7 @@
 from core.functions import functions
 from core.view import view
+
+
 class head:
     data = {
         'favicon': '',
@@ -69,23 +71,20 @@ class head:
         return ret
 
 
-
 class header:
-    data = { 'logo' : '', 'url_exit' : '', }
+    data = {'logo': '', 'url_exit': '', }
+
     def normal(self):
         from core.app import app
         import datetime
-        ret={'body':''}
+        ret = {'body': ''}
         if app.post.getfirst("ajax") is None:
             #logo = logo_model.getById(3);
             #self.data['logo_max'] = image.generar_url(logo['foto'][0], 'panel_max');
             #logo = logo_model.getById(4);
             #self.data['logo_min'] = image.generar_url(logo['foto'][0], 'panel_min');
-            self.data['url_exit'] = functions.generar_url(['logout'], False);
-            view.add_array(self.data);
-            view.add('date', datetime.datetime.today().strftime('%Y-%m-%d'));
-            ret['body']=view.render('header');
+            self.data['url_exit'] = functions.generar_url(['logout'], False)
+            view.add_array(self.data)
+            view.add('date', datetime.datetime.today().strftime('%Y-%m-%d'))
+            ret['body'] = view.render('header')
         return ret
-        
-    
-

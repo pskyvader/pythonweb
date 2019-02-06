26980bcf8177a8d0e897620a95af8be9c190025b
mysitioc <mysitioc@mysitio.cl>
2019-02-06 13:30:04 -0600

    --- Auto-Git Commit ---

--- app/controllers/back/themes/paper/header.py
+++ app/controllers/back/themes/paper/header.py
@@ -76,10 +76,10 @@ class header:
         from core.app import app
         import datetime
         if app.post.getfirst("ajax") is None:
-            logo = logo_model.getById(3);
-            self.data['logo_max'] = image.generar_url(logo['foto'][0], 'panel_max');
-            logo = logo_model.getById(4);
-            self.data['logo_min'] = image.generar_url(logo['foto'][0], 'panel_min');
+            #logo = logo_model.getById(3);
+            #self.data['logo_max'] = image.generar_url(logo['foto'][0], 'panel_max');
+            #logo = logo_model.getById(4);
+            #self.data['logo_min'] = image.generar_url(logo['foto'][0], 'panel_min');
             self.data['url_exit'] = functions.generar_url(['logout'], False);
             view.add_array(self.data);
             view.add('date', datetime.datetime.today().strftime('%Y-%m-%d'));

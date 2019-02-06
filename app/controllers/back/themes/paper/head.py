ff07d247acc68eea7154a03da342a0870d0c1f31
mysitioc <mysitioc@mysitio.cl>
2019-02-06 13:09:19 -0600

    --- Auto-Git Commit ---

--- app/controllers/back/themes/paper/head.py
+++ app/controllers/back/themes/paper/head.py
@@ -62,7 +62,7 @@ class head:
         from core.view import view
         import json
         
-        ret={'headers':'','body':'asdf'}
+        ret={'headers':'','body':''}
         if app.post.getfirst("ajax") is not None:
             if app.post.getfirst("ajax_header") is not None:
                 ret['headers']='Content-Type: application/json'

c07dffac2b80b1cd722dfd32e8e4c41c1800c8e0
mysitioc <mysitioc@mysitio.cl>
2019-02-06 14:36:51 -0600

    --- Auto-Git Commit ---

--- app/controllers/back/themes/paper/footer.py
+++ app/controllers/back/themes/paper/footer.py
@@ -6,20 +6,6 @@ class footer:
     def normal(self):
         ret = {'body': ''}
         if app.post.getfirst("ajax") is None:
-            #logo = logo_model.getById(3);
-            #self.data['logo_max'] = image.generar_url(logo['foto'][0], 'panel_max');
-            #logo = logo_model.getById(4);
-            #self.data['logo_min'] = image.generar_url(logo['foto'][0], 'panel_min');
-            self.data['url_exit'] = functions.generar_url(['logout'], False)
-            view.add_array(self.data)
-            view.add('date', datetime.datetime.today().strftime('%Y-%m-%d'))
-            ret['body'] = view.render('header')
+            ret['body'] = view.render('footer')
+            ret['body']+=view.js()
         return ret
-
-public function normal()
-    {
-        if (!isset($_POST['ajax'])) {
-            view::render('footer');
-            view::js();
-        }
-    }

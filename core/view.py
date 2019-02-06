38600df52cb1a31f5d82f1a91fb71a75e1c079f8
mysitioc <mysitioc@mysitio.cl>
2019-02-06 14:37:06 -0600

    --- Auto-Git Commit ---

--- core/view.py
+++ core/view.py
@@ -58,6 +58,10 @@ class view:
     @staticmethod
     def css(return_css=False):
         return ''
+    
+    @staticmethod
+    def js(return_js=False):
+        return ''
 
     @staticmethod
     def set_theme(theme):

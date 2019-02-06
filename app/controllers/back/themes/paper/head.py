abad1f6884d04a586c371489c58e79edd945e6ab
mysitioc <mysitioc@mysitio.cl>
2019-02-06 10:47:33 -0600

    --- Auto-Git Commit ---

--- app/controllers/back/themes/paper/head.py
+++ app/controllers/back/themes/paper/head.py
@@ -3,4 +3,20 @@ from core.functions import functions
 import os
 
 class head:
-    return False
+    data = {
+        'favicon' : '',
+        'keywords' : false,
+        'keywords_text' : '',
+        'description' : false,
+        'description_text' : '',
+        'title' : '',
+        'current_url' : '',
+        'image' : false,
+        'image_url' : '',
+        'logo' : '',
+        'color_primario' : '',
+        'manifest_url' : '',
+        'path' : '',
+        'modulo' : '',
+        'max_size' : -1,
+    }

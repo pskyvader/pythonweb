c176e20915bcca69f13de66d057a44a8d9d38078
mysitioc <mysitioc@mysitio.cl>
2019-02-06 13:37:35 -0600

    --- Auto-Git Commit ---

--- core/functions.py
+++ core/functions.py
@@ -1,3 +1,4 @@
+from core.app import app
 class functions():
     @staticmethod
     def url_redirect(url):
@@ -11,7 +12,6 @@ class functions():
 
     @staticmethod
     def generar_url(url, extra={}, front_auto=True, front=True):
-        from core.app import app
         from urllib.parse import urlencode
         url = '/'.join(url)
         if isinstance(extra, dict) and len(extra) > 0:
@@ -26,7 +26,6 @@ class functions():
 
     @staticmethod
     def current_url():
-        from core.app import app
         from urllib.parse import urlencode
         environ = app.environ
         url = environ['wsgi.url_scheme']+'://'

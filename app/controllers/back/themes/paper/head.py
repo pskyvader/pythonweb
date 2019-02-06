95bd0e06fb09c5f3b142b39517005d89860e18bc
mysitioc <mysitioc@mysitio.cl>
2019-02-06 13:00:49 -0600

    --- Auto-Git Commit ---

--- app/controllers/back/themes/paper/head.py
+++ app/controllers/back/themes/paper/head.py
@@ -17,58 +17,44 @@ class head:
         'max_size': -1,
     }
 
-    metadata={}
-
     def __init__(self, metadata):
         from core.app import app
         from core.functions import functions
-        head_tmp={}
         for key, value in metadata.items():
             if key in self.data:
-                head_tmp = value
+                head.data[key] = value
 
         config = app.get_config()
-        head_tmp['current_url'] = functions.current_url()
-        head_tmp['path'] = app.path
-        head_tmp['color_primario'] = config['color_primario']
-        head_tmp['googlemaps_key'] = config['googlemaps_key']
+        head.data['current_url'] = functions.current_url()
+        head.data['path'] = app.path
+        head.data['color_primario'] = config['color_primario']
+        head.data['googlemaps_key'] = config['googlemaps_key']
         # size=functions::get_max_size()
-        #head_tmp['max_size'] = size
-        # head_tmp['max_size_format'] = (size<0)?"Ilimitado":functions::file_size(size,true)
+        #head.data['max_size'] = size
+        # head.data['max_size_format'] = (size<0)?"Ilimitado":functions::file_size(size,true)
 
-        titulo = head_tmp['title'] + ' - ' + config['title']
+        titulo = head.data['title'] + ' - ' + config['title']
         if (len(titulo) > 75):
-            titulo = head_tmp['title'] + ' - ' + config['short_title']
-
-        if (len(titulo) > 75):
-            titulo = head_tmp['title']
+            titulo = head.data['title'] + ' - ' + config['short_title']
 
         if (len(titulo) > 75):
-            titulo = head_tmp['title'][0, 75]
+            titulo = head.data['title']
 
-        head.metadata['title'] = titulo
+        if (len(titulo) > 75):
+            titulo = head.data['title'][0, 75]
+
+        head.data['title'] = titulo
 
         # logo = logo_model::getById(3)
-        # head_tmp['logo'] = image::generar_url(logo['foto'][0], 'panel_max')
+        # head.data['logo'] = image::generar_url(logo['foto'][0], 'panel_max')
         # if (isset(metadata['image'])) {
-        #   head_tmp['image_url'] = metadata['image']
-        #  head_tmp['image'] = true
+        #   head.data['image_url'] = metadata['image']
+        #  head.data['image'] = true
         # }
         # logo = logo_model::getById(1)
-        # head_tmp['favicon'] = image::generar_url(logo['foto'][0], 'favicon')
+        # head.data['favicon'] = image::generar_url(logo['foto'][0], 'favicon')
 
-        head_tmp['manifest_url'] = app.get_url() + 'manifest.js'
-
-        for key, value in head_tmp.items():
-            if key not in head.metadata:
-                head.metadata = value
-
-        for key, value in head.data.items():
-            if key not in head.metadata:
-                head.metadata = value
-
-
-
+        head.data['manifest_url'] = app.get_url() + 'manifest.js'
 
 
     def normal(self):

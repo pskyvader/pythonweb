d8ad92d1e805500a18f41b4257ce62c0d80b0769
mysitioc <mysitioc@mysitio.cl>
2019-02-06 11:44:01 -0600

    --- Auto-Git Commit ---

--- core/app.py
+++ core/app.py
@@ -136,7 +136,9 @@ class app:
         post_env = app.environ.copy()
         post_env['QUERY_STRING'] = ''
         post = cgi.FieldStorage(
-            fp=app.environ['wsgi.input']
+            fp=app.environ['wsgi.input'],
+            environ=post_env,
+            keep_blank_values=True
         )
         return post
 

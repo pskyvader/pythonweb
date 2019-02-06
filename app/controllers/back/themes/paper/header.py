524c33ae72cf5294ee79b8aba64ab8b2a34551e2
mysitioc <mysitioc@mysitio.cl>
2019-02-06 13:26:49 -0600

    --- Auto-Git Commit ---

---
+++ app/controllers/back/themes/paper/header.py
@@ -0,0 +1,91 @@
+class head:
+    data = {
+        'favicon': '',
+        'keywords': '',
+        'description': '',
+        'title': '',
+        'current_url': '',
+        'image': '',
+        'color_primario': '',
+        'manifest_url': '',
+        'path': '',
+        'modulo': '',
+        'max_size': -1,
+    }
+
+    def __init__(self, metadata):
+        from core.app import app
+        from core.functions import functions
+        for key, value in metadata.items():
+            if key in self.data:
+                head.data[key] = value
+
+        config = app.get_config()
+        head.data['current_url'] = functions.current_url()
+        head.data['path'] = app.path
+        head.data['color_primario'] = config['color_primario']
+        head.data['googlemaps_key'] = config['googlemaps_key']
+        # size=functions::get_max_size()
+        #head.data['max_size'] = size
+        # head.data['max_size_format'] = (size<0)?"Ilimitado":functions::file_size(size,true)
+
+        titulo = head.data['title'] + ' - ' + config['title']
+        if (len(titulo) > 75):
+            titulo = head.data['title'] + ' - ' + config['short_title']
+
+        if (len(titulo) > 75):
+            titulo = head.data['title']
+
+        if (len(titulo) > 75):
+            titulo = head.data['title'][0:75]
+
+        head.data['title'] = titulo
+
+        # if (isset(metadata['image'])) {
+        #   head.data['image'] = metadata['image']
+        # }else{
+        # logo = logo_model::getById(3)
+        # head.data['image']=image::generar_url(logo['foto'][0], 'panel_max')
+        # }
+        # logo = logo_model::getById(1)
+        # head.data['favicon'] = image::generar_url(logo['foto'][0], 'favicon')
+
+        head.data['manifest_url'] = app.get_url() + 'manifest.js'
+
+    def normal(self):
+        from core.app import app
+        from core.view import view
+        import json
+
+        ret = {'headers': '', 'body': ''}
+        if app.post.getfirst("ajax") is None:
+            if app.post.getfirst("ajax_header") is None:
+                self.data['css'] = view.css(True)
+                view.add_array(self.data)
+                ret['body'] = view.render('head')
+            else:
+                ret['headers'] = 'Content-Type: application/json'
+                ret['body'] = json.dumps(self.data)
+        return ret
+
+
+
+class header:
+    data = {
+        'logo' : '',
+        'url_exit' : '',
+    }
+    public function normal()
+    {
+        if (!isset($_POST['ajax'])) {
+            $logo = logo_model::getById(3);
+            $this->data['logo_max'] = image::generar_url($logo['foto'][0], 'panel_max');
+            $logo = logo_model::getById(4);
+            $this->data['logo_min'] = image::generar_url($logo['foto'][0], 'panel_min');
+            $this->data['url_exit'] = functions::generar_url(array('logout'), false);
+            view::set_array($this->data);
+            view::set('date', date('Y-m-d H:i:s'));
+            view::render('header');
+        }
+    }
+}

57d35e9b5bd501a7c08887a25b9941d08659584f
mysitioc <mysitioc@mysitio.cl>
2019-02-06 13:39:05 -0600

    --- Auto-Git Commit ---

--- core/functions.py
+++ core/functions.py
@@ -1,11 +1,12 @@
 from core.app import app
 from urllib.parse import urlencode
+
+
 class functions():
     @staticmethod
     def url_redirect(url):
         ruta = functions.generar_url(url)
         current = functions.current_url()
-
         if (ruta != current):
             return ruta
         else:
@@ -15,11 +16,11 @@ class functions():
     def generar_url(url, extra={}, front_auto=True, front=True):
         url = '/'.join(url)
         if isinstance(extra, dict) and len(extra) > 0:
-            url = url+"?" + urlencode(extra,'utf-8')
+            url = url+"?" + urlencode(extra, 'utf-8')
         else:
             if (len(app.get) > 0):
                 if not isinstance(extra, bool) or extra == True:
-                    url = url+"?" + urlencode(app.get,'utf-8')
+                    url = url+"?" + urlencode(app.get, 'utf-8')
 
         url = (app.get_url() if front_auto else app.get_url(front)) + url
         return url
@@ -41,6 +42,6 @@ class functions():
                     url += ':' + environ['SERVER_PORT']
         url += environ['SCRIPT_NAME']
         url += environ['PATH_INFO']
-        if len(app.get)>0:
-            url += '?' + urlencode(app.get,'utf-8')
+        if len(app.get) > 0:
+            url += '?' + urlencode(app.get, 'utf-8')
         return url

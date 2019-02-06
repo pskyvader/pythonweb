05b2fc99a57bc10093d746a77f1850a8e92e1145
mysitioc <mysitioc@mysitio.cl>
2019-02-06 14:11:05 -0600

    --- Auto-Git Commit ---

--- app/controllers/back/themes/paper/aside.py
+++ app/controllers/back/themes/paper/aside.py
@@ -103,8 +103,7 @@ class aside:
                 url = functions.generar_url(["moduloconfiguracion"], False)
                 active = (url == current_url)
                 me = {'url': url, 'icon': 'mode_edit', 'title': 'Modulos', 'has_submenu': True, 'active': active, 'separador': False}
-                me['submenu'].append(
-                    {'url': url, 'sub_title': 'TODOS', 'active': active})
+                me['submenu'].append( {'url': url, 'sub_title': 'TODOS', 'active': active})
 
                 #mc=moduloconfiguracion_model.getAll()
                 mc={}

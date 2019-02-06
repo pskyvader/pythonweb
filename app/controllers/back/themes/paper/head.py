0ad3238e77fa8eb3960e3df3638ce7dbaf3767ee
mysitioc <mysitioc@mysitio.cl>
2019-02-06 11:05:04 -0600

    --- Auto-Git Commit ---

--- app/controllers/back/themes/paper/head.py
+++ app/controllers/back/themes/paper/head.py
@@ -20,3 +20,44 @@ class head:
         'modulo' : '',
         'max_size' : -1,
     }
+
+    def __init__(self, metadata):
+        for key,value in metadata.items():
+            if key in self.data:
+                head.data=value
+        
+        from core.app import app
+        config=app.get_config()
+        head.data['current_url']=functions.current_url()
+        head.data['path'] = app.path
+        head.data['color_primario'] = config['color_primario']
+        head.data['googlemaps_key'] = config['googlemaps_key']
+        #size=functions::get_max_size()
+        #head.data['max_size'] = size
+        #head.data['max_size_format'] = (size<0)?"Ilimitado":functions::file_size(size,true)
+
+        title = config['title']
+        short_title = config['short_title']
+        titulo = head.data['title'] + ' - ' + title
+        if (len(titulo) > 75):
+            titulo = head.data['title'] + ' - ' + short_title
+        
+        if (len(titulo) > 75):
+            titulo = head.data['title']
+        
+
+        if (len(titulo) > 75):
+            titulo = head.data['title'][0,75]
+        
+        head.data['title'] = titulo
+
+        logo = logo_model::getById(3)
+        head.data['logo'] = image::generar_url(logo['foto'][0], 'panel_max')
+        if (isset(metadata['image'])) {
+            head.data['image_url'] = metadata['image']
+            head.data['image'] = true
+        }
+        logo = logo_model::getById(1)
+        head.data['favicon'] = image::generar_url(logo['foto'][0], 'favicon')
+
+        head.data['manifest_url'] = app::get_url() . 'manifest.js'

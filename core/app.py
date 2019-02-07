4b20316c9e0f03cd9ca8487ec3f95e9f54733129
mysitioc <mysitioc@mysitio.cl>
2019-02-07 15:27:43 -0600

    --- Auto-Git Commit ---

--- core/app.py
+++ core/app.py
@@ -11,8 +11,8 @@ import importlib
 class app:
     config = {}
     app_dir = 'app/'
-    controller_dir = app_dir+'controllers/'
-    view_dir = app_dir+'views/'
+    controller_dir = app_dir + 'controllers/'
+    view_dir = app_dir + 'views/'
     url = {}
     front = True
     path = ''
@@ -21,7 +21,7 @@ class app:
     get = {}
 
     def __init__(self, root):
-        app.root = root+'/'
+        app.root = root + '/'
 
     def init(self, environ):
         app.environ = environ
@@ -37,7 +37,7 @@ class app:
 
         app.path = https + www + site + "/"
         if subdirectorio != '':
-            self.path += subdirectorio + "/"
+            self.path + = subdirectorio + "/"
             subdirectorio = "/" + subdirectorio + "/"
         else:
             subdirectorio = "/"
@@ -51,26 +51,26 @@ class app:
             app.front = True
 
         if app.front:
-            app.controller_dir = app.app_dir + \
-                'controllers/front/themes/'+config['theme']+'/'
-            app.view_dir = app.app_dir + \
-                'views/front/themes/'+config['theme']+'/'
+            app.controller_dir = app.app_dir +  \
+                'controllers/front/themes/' + config['theme'] + '/'
+            app.view_dir = app.app_dir +  \
+                'views/front/themes/' + config['theme'] + '/'
         else:
-            app.controller_dir = app.app_dir+'controllers/' + \
-                'back/themes/'+config['theme_back']+'/'
-            app.view_dir = app.app_dir+'views/' + \
-                'back/themes/'+config['theme_back']+'/'
+            app.controller_dir = app.app_dir + 'controllers/' +  \
+                'back/themes/' + config['theme_back'] + '/'
+            app.view_dir = app.app_dir + 'views/' +  \
+                'back/themes/' + config['theme_back'] + '/'
 
         app.url['base'] = app.path
-        app.url['admin'] = app.path+config['admin']+'/'
+        app.url['admin'] = app.path + config['admin'] + '/'
 
         app.url['base_sub'] = subdirectorio
         app.url['admin_sub'] = subdirectorio + config['admin'] + '/'
 
-        view.set_theme(app.root+app.view_dir)
+        view.set_theme(app.root + app.view_dir)
 
-        controller = app.controller_dir+url[0]
-        my_file = Path(app.root+controller+'.py')
+        controller = app.controller_dir + url[0]
+        my_file = Path(app.root + controller + '.py')
         if my_file.is_file():
             current_module = importlib.import_module(
                 controller.replace("/", "."))
@@ -89,17 +89,17 @@ class app:
             if response['error'] == 301:
                 if config['debug']:
                     data_return['status'] = '200 OK'
-                    response['body'] = '<html><body>redirige <a href="' + \
-                        response['redirect']+'">' + \
-                        response['redirect']+'</a></body></html>'
+                    response['body'] = '<html><body>redirige <a href="' +  \
+                        response['redirect'] + '">' +  \
+                        response['redirect'] + '</a></body></html>'
                 else:
                     data_return['status'] = '301 Moved Permanently'
                     response['headers'] = [('Location', response['redirect'])]
                     response['body'] = ''
             else:
                 data_return['status'] = '404 Not Found'
-                response['body'] = '<html><body>No encontrado ' + \
-                    str(my_file)+'</body></html>'
+                response['body'] = '<html><body>No encontrado ' +  \
+                    str(my_file) + '</body></html>'
         else:
             data_return['status'] = '200 OK'
 
@@ -146,7 +146,7 @@ class app:
     @staticmethod
     def get_config():
         if len(app.config) == 0:
-            with open(app.app_dir+'config/config.json') as f:
+            with open(app.app_dir + 'config/config.json') as f:
                 app.config = json.load(f)
         return app.config
 

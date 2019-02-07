5f98cb684f191c239360bf1d435b71210d403223
mysitioc <mysitioc@mysitio.cl>
2019-02-07 16:14:44 -0600

    --- Auto-Git Commit ---

--- core/view.py
+++ core/view.py
@@ -157,8 +157,6 @@ class view:
             view.add('js', [])
             view.add('is_css', True)
             view.add('css', css)
-            
-            print(view.data)
             return view.render('resources')
 
     @staticmethod

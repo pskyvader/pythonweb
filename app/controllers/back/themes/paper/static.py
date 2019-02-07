48042e3241dd23b14dd14d5434ac86b9f9f36950
mysitioc <mysitioc@mysitio.cl>
2019-02-07 14:19:56 -0600

    --- Auto-Git Commit ---

--- app/controllers/back/themes/paper/static.py
+++ app/controllers/back/themes/paper/static.py
@@ -7,19 +7,3 @@ def init(var):
     h = static()
     ret = h.index(var)
     return ret
-
-
-class static:
-    def index(self, var):
-        ret = {'body': ''}
-        theme = view.get_theme()
-        resource = '/'.join(var)
-        resource_url = theme + resource
-        file = Path(resource_url)
-        if not file.is_file() or len(var) == 0:
-            ret = {'error': 404}
-        else:
-            ret['body'] = open(resource_url, "r").read()
-            file_extension = splitext(resource_url)[1][1:]
-            ret['headers'] = 'Content-Type: text/'+file_extension
-        return ret

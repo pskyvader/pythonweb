df9a31ef936a24cde8c3d5eb9f7c5e537b2ea1b8
mysitioc <mysitioc@mysitio.cl>
2019-02-07 14:04:11 -0600

    --- Auto-Git Commit ---

--- app/controllers/back/themes/paper/static.py
+++ app/controllers/back/themes/paper/static.py
@@ -2,28 +2,28 @@ from core.view import view
 from pathlib import Path
 from os.path import splitext
 
+
 def init(var):
     h = static()
     ret = h.index(var)
     return ret
 
 
-
 class static:
     url = ['home']
     metadata = {'title': 'Home', 'modulo': 'home'}
 
-    def index(self,var):
-        ret = {'body':''}
+    def index(self, var):
+        ret = {'body': ''}
         theme = view.get_theme()
-        resource='/'.join(var)
-        resource_url = theme + resource 
+        resource = '/'.join(var)
+        resource_url = theme + resource
         my_file = Path(resource_url)
         if not my_file.is_file():
-            ret = { 'error': 404 }
+            ret = {'error': 404}
         else:
             ret['body'] = open(resource_url, "r").read()
-            file_extension =splitext(resource_url)[1][1:]
+            file_extension = splitext(resource_url)[1][1:]
             ret['headers'] = 'Content-Type: text/'+file_extension
 
         return ret

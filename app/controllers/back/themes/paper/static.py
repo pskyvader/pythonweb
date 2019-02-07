a34437e038424ad13d03b50b109670a2bab46831
mysitioc <mysitioc@mysitio.cl>
2019-02-07 17:27:44 -0600

    --- Auto-Git Commit ---

--- app/controllers/back/themes/paper/static.py
+++ app/controllers/back/themes/paper/static.py
@@ -25,7 +25,7 @@ class static:
         else:
             from codecs import open
             #ret['body'] = open(resource_url, "r",encoding = "ISO-8859-1").read()
-            ret['body'] = open(resource_url, encoding='utf-8').read()
+            ret['body'] = open(resource_url,errors=None).read()
             ret['encode']=False
             file_extension = splitext(resource_url)[1][1:]
             ret['headers'] = [ ('Content-Type', 'text/'+file_extension) ]

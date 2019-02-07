6b62869ca8971683e3d1743af7bc87dba548ee6b
mysitioc <mysitioc@mysitio.cl>
2019-02-07 14:01:11 -0600

    --- Auto-Git Commit ---

--- app/controllers/back/themes/paper/static.py
+++ app/controllers/back/themes/paper/static.py
@@ -1,5 +1,6 @@
 from core.view import view
 from pathlib import Path
+from os.path import splitext
 
 def init(var):
     h = static()
@@ -15,12 +16,14 @@ class static:
     def index(self,var):
         ret = {'body':''}
         theme = view.get_theme()
-        template_url = theme + template + "." + view.extension
-        my_file = Path(template_url)
+        resource='/'.join(var)
+        resource_url = theme + resource 
+        my_file = Path(resource_url)
         if not my_file.is_file():
-
-        ret = {
-                'error': 404
-            }
+            ret = { 'error': 404 }
+        else:
+            ret['body'] = open(resource_url, "r").read()
+            splitext(resource_url)
+            ret['headers'] = 'Content-Type: text/css'
 
         return ret

94ecfe05d5f735bdc99be2dacd800afcd0164309
mysitioc <mysitioc@mysitio.cl>
2019-02-08 12:45:05 -0600

    --- Auto-Git Commit ---

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -12,7 +12,10 @@ def application2(environ, start_response):
     main_data = app_web.init(environ)
     start_response(main_data['status'], main_data['headers'])
     ret = main_data['response_body']
-    return [bytes(ret, 'utf-8')]
+    if isinstance(ret, str):
+        return [bytes(ret, 'utf-8')]
+    else:
+        return [ret]
 
 
 class LoggingMiddleware:

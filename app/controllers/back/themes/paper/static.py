a50f7683e8c23711785d39e349817ee1521ac6d0
mysitioc <mysitioc@mysitio.cl>
2019-02-08 13:11:27 -0600

    --- Auto-Git Commit ---

--- app/controllers/back/themes/paper/static.py
+++ app/controllers/back/themes/paper/static.py
@@ -27,7 +27,7 @@ class static:
             ret = {'error': 404}
         else:
             mime=mimetypes.guess_type(resource_url)[0]
-            ret['headers'] = [ ('Content-Type', mime+'; charset=utf-8')]
+            ret['headers'] = [ ('Content-Type', mime+'; charset=utf-8'),('Accept-encoding', 'gzip,deflate')]
             with open(resource_url, "rb") as imageFile:
                 f = imageFile.read()
                 b = bytearray(f)

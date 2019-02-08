4e82888024e6e50271ae4d67d0820463d52dd8a6
mysitioc <mysitioc@mysitio.cl>
2019-02-08 12:00:33 -0600

    --- Auto-Git Commit ---

--- core/view.py
+++ core/view.py
@@ -86,7 +86,7 @@ class view:
                 c['url'] = theme + c['url']
                 my_file = Path(c['url'])
                 if my_file.is_file():
-                    if combine and c['combine'] and not c['defer']:
+                    if combine and c['combine'] and c['defer']:
                         fecha = functions.fecha_archivo(c['url'], True)
                         if (fecha > nuevo):
                             nuevo = fecha

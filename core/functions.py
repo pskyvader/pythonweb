baf752700e14bf036cd8ac8f4f465719fdccc3d0
mysitioc <mysitioc@mysitio.cl>
2019-02-15 13:25:02 -0600

    --- Auto-Git Commit ---

--- core/functions.py
+++ core/functions.py
@@ -26,6 +26,7 @@ class functions():
         c[cookie] = 'true' if value else 'false'
         c[cookie]["path"] = directory
         c[cookie]["expires"] = time
+        print(c)
         return True
 
     @staticmethod

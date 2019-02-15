4513c58140d5de802a6b4b2170bb5c3999b3958f
mysitioc <mysitioc@mysitio.cl>
2019-02-15 13:17:47 -0600

    --- Auto-Git Commit ---

--- core/functions.py
+++ core/functions.py
@@ -26,6 +26,7 @@ class functions():
         c[cookie] = value
         c[cookie]["path"] = directory
         c[cookie]["expires"] = datetime.now().timestamp() + time
+        print('set cookie:')
         print(c)
         return True
 

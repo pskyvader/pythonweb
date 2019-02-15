e165886f30ba06f31a8286752f453eb0b266fd03
mysitioc <mysitioc@mysitio.cl>
2019-02-15 13:06:32 -0600

    --- Auto-Git Commit ---

--- core/view.py
+++ core/view.py
@@ -177,8 +177,7 @@ class view:
             if os.access(dir_resources, os.R_OK):
                 combine_files = ''
                 for l in locales:
-                    combine_files += '\n' + open(l['url'],
-                                                    "r", encoding='utf-8').read()
+                    combine_files += '\n' + open(l['url'], "r", encoding='utf-8').read()
 
                 test = os.listdir(dir_resources)
                 for item in test:

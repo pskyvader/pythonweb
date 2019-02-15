6aa5ebd4c4ff63874b7257e987f5ea167aa5412f
mysitioc <mysitioc@mysitio.cl>
2019-02-15 12:25:15 -0600

    --- Auto-Git Commit ---

--- app/views/back/themes/paper/js/online.js
+++ app/views/back/themes/paper/js/online.js
@@ -8,6 +8,7 @@ function online() {
     // Sólo hacer el fetch si navigator.onLine es true
     if (navigator.onLine) {
         fetch(path + 'ping').then(function(response) {
+            console.log(response);
             if (!response.ok) {
                 if (is_online) {
                     tiempo = tiempo_offline;

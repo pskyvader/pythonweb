8f423815644881617e5c52d834ec3d43068a4ce6
mysitioc <mysitioc@mysitio.cl>
2019-02-18 12:32:17 -0600

    --- Auto-Git Commit ---

--- app/controllers/back/themes/paper/manifest.py
+++ app/controllers/back/themes/paper/manifest.py
@@ -1,44 +1,47 @@
 from core.app import app
 from core.functions import functions
+import json
+
+
 def init(var):
     h = manifest()
     ret = h.index()
     return ret
 
+
 class manifest:
     def index(self):
-        ret = {'body':''}
-        version_application=1
-        config=app.get_config
-        logo=logo_model.getById(7)
+        ret = {'body': ''}
+        version_application = 1
+        config = app.get_config
+        logo = logo_model.getById(7)
         manifest = {
-            'short_name' : config['short_title'],
-            'name' : config['title'],
-            'icons' : {
+            'short_name': config['short_title'],
+            'name': config['title'],
+            'icons': {
                 {
-                    'src' : image.generar_url(logo['foto'][0], 'icono50'),
-                    'type' : 'image/png',
-                    'sizes' : '50x50',
+                    'src': image.generar_url(logo['foto'][0], 'icono50'),
+                    'type': 'image/png',
+                    'sizes': '50x50',
                 }, {
-                    'src' : image.generar_url(logo['foto'][0], 'icono100'),
-                    'type' : 'image/png',
-                    'sizes' : '100x100',
+                    'src': image.generar_url(logo['foto'][0], 'icono100'),
+                    'type': 'image/png',
+                    'sizes': '100x100',
                 }, {
-                    'src' : image.generar_url(logo['foto'][0], 'icono200'),
-                    'type' : 'image/png',
-                    'sizes' : '200x200',
+                    'src': image.generar_url(logo['foto'][0], 'icono200'),
+                    'type': 'image/png',
+                    'sizes': '200x200',
                 }, {
-                    'src' : image.generar_url(logo['foto'][0], 'icono600'),
-                    'type' : 'image/png',
-                    'sizes' : '600x600',
+                    'src': image.generar_url(logo['foto'][0], 'icono600'),
+                    'type': 'image/png',
+                    'sizes': '600x600',
                 }
             },
-            "start_url" : functions.generar_url(["application","index",version_application], false),
-            "background_color" : config['color_secundario'],
-            "display" : "standalone",
-            "theme_color" : config['color_primario'],
-        )
-        header('Content-Type: application/json')
-        echo functions.encode_json(manifest)
-
+            "start_url": functions.generar_url(["application", "index", version_application], False),
+            "background_color": config['color_secundario'],
+            "display": "standalone",
+            "theme_color": config['color_primario'],
+        }
+        ret['headers'] = 'Content-Type: application/json'
+        ret['body'] = json.dumps(self.data)
         return ret

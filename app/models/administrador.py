4bfad90fb40031144f630fc5f5ab0e3ec8ffa4b2
mysitioc <mysitioc@mysitio.cl>
2019-02-18 14:14:49 -0600

    --- Auto-Git Commit ---

--- app/models/administrador.py
+++ app/models/administrador.py
@@ -3,5 +3,5 @@ class administrador:
     @staticmethod
     def get_all():
         connection = database.instance()
-        row = connection.consulta('select * from seo_administrador')
+        row = connection.consulta('select * from seo_administrador',False)
         return row

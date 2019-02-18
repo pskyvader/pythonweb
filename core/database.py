09b384349715d25d8454b413eca31d9e86625dc9
mysitioc <mysitioc@mysitio.cl>
2019-02-18 14:21:49 -0600

    --- Auto-Git Commit ---

--- core/database.py
+++ core/database.py
@@ -26,7 +26,7 @@ class database():
             self.conect()
         except:
             print('error DB connection')
-            self._errors='Error DB connection'
+            self._errors='Error DB connection '+ self._dbHost+ self._dbUser+ self._dbPassword+ self._dbName
 
     def conect(self):
         self._connection = pymysql.connect( self._dbHost, self._dbUser, self._dbPassword, self._dbName)

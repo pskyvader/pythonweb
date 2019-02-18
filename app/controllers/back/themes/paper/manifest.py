59ac39d64c618d396967b68acd9196fbf75ca7c1
mysitioc <mysitioc@mysitio.cl>
2019-02-18 12:23:17 -0600

    --- Auto-Git Commit ---

---
+++ app/controllers/back/themes/paper/manifest.py
@@ -0,0 +1,20 @@
+def init(var):
+    h = home()
+    if len(var) > 0:
+        if hasattr(h, var[0]) and callable(getattr(h, var[0])):
+            fun = var[0]
+            del var[0]
+            method=getattr(h, fun)
+            ret = method(var)
+        else:
+            ret = {
+                'error': 404,
+            }
+    else:
+        ret = h.index()
+    return ret
+
+class manifest:
+    def index(self):
+        ret = {'body':''}
+        return ret

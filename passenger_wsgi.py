7002142b3d8e2b6ad345a08eec35e9ffc10a43e6
mysitioc <mysitioc@mysitio.cl>
2019-02-18 11:20:53 -0600

    --- Auto-Git Commit ---

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -27,7 +27,7 @@ class LoggingMiddleware:
         errors = environ['wsgi.errors']
 
         def _start_response(status, headers, *args):
-            print(errors,len(errors),type(errors))
+            print(errors,type(errors))
             if errors!="":
                 pprint.pprint(('RESPONSE', status, headers), stream=errors)
             return start_response(status, headers, *args)

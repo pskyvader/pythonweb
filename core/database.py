143d80c8a9bd3dd9a6761116f34fd287ee4baa14
mysitioc <mysitioc@mysitio.cl>
2019-02-18 13:32:33 -0600

    --- Auto-Git Commit ---

---
+++ core/database.py
@@ -0,0 +1,14 @@
+
+class database():
+    _dbUser
+    _dbPassword
+    _dbHost
+    _dbName
+
+    _connection
+    _instance
+
+    _prefix
+
+    def __init__(self):
+        app.root = root + '/'

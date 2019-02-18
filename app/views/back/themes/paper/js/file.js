171cf02e9eae5773fcd47b449096adfc792eec9c
mysitioc <mysitioc@mysitio.cl>
2019-02-18 15:04:58 -0600

    --- Auto-Git Commit ---

--- app/views/back/themes/paper/js/file.js
+++ app/views/back/themes/paper/js/file.js
@@ -2,7 +2,6 @@ function inicio_file() {
     $('.file_multiple').each(function() {
         inicio_file_multiple($(this));
     });
-    
     $('.file_individual').each(function() {
         var t = $(this);
         $(t).on('change', 'input[name="..."]', function(e) {

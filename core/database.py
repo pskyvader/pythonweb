4b6b6e202d5e4adbeae4f315084e027ee573616c
mysitioc <mysitioc@mysitio.cl>
2019-02-18 14:31:05 -0600

    --- Auto-Git Commit ---

--- core/database.py
+++ core/database.py
@@ -30,7 +30,7 @@ class database():
 
     def conect(self):
         self._connection = pymysql.connect(
-            self._dbHost, self._dbUser, self._dbPassword, self._dbName, charset='utf8mb4')
+            self._dbHost, self._dbUser, self._dbPassword, self._dbName, charset='utf8mb4',cursorclass=pymysql.cursors.DictCursor)
 
     def prepare(self):
         cursor = self._connection.cursor()

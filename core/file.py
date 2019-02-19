362ee0a9a1051436cd91d713596950ebc37f36e1
mysitioc <mysitioc@mysitio.cl>
2019-02-19 14:43:57 -0600

    --- Auto-Git Commit ---

--- core/file.py
+++ core/file.py
@@ -1,12 +1,14 @@
+from pathlib import Path
 from .image import image
 class file(image):
     @classmethod
     def delete(cls,folder, file = '', subfolder = '', sub = ''):
+        import shutil
         if "" == file and '' != subfolder:
             url = cls.get_upload_dir() + folder + '/' + subfolder + '/'
-            if (file_exists(url)) {
-                cls.removeDirectory(url)
-            }
+            my_file = Path(url)
+            if my_file.is_dir():
+                shutil.rmtree(url)
         } elseif ('' == file && '' == subfolder) {
             url = cls.get_upload_dir() . folder . '/'
             if (file_exists(url)) {

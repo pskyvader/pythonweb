93d14cae646a31c7eac4d4ef787ea56ce578e565
mysitioc <mysitioc@mysitio.cl>
2019-02-19 14:43:12 -0600

    --- Auto-Git Commit ---

--- core/file.py
+++ core/file.py
@@ -1,2 +1,24 @@
 from .image import image
 class file(image):
+    @classmethod
+    def delete(cls,folder, file = '', subfolder = '', sub = ''):
+        if "" == file and '' != subfolder:
+            url = cls.get_upload_dir() . folder . '/' . subfolder . '/'
+            if (file_exists(url)) {
+                cls.removeDirectory(url)
+            }
+        } elseif ('' == file && '' == subfolder) {
+            url = cls.get_upload_dir() . folder . '/'
+            if (file_exists(url)) {
+                cls.removeDirectory(url)
+            }
+        } else {
+            if ('' != subfolder) {
+                subfolder .= '/'
+            }
+            if ('' != sub) {
+                sub .= '/'
+            }
+            url = cls.get_upload_dir() . folder . '/' . subfolder . sub . file['url']
+            if (file_exists(url)) {
+                unlink(url)

d2e716f0b55ea61c904c8fa8e10e9a92dbf485d0
mysitioc <mysitioc@mysitio.cl>
2019-02-19 14:21:26 -0600

    --- Auto-Git Commit ---

--- core/functions.py
+++ core/functions.py
@@ -99,7 +99,15 @@ class functions():
         $url = implode('-', array_filter($url));
         $url = strtolower($url);
         return $url;
-    }
+    
+    def replaceMultiple(mainString, toBeReplaces, newString):
+        # Iterate over the strings to be replaced
+        for elem in toBeReplaces :
+            # Check if string is in the main string
+            if elem in mainString :
+                # Replace the string
+                mainString = mainString.replace(elem, newString)
+        return  mainString
 
     @staticmethod
     def fecha_archivo(archivo, only_fecha=False, final_file=''):

1b67af387c58796c1003c0214947b5ae2d5f8cc8
mysitioc <mysitioc@mysitio.cl>
2019-02-19 14:45:27 -0600

    --- Auto-Git Commit ---

--- core/file.py
+++ core/file.py
@@ -1,8 +1,10 @@
 from pathlib import Path
 from .image import image
+
+
 class file(image):
     @classmethod
-    def delete(cls,folder, file = '', subfolder = '', sub = ''):
+    def delete(cls, folder, file='', subfolder='', sub=''):
         import shutil
         if "" == file and '' != subfolder:
             url = cls.get_upload_dir() + folder + '/' + subfolder + '/'
@@ -17,10 +19,12 @@ class file(image):
         else:
             if '' != subfolder:
                 subfolder += '/'
-            
+
             if '' != sub:
                 sub += '/'
-            
-            url = cls.get_upload_dir() + folder + '/' + subfolder + sub + file['url']
-            if (file_exists(url)) {
-                unlink(url)
+
+            url = cls.get_upload_dir() + folder + '/' + \
+                subfolder + sub + file['url']
+            my_file = Path(url)
+            if my_file.is_file():
+                my_file.unlink()

629dceddf523bee91d7c90363f70ffbd72294379
mysitioc <mysitioc@mysitio.cl>
2019-02-20 10:57:09 -0600

    --- Auto-Git Commit ---

--- app/models/administrador.py
+++ app/models/administrador.py
@@ -5,3 +5,59 @@ class administrador(base_model):
     idname = 'idadministrador'
     table                = 'administrador'
     cookie=''
+    @classmethod
+    def insert(cls, data,  loggging=True):
+
+        if 'pass' in data and data['pass']!='':
+            if 'pass_repetir' in data and data['pass_repetir']!='':
+                if data['pass'] != data['pass_repetir']:
+                    return {'exito' => False, 'mensaje' : 'Contraseñas no coinciden'}
+            else:
+                return {'exito' : False, 'mensaje' : 'Contraseña no existe'}
+            }
+        } else {
+            return array('exito' => false, 'mensaje' => 'Contraseña no existe');
+        }
+
+        # fields     = table.getByname(cls.table)
+        fields = {}
+        insert = database.create_data(fields, data)
+        connection = database.instance()
+        row = connection.insert(cls.table, cls.idname, insert)
+        if isinstance(row, int) and row > 0:
+            last_id = row
+            if loggging:
+                #log.insert_log(cls.table, cls.idname, cls, insert)
+                pass
+            return last_id
+        else:
+            return row
+
+
+        if (isset($data['pass']) && $data['pass'] != '') {
+            if (isset($data['pass_repetir']) && $data['pass_repetir'] != '') {
+                if ($data['pass'] != $data['pass_repetir']) {
+                    return array('exito' => false, 'mensaje' => 'Contraseñas no coinciden');
+                }
+            } else {
+                return array('exito' => false, 'mensaje' => 'Contraseña no existe');
+            }
+        } else {
+            return array('exito' => false, 'mensaje' => 'Contraseña no existe');
+        }
+        $fields          = table::getByname(static::$table);
+        $insert          = database::create_data($fields, $data);
+        $insert['pass']  = database::encript($insert['pass']);
+        $insert['email'] = strtolower($insert['email']);
+        $connection      = database::instance();
+        $row             = $connection->insert(static::$table, static::$idname, $insert);
+        if (is_int($row) && $row>0) {
+            $last_id = $row;
+            if ($log) {
+                log::insert_log(static::$table, static::$idname, __FUNCTION__, $insert);
+            }
+            return $last_id;
+        } else {
+            return $row;
+        }
+    }

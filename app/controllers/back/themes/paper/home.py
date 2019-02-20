c6d5235eff328b2a1a0e1a08482682ab0f079881
mysitioc <mysitioc@mysitio.cl>
2019-02-20 10:35:23 -0600

    --- Auto-Git Commit ---

--- app/controllers/back/themes/paper/home.py
+++ app/controllers/back/themes/paper/home.py
@@ -29,6 +29,9 @@ class home:
 
     def index(self):
         ret = {'body':''}
+        if not administrador_model.verificar_sesion():
+            self.url = ['login', 'index', 'home']
+        
         url_return=functions.url_redirect(self.url)
         if url_return!='':
             ret['error']=301

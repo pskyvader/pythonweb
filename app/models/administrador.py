da6e0ba1cf127fbdecd8f4a5aa310ac098eaa012
mysitioc <mysitioc@mysitio.cl>
2019-02-20 10:58:39 -0600

    --- Auto-Git Commit ---

--- app/models/administrador.py
+++ app/models/administrador.py
@@ -1,26 +1,27 @@
 from core.database import database
 from .base_model import base_model
 
+
 class administrador(base_model):
     idname = 'idadministrador'
-    table                = 'administrador'
-    cookie=''
+    table = 'administrador'
+    cookie = ''
+
     @classmethod
     def insert(cls, data,  loggging=True):
-
-        if 'pass' in data and data['pass']!='':
-            if 'pass_repetir' in data and data['pass_repetir']!='':
+        if 'pass' in data and data['pass'] != '':
+            if 'pass_repetir' in data and data['pass_repetir'] != '':
                 if data['pass'] != data['pass_repetir']:
-                    return {'exito': False, 'mensaje' : 'Contraseñas no coinciden'}
+                    return {'exito': False, 'mensaje': 'Contraseñas no coinciden'}
             else:
-                return {'exito' : False, 'mensaje' : 'Contraseña no existe'}
+                return {'exito': False, 'mensaje': 'Contraseña no existe'}
         else:
             return {'exito': False, 'mensaje': 'Contraseña no existe'}
 
         # fields     = table.getByname(cls.table)
         fields = {}
         insert = database.create_data(fields, data)
-        insert['pass']  = database.encript(insert['pass'])
+        insert['pass'] = database.encript(insert['pass'])
         insert['email'] = insert['email'].lower()
 
         connection = database.instance()

3ddc5dfb6fcc5c439571b43448b9b267a7154eab
mysitioc <mysitioc@mysitio.cl>
2019-02-22 12:59:27 -0600

    --- Auto-Git Commit ---

--- app/controllers/back/themes/paper/login.py
+++ app/controllers/back/themes/paper/login.py
@@ -1,5 +1,5 @@
 def init(var):
-    h = home()
+    h = login()
     if len(var) > 0:
         if hasattr(h, var[0]) and callable(getattr(h, var[0])):
             fun = var[0]
@@ -13,3 +13,53 @@ def init(var):
     else:
         ret = h.index()
     return ret
+
+
+class login:
+    url = ['login']
+    metadata = {'title': 'login', 'modulo': 'login'}
+
+    def index(self):
+        ret = {'body':''}
+        if not administrador_model.verificar_sesion():
+            self.url = ['login', 'index', 'login']
+        
+        url_return=functions.url_redirect(self.url)
+        if url_return!='':
+            ret['error']=301
+            ret['redirect']=url_return
+            return ret
+        
+        registros=administrador_model.getAll(where={'idpadre':1},condiciones={'limit':1,'limit2':0})
+        h = head(self.metadata)
+        ret_head=h.normal()
+        if ret_head['headers']!='':
+            return ret_head
+        ret['body']+=ret_head['body']
+        
+        he=header()
+        #ret_header=he.normal()
+        #ret['body']+=ret_header['body']
+        ret['body']+=he.normal()['body']
+
+        asi = aside()
+        ret_asi=asi.normal()
+        ret['body']+=ret_asi['body']
+
+
+        view.add('title', 'index')
+        view.add('var', str(registros))
+        breadcrumb=[
+            {'active':'active','url':'aaaa','title':'titulo'},
+            {'active':'','url':'bbb','title':'titulo2'},
+            {'active':'active','url':'ccc','title':'titulo3'},
+        ]
+        view.add('breadcrumb', breadcrumb)
+        ret['body'] += view.render('login')
+
+
+        f = footer()
+        ret_f=f.normal()
+        ret['body']+=ret_f['body']
+
+        return ret

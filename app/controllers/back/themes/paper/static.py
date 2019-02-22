a7304b36b9bfbd38e5068c649da561ed62a74d12
mysitioc <mysitioc@mysitio.cl>
2019-02-22 10:51:15 -0600

    --- Auto-Git Commit ---

--- app/controllers/back/themes/paper/static.py
+++ app/controllers/back/themes/paper/static.py
@@ -67,8 +67,8 @@ class static:
 
         if 'wsgi.file_wrapper' in app.environ:
             # Return env[wsgi.fw](file, block size)
-            return app.environ['wsgi.file_wrapper'](the_file , 1024) 
+            return app.environ['wsgi.file_wrapper'](resource_url , 1024) 
         else: 
-            return iter(lambda: the_file.read(1024), '')
+            return iter(lambda: resource_url.read(1024), '')
 
         return ret

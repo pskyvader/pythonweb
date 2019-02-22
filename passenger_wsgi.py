c0a5054dc6a93e5c9991dd0d68227d2c6e96fa93
mysitioc <mysitioc@mysitio.cl>
2019-02-22 10:47:30 -0600

    --- Auto-Git Commit ---

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -7,7 +7,9 @@ from beaker.middleware import SessionMiddleware
 sys.path.insert(0, os.path.dirname(__file__))
 
 def application2(environ, start_response):
-    print(environ)
+
+    print(environ['wsgi.file_wrapper'])
+
     app_web = app(os.path.dirname(__file__))
     main_data = app_web.init(environ)
     ret = main_data['response_body']

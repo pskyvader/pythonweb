667a04f9ec80a63b4173452ea69112f10d566d84
mysitioc <mysitioc@mysitio.cl>
2019-02-22 12:58:57 -0600

    --- Auto-Git Commit ---

---
+++ app/controllers/back/themes/paper/login.py
@@ -0,0 +1,15 @@
+def init(var):
+    h = home()
+    if len(var) > 0:
+        if hasattr(h, var[0]) and callable(getattr(h, var[0])):
+            fun = var[0]
+            del var[0]
+            method=getattr(h, fun)
+            ret = method(var)
+        else:
+            ret = {
+                'error': 404,
+            }
+    else:
+        ret = h.index()
+    return ret

ac8858383ab8e3e563a73a76f00997fd72c34319
mysitioc <mysitioc@mysitio.cl>
2019-02-22 16:51:19 -0600

    --- Auto-Git Commit ---

--- core/database.py
+++ core/database.py
@@ -353,18 +353,13 @@ class database():
     @staticmethod
     def encript(password):
         import hashlib
-        #import crypt
-        import pcrypt
+        import crypt
         salt = hashlib.sha1()
         salt.update(password.encode('utf-8'))
-        #p = crypt.crypt(password, salt.hexdigest())
-        p = pcrypt.crypt(password, salt.hexdigest())
-        print(p)
-
+        p = crypt.crypt(password, salt.hexdigest())
         salt2=hashlib.sha1()
         salt2.update(p.encode('utf-8'))
         password= salt.hexdigest() + salt2.hexdigest()
-        print(password)
         return password
 
     @staticmethod

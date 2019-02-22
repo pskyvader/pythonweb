128507dddf1e0e10ec3b7dcf8565d22f57d19974
mysitioc <mysitioc@mysitio.cl>
2019-02-22 13:34:58 -0600

    --- Auto-Git Commit ---

--- app/controllers/back/themes/paper/login.py
+++ app/controllers/back/themes/paper/login.py
@@ -5,106 +5,106 @@ from app.models.administrador import administrador as administrador_model
 from .head import head
 from .footer import footer
 
+
 def init(var):
     h = login()
     if len(var) > 0:
         if hasattr(h, var[0]) and callable(getattr(h, var[0])):
             fun = var[0]
             del var[0]
-            method=getattr(h, fun)
+            method = getattr(h, fun)
             ret = method(var)
         else:
             ret = {
                 'error': 404,
             }
     else:
-        ret = h.index()
+        ret = h.index({})
     return ret
 
 
 class login:
-    url = ['login','index']
+    url = ['login', 'index']
     metadata = {'title': 'login', 'modulo': 'login'}
 
-    def index(self,url):
+    def index(self, url):
         from time import time
-        ret = {'body':''}
-        self.url=self.url+url
+        ret = {'body': ''}
+        self.url = self.url+url
 
-        cookie_admin=functions.get_cookie('cookieadmin'+app.prefix_site)
-        if cookie_admin!=False:
+        cookie_admin = functions.get_cookie('cookieadmin'+app.prefix_site)
+        if cookie_admin != False:
             logueado = administrador_model.login_cookie(cookie_admin)
             if logueado:
-                if not url: 
+                if not url:
                     self.url = ['home']
                 else:
                     self.url = url
-                    
 
-        if 'bloqueo_administrador' in app.session and app.session['bloqueo_administrador']>time():
-            ret['body'] = "IP Bloqueada por intentos fallidos. Intente más tarde. tiempo: "+time()-app.session['bloqueo_administrador']+" segundos"
+        if 'bloqueo_administrador' in app.session and app.session['bloqueo_administrador'] > time():
+            ret['body'] = "IP Bloqueada por intentos fallidos. Intente más tarde. tiempo: " + \
+                time()-app.session['bloqueo_administrador']+" segundos"
             return ret
-        
-        
-        if 'intento_administrador' in app.session and int(app.session['intento_administrador'])%5==0:
-            app.session['bloqueo_administrador']= time() + 60*int(app.session['intento_administrador'])
-            #if app.session['intento_administrador']>=15) bloquear_ip(getRealIP())
-            app.session['intento_administrador']=int(app.session['intento_administrador'])+1
-            
 
-        error_login=False
+        if 'intento_administrador' in app.session and int(app.session['intento_administrador']) % 5 == 0:
+            app.session['bloqueo_administrador'] = time(
+            ) + 60*int(app.session['intento_administrador'])
+            # if app.session['intento_administrador']>=15) bloquear_ip(getRealIP())
+            app.session['intento_administrador'] = int(
+                app.session['intento_administrador'])+1
+
+        error_login = False
         if 'email' in app.post and 'pass' in app.post and 'token' in app.post:
-            if app.session['login_token']['token']==app.post['token']:
-                if time()-int(app.session['login_token']['time'])<=120:
+            if app.session['login_token']['token'] == app.post['token']:
+                if time()-int(app.session['login_token']['time']) <= 120:
                     if not 'recorda' in app.post:
-                        app.post['recordar']=''
-                    logueado=administrador_model.login(app.post['email'],app.post['pass'],app.post['recordar'])
+                        app.post['recordar'] = ''
+                    logueado = administrador_model.login(
+                        app.post['email'], app.post['pass'], app.post['recordar'])
                     if logueado:
                         if 'intento_administrador' in app.session['intento_administrador']:
-                            app.session['intento_administrador']=0
-                        if not url: 
+                            app.session['intento_administrador'] = 0
+                        if not url:
                             self.url = ['home']
                         else:
-                            self.url=url
+                            self.url = url
                     else:
-                        error_login=True
+                        error_login = True
                         if 'intento_administrador' in app.session['intento_administrador']:
-                            app.session['intento_administrador']=0
-                        app.session['intento_administrador']+=1
+                            app.session['intento_administrador'] = 0
+                        app.session['intento_administrador'] += 1
                 else:
-                    error_login=True
+                    error_login = True
             else:
-                error_login=True
+                error_login = True
                 if 'intento_administrador' in app.session['intento_administrador']:
-                    app.session['intento_administrador']=0
-                app.session['intento_administrador']+=5
-                
-        url_return=functions.url_redirect(self.url)
-        if url_return!='':
-            ret['error']=301
-            ret['redirect']=url_return
+                    app.session['intento_administrador'] = 0
+                app.session['intento_administrador'] += 5
+
+        url_return = functions.url_redirect(self.url)
+        if url_return != '':
+            ret['error'] = 301
+            ret['redirect'] = url_return
             return ret
-        
 
         token = functions.generar_pass(20)
-        app.session['login_token'] = {'token' : token, 'time' : time()}
+        app.session['login_token'] = {'token': token, 'time': time()}
         h = head(self.metadata)
-        ret_head=h.normal()
-        if ret_head['headers']!='':
+        ret_head = h.normal()
+        if ret_head['headers'] != '':
             return ret_head
-        ret['body']+=ret_head['body']
+        ret['body'] += ret_head['body']
 
         view.add('logo', '')
         view.add('error_login', error_login)
         view.add('token', token)
         view.add('url_recuperar', functions.generar_url(["recuperar"]))
-        #logo=logo_model.getById(2)
+        # logo=logo_model.getById(2)
         #view.add('logo', image.generar_url(logo['foto'][0], 'login'))
         ret['body'] += view.render('login')
-        
 
         f = footer()
-        ret_f=f.normal()
-        ret['body']+=ret_f['body']
+        ret_f = f.normal()
+        ret['body'] += ret_f['body']
 
         return ret

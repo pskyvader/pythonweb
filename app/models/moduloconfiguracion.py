5245562b84b9b8d76e088a7029e31e69dd7933e0
mysitioc <mysitioc@mysitio.cl>
2019-02-28 12:39:41 -0600

    --- Auto-Git Commit ---

---
+++ app/models/moduloconfiguracion.py
@@ -0,0 +1,8 @@
+from core.database import database
+from .base_model import base_model
+import json
+
+
+class moduloconfiguracion(base_model):
+    idname = 'idmoduloconfiguracion'
+    table = 'moduloconfiguracion'

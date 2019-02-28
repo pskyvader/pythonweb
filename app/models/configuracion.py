319fde2f5ee1fec15fc7f1012e8766088c5f5059
mysitioc <mysitioc@mysitio.cl>
2019-02-28 12:00:24 -0600

    --- Auto-Git Commit ---

--- app/models/configuracion.py
+++ app/models/configuracion.py
@@ -4,3 +4,27 @@ from .base_model import base_model
 class configuracion(base_model):
     idname = 'idconfiguracion'
     table = 'configuracion'
+
+    @classmethod
+    def getByVariable(cls, variable:str):
+        $where      = array('variable' => $variable);
+        $condicion  = array('limit' => 1);
+        $connection = database::instance();
+        $row        = $connection->get(static::$table, static::$idname, $where, $condicion);
+        return (count($row) == 1) ? $row[0]['valor'] : false;
+    }
+
+    public static function setByVariable(string $variable, string $valor)
+    {
+        $where      = array('variable' => $variable);
+        $condicion  = array('limit' => 1);
+        $connection = database::instance();
+        $row        = $connection->get(static::$table, static::$idname, $where, $condicion);
+
+        if (count($row) == 0) {
+            $row = self::insert(array('variable' => $variable, 'valor' => $valor));
+        } else {
+            $row = self::update(array('variable' => $variable, 'valor' => $valor, 'id' => $row[0][0]));
+        }
+        return $row;
+    }

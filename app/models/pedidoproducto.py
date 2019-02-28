0437f65a2d5e2ea64503dba438b63211ab552ed0
mysitioc <mysitioc@mysitio.cl>
2019-02-28 13:16:27 -0600

    --- Auto-Git Commit ---

--- app/models/pedidoproducto.py
+++ app/models/pedidoproducto.py
@@ -1,5 +1,6 @@
 from core.database import database
 from .base_model import base_model
+from .log import log
 import json
 
 class pedidoproducto(base_model):
@@ -18,7 +19,7 @@ class pedidoproducto(base_model):
         if isinstance(row, int) and row > 0:
             last_id = row
             if loggging:
-                #log.insert_log(cls.table, cls.idname, cls, insert)
+                log.insert_log(cls.table, cls.idname, cls, insert)
                 pass
             return last_id
         else:

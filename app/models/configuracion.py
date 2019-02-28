9d29f38853ec36a5a247acd8d146dba5f43c5b4d
mysitioc <mysitioc@mysitio.cl>
2019-02-28 12:02:39 -0600

    --- Auto-Git Commit ---

--- app/models/configuracion.py
+++ app/models/configuracion.py
@@ -7,25 +7,24 @@ class configuracion(base_model):
     table = 'configuracion'
 
     @classmethod
-    def getByVariable(cls, variable:str):
-        where      = {'variable' : variable}
-        condicion  = {'limit' : 1}
+    def getByVariable(cls, variable: str):
+        where = {'variable': variable}
+        condicion = {'limit': 1}
         connection = database.instance()
-        row        = connection.get(cls.table, cls.idname, where, condicion)
+        row = connection.get(cls.table, cls.idname, where, condicion)
         return row[0]['valor'] if (len(row) == 1) else False
-    
 
-    public cls function setByVariable(string variable, string valor)
-    {
-        where      = {'variable' : variable)
-        condicion  = {'limit' : 1)
+    @classmethod
+    def setByVariable(cls, variable: str, valor: str):
+        where = {'variable': variable}
+        condicion = {'limit': 1}
         connection = database.instance()
-        row        = connection.get(cls.table, cls.idname, where, condicion)
+        row = connection.get(cls.table, cls.idname, where, condicion)
 
-        if (len(row) == 0) {
-            row = self.insert({'variable' : variable, 'valor' : valor))
-        } else {
-            row = self.update({'variable' : variable, 'valor' : valor, 'id' : row[0][0]))
-        }
+        if len(row) == 0:
+            row = cls.insert({'variable': variable, 'valor': valor})
+        else:
+            row = cls.update(
+                {'variable': variable, 'valor': valor, 'id': row[0][0]})
+
         return row
-    }

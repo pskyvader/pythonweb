434519983c7cae1967f472782cc6e1bfce158210
mysitioc <mysitioc@mysitio.cl>
2019-02-28 12:01:09 -0600

    --- Auto-Git Commit ---

--- app/models/configuracion.py
+++ app/models/configuracion.py
@@ -1,3 +1,4 @@
+from core.database import database
 from .base_model import base_model
 
 
@@ -7,24 +8,24 @@ class configuracion(base_model):
 
     @classmethod
     def getByVariable(cls, variable:str):
-        $where      = array('variable' => $variable);
-        $condicion  = array('limit' => 1);
-        $connection = database::instance();
-        $row        = $connection->get(static::$table, static::$idname, $where, $condicion);
-        return (count($row) == 1) ? $row[0]['valor'] : false;
+        where      = {'variable' : variable}
+        condicion  = {'limit' : 1}
+        connection = database.instance()
+        row        = connection->get(static.table, static.idname, where, condicion)
+        return (count(row) == 1) ? row[0]['valor'] : False
     }
 
-    public static function setByVariable(string $variable, string $valor)
+    public static function setByVariable(string variable, string valor)
     {
-        $where      = array('variable' => $variable);
-        $condicion  = array('limit' => 1);
-        $connection = database::instance();
-        $row        = $connection->get(static::$table, static::$idname, $where, $condicion);
+        where      = {'variable' : variable)
+        condicion  = {'limit' : 1)
+        connection = database.instance()
+        row        = connection->get(static.table, static.idname, where, condicion)
 
-        if (count($row) == 0) {
-            $row = self::insert(array('variable' => $variable, 'valor' => $valor));
+        if (count(row) == 0) {
+            row = self.insert({'variable' : variable, 'valor' : valor))
         } else {
-            $row = self::update(array('variable' => $variable, 'valor' => $valor, 'id' => $row[0][0]));
+            row = self.update({'variable' : variable, 'valor' : valor, 'id' : row[0][0]))
         }
-        return $row;
+        return row
     }

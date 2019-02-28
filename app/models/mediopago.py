c514b24e9c7a74cf8d4c6dd7a20e9fb20306f9bf
mysitioc <mysitioc@mysitio.cl>
2019-02-28 12:21:40 -0600

    --- Auto-Git Commit ---

--- app/models/mediopago.py
+++ app/models/mediopago.py
@@ -4,3 +4,10 @@ from .base_model import base_model
 class mediopago(base_model):
     idname = 'idmediopago'
     table = 'mediopago'
+    @classmethod
+    def getById(cls,id:int):
+        where = {cls.idname : id}
+        connection = database.instance();
+        row        = connection->get(cls.table, cls.idname, where);
+        return (count(row) == 1) ? row[0] : row;
+    }

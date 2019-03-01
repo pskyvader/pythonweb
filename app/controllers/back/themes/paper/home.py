a9e64db0781aa275c0d29ce2d91809ac6b736fb0
mysitioc <mysitioc@mysitio.cl>
2019-03-01 15:35:42 -0600

    --- Auto-Git Commit ---

--- app/controllers/back/themes/paper/home.py
+++ app/controllers/back/themes/paper/home.py
@@ -15,7 +15,7 @@ class home(base):
 
     def __init__(self):
         print(self.__class__.__name__)
-        return super().__init__(self.__class__)
+        super().__init__(self.__class__.__name__)
 
     @classmethod
     def init(cls,var):

457afcb4cf1da183810de2f21aa063950c1774a5
mysitioc <mysitioc@mysitio.cl>
2019-03-01 11:41:21 -0600

    --- Auto-Git Commit ---

--- app/models/sitemap.py
+++ app/models/sitemap.py
@@ -7,3 +7,28 @@ import json
 class sitemap(base_model):
     idname = 'idsitemap'
     table = 'sitemap'
+
+    @classmethod
+    def getAll(cls, where={}, condiciones={}, select=""):
+        return_total = None
+        connection = database.instance()
+        if select == 'total':
+            return_total = True
+
+        row = connection.get(cls.table, cls.idname, where, condiciones, select)
+
+        if return_total != None:
+            return len(row)
+        else:
+            return row
+
+    @classmethod
+    def truncate(cls):
+        respuesta = {'exito': True, 'mensaje': []}
+
+        connection = database.instance()
+        respuesta['exito'] = connection.truncate([cls.table])
+        if not respuesta['exito']:
+            respuesta['mensaje'] = 'Error al vaciar tablas'
+
+        return respuesta

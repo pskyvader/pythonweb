00ee2c46d1e77e3b776211bc5072cfed9d5ecf8a
mysitioc <mysitioc@mysitio.cl>
2019-03-01 14:07:25 -0600

    --- Auto-Git Commit ---

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -8,7 +8,7 @@ sys.path.insert(0, os.path.dirname(__file__))
 
 def application2(environ, start_response):
     from datetime import datetime
-    init_time = datetime.now()
+    #init_time = datetime.now()
     app_web = app(os.path.dirname(__file__))
     main_data = app_web.init(environ)
     ret = main_data['response_body']

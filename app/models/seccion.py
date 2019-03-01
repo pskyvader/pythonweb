c51b62d5b639ee83fc136fe7cf7ffcaca3a99226
mysitioc <mysitioc@mysitio.cl>
2019-03-01 11:23:50 -0600

    --- Auto-Git Commit ---

--- app/models/seccion.py
+++ app/models/seccion.py
@@ -1,5 +1,6 @@
 from .base_model import base_model
 from core.database import database
+import json
 
 
 class seccion(base_model):
@@ -9,7 +10,7 @@ class seccion(base_model):
     @classmethod
     def getAll(cls, where={}, condiciones={}, select=""):
         limit = None
-        idpadre = None
+        idseccioncategoria = None
         return_total = None
         connection = database.instance()
         # fields     = table.getByname(cls.table)
@@ -17,9 +18,9 @@ class seccion(base_model):
         if 'estado' not in where and app.front and 'estado' in fields:
             where['estado'] = True
 
-        if 'idpadre' in where:
-            if 'idpadre' in fields:
-                idpadre = where['idpadre']
+        if 'idseccioncategoria' in where:
+            if 'idseccioncategoria' in fields:
+                idseccioncategoria = where['idseccioncategoria']
                 if 'limit' in condiciones:
                     limit = condiciones['limit']
                     limit2 = 0
@@ -30,7 +31,7 @@ class seccion(base_model):
                         limit = 0
                     limit2 = condiciones['limit2']
                     del condiciones['limit2']
-            del where['idpadre']
+            del where['idseccioncategoria']
 
         if 'order' not in condiciones and 'orden' in fields:
             condiciones['order'] = 'orden ASC'
@@ -57,16 +58,16 @@ class seccion(base_model):
 
         if select == 'total':
             return_total = True
-            if idpadre != None:
+            if idseccioncategoria != None:
                 select = ''
 
         row = connection.get(cls.table, cls.idname, where, condiciones, select)
         deleted = False
         for r in row:
             deleted = False
-            if 'idpadre' in r:
-                r['idpadre'] = json.loads(r['idpadre'])
-                if idpadre != None and idpadre not in r['idpadre']:
+            if 'idseccioncategoria' in r:
+                r['idseccioncategoria'] = json.loads(r['idseccioncategoria'])
+                if idseccioncategoria != None and idseccioncategoria not in r['idseccioncategoria']:
                     deleted = True
                     del r
 
@@ -92,148 +93,62 @@ class seccion(base_model):
 
 
 
+    @classmethod
+    def getById(cls, id: int):
+        where = {cls.idname: id}
+        if app.front:
+            # fields     = table.getByname(cls.table)
+            fields = {}
+            if 'estado' in fields:
+                where['estado'] = True
 
-public static function getAll(array $where = array(), array $condiciones = array(), string $select = "")
-    {
-        $connection = database::instance();
-        if (!isset($where['estado']) && app::$_front) {
-            $where['estado'] = true;
-        }
-        if (isset($where['idseccioncategoria'])) {
-            $idseccioncategoria = $where['idseccioncategoria'];
-            unset($where['idseccioncategoria']);
-            if(isset($condiciones['limit'])){
-                $limit=$condiciones['limit'];
-                $limit2=0;
-                unset($condiciones['limit']);
-            }
-            if(isset($condiciones['limit2'])){
-                if(!isset($limit)) $limit=0;
-                $limit2=$condiciones['limit2'];
-                unset($condiciones['limit2']);
-            }
-        }
+        connection = database.instance()
+        row = connection.get(cls.table, cls.idname, where)
+        if len(row) == 1:
+            row[0]['idseccioncategoria'] = json.loads(row[0]['idseccioncategoria'])
+            if 'foto' in row[0]:
+                row[0]['foto'] = json.loads(row[0]['foto'])
+            if 'archivo' in row[0]:
+                row[0]['archivo'] = json.loads(row[0]['archivo'])
+        return row[0] if len(row) == 1 else row
 
-        if (!isset($condiciones['order'])) {
-            $condiciones['order'] = 'orden ASC';
-        }
 
-        if (isset($condiciones['palabra'])) {
-            $fields = table::getByname(static::$table);
-            $condiciones['buscar'] = array();
-            if (isset($fields['titulo'])) {
-                $condiciones['buscar']['titulo'] = $condiciones['palabra'];
-            }
+    @classmethod
+    def copy(cls, id: int, loggging=True):
+        from .log import log
+        from core.image import image
+        row = cls.getById(id)
 
-            if (isset($fields['keywords'])) {
-                $condiciones['buscar']['keywords'] = $condiciones['palabra'];
-            }
+        if 'foto' in row:
+            foto_copy = row['foto']
+            del row['foto']
+        else:
+            foto_copy = None
 
-            if (isset($fields['descripcion'])) {
-                $condiciones['buscar']['descripcion'] = $condiciones['palabra'];
-            }
+        if 'archivo' in row:
+            del row['archivo']
+        row['idseccioncategoria']=json.dumps(row['idseccioncategoria'])
+        # fields     = table.getByname(cls.table)
+        fields = {}
+        insert = database.create_data(fields, row)
+        connection = database.instance()
+        row = connection.insert(cls.table, cls.idname, insert)
+        if isinstance(row, int) and row > 0:
+            last_id = row
+            if foto_copy != None:
+                new_fotos = []
+                for foto in foto_copy:
+                    copiar = image.copy(
+                        foto, last_id, foto['folder'], foto['subfolder'], last_id, '')
+                    new_fotos.append(copiar['file'][0])
+                    image.regenerar(copiar['file'][0])
 
-            if (isset($fields['metadescripcion'])) {
-                $condiciones['buscar']['metadescripcion'] = $condiciones['palabra'];
-            }
+                update = {'id': last_id, 'foto': json.dumps(new_fotos)}
+                cls.update(update)
 
-        }
-
-        if($select=='total'){
-            $return_total=true;
-            if(isset($idseccioncategoria)){
-                $select='';
-            }
-        }
-        $row = $connection->get(static::$table, static::$idname, $where, $condiciones, $select);
-            foreach ($row as $key => $value) {
-                if(isset($row[$key]['idseccioncategoria'])){
-                    $row[$key]['idseccioncategoria'] = functions::decode_json($row[$key]['idseccioncategoria']);
-                    if (isset($idseccioncategoria) && !in_array($idseccioncategoria, $row[$key]['idseccioncategoria'])) {
-                        unset($row[$key]);
-                    }
-                }
-                if (isset($row[$key]) && isset($row[$key]['foto'])) {
-                    $row[$key]['foto'] = functions::decode_json($row[$key]['foto']);
-                }
-                if (isset($row[$key]) && isset($row[$key]['archivo'])) {
-                    $row[$key]['archivo'] = functions::decode_json($row[$key]['archivo']);
-                }
-        }
-        if (isset($idseccioncategoria)) {
-            $row = array_values($row);
-        }
-        if(isset($limit)){
-            if($limit2==0){
-                $row=array_slice($row,$limit2,$limit);
-            }else{
-                $row=array_slice($row,$limit,$limit2);
-            }
-        }
-        if(isset($return_total)){
-            return count($row);
-        }
-        return $row;
-    }
-
-    public static function getById(int $id)
-    {
-        $where = array(static::$idname => $id);
-        if (app::$_front) {
-            $fields = table::getByname(static::$table);
-            if (isset($fields['estado'])) {
-                $where['estado'] = true;
-            }
-
-        }
-        $connection = database::instance();
-        $row = $connection->get(static::$table, static::$idname, $where);
-        if (count($row) == 1) {
-            $row[0]['idseccioncategoria'] = functions::decode_json($row[0]['idseccioncategoria']);
-            if (isset($idseccioncategoria) && !in_array($idseccioncategoria, $row[0]['idseccioncategoria'])) {
-                unset($row[0]);
-            }
-            if (isset($row[0]) && isset($row[0]['foto'])) {
-                $row[0]['foto'] = functions::decode_json($row[0]['foto']);
-            }
-            if (isset($row[0]) && isset($row[0]['archivo'])) {
-                $row[0]['archivo'] = functions::decode_json($row[0]['archivo']);
-            }
-        }
-        return (count($row) == 1) ? $row[0] : $row;
-    }
-
-    public static function copy(int $id)
-    {
-        $row = static::getById($id);
-        if (isset($row['foto'])) {
-            $foto_copy=$row['foto'];
-            unset($row['foto']);
-        }
-        if (isset($row['archivo'])) {
-            unset($row['archivo']);
-        }
-        $row['idseccioncategoria']=functions::encode_json($row['idseccioncategoria']);
-        $fields = table::getByname(static::$table);
-        $insert = database::create_data($fields, $row);
-        $connection = database::instance();
-        $row = $connection->insert(static::$table, static::$idname, $insert);
-        if (is_int($row) && $row>0) {
-            $last_id = $row;
-            if(isset($foto_copy)){
-                $new_fotos=array();
-                foreach ($foto_copy as $key => $foto) {
-                    $copiar = image::copy($foto, $last_id, $foto['folder'], $foto['subfolder'], $last_id, '');
-                    $new_fotos[]=$copiar['file'][0];
-                    image::regenerar($copiar['file'][0]);
-                }
-                $update=array('id'=>$last_id,'foto'=>functions::encode_json($new_fotos));
-                static::update($update);
-            }
-            log::insert_log(static::$table, static::$idname, __FUNCTION__, $insert);
-            return $last_id;
-        } else {
-            return $row;
-        }
-    }
-
+            if loggging:
+                log.insert_log(cls.table, cls.idname, cls, (insert))
+                pass
+            return last_id
+        else:
+            return row

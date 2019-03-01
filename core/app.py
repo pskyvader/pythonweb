c2b6dfbc12688185e98b29e1a9edb914fe45f53b
mysitioc <mysitioc@mysitio.cl>
2019-03-01 15:18:26 -0600

    --- Auto-Git Commit ---

--- core/app.py
+++ core/app.py
@@ -85,7 +85,6 @@ class app:
         controller = app.controller_dir + url[0]
         my_file = Path(app.root + controller + '.py')
         if my_file.is_file():
-            print(controller.replace("/", ".")+'.'+url[0])
             current_module = importlib.import_module(controller.replace("/", "."))
             del url[0]
             print(current_module)

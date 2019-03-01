ddddfcac388019d07bc16eba270a7cc017459183
mysitioc <mysitioc@mysitio.cl>
2019-03-01 14:08:55 -0600

    --- Auto-Git Commit ---

--- app/controllers/back/themes/paper/base.py
+++ app/controllers/back/themes/paper/base.py
@@ -1,6 +1,7 @@
 from core.app import app
 from app.models.moduloconfiguracion import moduloconfiguracion as moduloconfiguracion_model
 from app.models.modulo import modulo as modulo_model
+import importlib
 
 class base:
     url            = []

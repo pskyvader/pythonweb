7bd2bf700ef0d4f4b416b6bc14860696ed211bd8
mysitioc <mysitioc@mysitio.cl>
2019-03-01 16:25:21 -0600

    --- Auto-Git Commit ---

--- app/controllers/back/themes/paper/login.py
+++ app/controllers/back/themes/paper/login.py
@@ -4,26 +4,10 @@ from core.view import view
 from app.models.administrador import administrador as administrador_model
 from .head import head
 from .footer import footer
+from .base import base
 
 
-def init(var):
-    h = login()
-    if len(var) > 0:
-        if hasattr(h, var[0]) and callable(getattr(h, var[0])):
-            fun = var[0]
-            del var[0]
-            method = getattr(h, fun)
-            ret = method(var)
-        else:
-            ret = {
-                'error': 404,
-            }
-    else:
-        ret = h.index([])
-    return ret
-
-
-class login:
+class login(base):
     url = ['login', 'index']
     metadata = {'title': 'login', 'modulo': 'login'}
 

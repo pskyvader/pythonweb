703e1bc3d6810d7f51634ff258c41a36fed69cce
mysitioc <mysitioc@mysitio.cl>
2019-03-01 15:21:27 -0600

    --- Auto-Git Commit ---

--- core/app.py
+++ core/app.py
@@ -86,9 +86,8 @@ class app:
         my_file = Path(app.root + controller + '.py')
         if my_file.is_file():
             current_module = importlib.import_module(controller.replace("/", "."))
+            current_module = getattr(current_module, url[0])
             del url[0]
-            print(current_module)
-            current_module=current_module.home()
             # returns {'body':str,'headers':str} or {'error':int,...'redirect':str}
             response = current_module.init(url)
         else:

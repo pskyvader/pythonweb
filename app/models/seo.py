b302502f9d14edea43aeead48edc719a1421c59b
mysitioc <mysitioc@mysitio.cl>
2019-03-01 11:34:05 -0600

    --- Auto-Git Commit ---

--- app/models/seo.py
+++ app/models/seo.py
@@ -4,3 +4,207 @@ from .base_model import base_model
 class seo(base_model):
     idname = 'idseo'
     table = 'seo'
+
+    @classmethod
+    def getAll(cls, where={}, condiciones={}, select=""):
+        limit = None
+        idpadre = None
+        return_total = None
+        connection = database.instance()
+        # fields     = table.getByname(cls.table)
+        fields = {}
+        if 'estado' not in where and app.front and 'estado' in fields:
+            where['estado'] = True
+
+        if 'idpadre' in where:
+            if 'idpadre' in fields:
+                idpadre = where['idpadre']
+                if 'limit' in condiciones:
+                    limit = condiciones['limit']
+                    limit2 = 0
+                    del condiciones['limit']
+
+                if 'limit2' in condiciones:
+                    if limit == None:
+                        limit = 0
+                    limit2 = condiciones['limit2']
+                    del condiciones['limit2']
+            del where['idpadre']
+
+        if 'order' not in condiciones and 'orden' in fields:
+            condiciones['order'] = 'orden ASC'
+
+        if 'palabra' in condiciones:
+            condiciones['buscar'] = {}
+            if 'titulo' in fields:
+                condiciones['buscar']['titulo'] = condiciones['palabra']
+
+            if 'keywords' in fields:
+                condiciones['buscar']['keywords'] = condiciones['palabra']
+
+            if 'descripcion' in fields:
+                condiciones['buscar']['descripcion'] = condiciones['palabra']
+
+            if 'metadescripcion' in fields:
+                condiciones['buscar']['metadescripcion'] = condiciones['palabra']
+
+            if 'cookie_pedido' in fields:
+                condiciones['buscar']['cookie_pedido'] = condiciones['palabra']
+
+            if len(condiciones['buscar']) == 0:
+                del condiciones['buscar']
+
+        if select == 'total':
+            return_total = True
+            if idpadre != None:
+                select = ''
+
+        row = connection.get(cls.table, cls.idname, where, condiciones, select)
+        deleted = False
+        for r in row:
+            deleted = False
+            if 'idpadre' in r:
+                r['idpadre'] = json.loads(r['idpadre'])
+                if idpadre != None and idpadre not in r['idpadre']:
+                    deleted = True
+                    del r
+
+            if return_total == None:
+                if not deleted and 'foto' in r:
+                    r['foto'] = json.loads(r['foto'])
+                if not deleted and 'banner' in r:
+                    r['banner'] = json.loads(r['banner'])
+                if not deleted and 'archivo' in r:
+                    r['archivo'] = json.loads(r['archivo'])
+
+        if limit != None:
+            if limit2 == 0:
+                row = row[0:limit]
+            else:
+                row = row[limit:limit2+1]
+
+        if return_total != None:
+            return len(row)
+        else:
+            return row
+
+
+
+    public static function getAll(array $where = array(),array  $condiciones = array(), string $select = "")
+    {
+        $connection = database::instance();
+        if (!isset($where['estado']) && app::$_front) {
+            $where['estado'] = true;
+        }
+
+        if (!isset($condiciones['order'])) {
+            $condiciones['order'] = 'orden ASC';
+        }
+
+        if (isset($condiciones['palabra'])) {
+            $fields = table::getByname(static::$table);
+            $condiciones['buscar'] = array();
+            if (isset($fields['titulo'])) {
+                $condiciones['buscar']['titulo'] = $condiciones['palabra'];
+            }
+
+            if (isset($fields['keywords'])) {
+                $condiciones['buscar']['keywords'] = $condiciones['palabra'];
+            }
+
+            if (isset($fields['descripcion'])) {
+                $condiciones['buscar']['descripcion'] = $condiciones['palabra'];
+            }
+
+            if (isset($fields['metadescripcion'])) {
+                $condiciones['buscar']['metadescripcion'] = $condiciones['palabra'];
+            }
+
+        }
+
+        if($select=='total'){
+            $return_total=true;
+        }
+        $row = $connection->get(static::$table, static::$idname, $where, $condiciones, $select);
+        if ($select == '') {
+            foreach ($row as $key => $value) {
+                if (isset($row[$key]['foto'])) {
+                    $row[$key]['foto'] = functions::decode_json($row[$key]['foto']);
+                }
+                if (isset($row[$key]['banner'])) {
+                    $row[$key]['banner'] = functions::decode_json($row[$key]['banner']);
+                }
+            }
+        }
+        if(isset($return_total)){
+            return count($row);
+        }
+        return $row;
+    }
+
+    public static function getById(int $id)
+    {
+        $where = array(static::$idname => $id);
+        if (app::$_front) {
+            $fields = table::getByname(static::$table);
+            if(isset($fields['estado'])) $where['estado'] = true;
+        }
+        $connection = database::instance();
+        $row = $connection->get(static::$table, static::$idname, $where);
+        if (count($row) == 1) {
+            if (isset($row[0]['foto'])) {
+                $row[0]['foto'] = functions::decode_json($row[0]['foto']);
+            }
+            if (isset($row[0]['banner'])) {
+                $row[0]['banner'] = functions::decode_json($row[0]['banner']);
+            }
+        }
+        return (count($row) == 1) ? $row[0] : $row;
+    }
+
+    public static function copy(int $id)
+    {
+        $row = static::getById($id);
+        if (isset($row['banner'])) {
+            $banner_copy=$row['banner'];
+            unset($row['banner']);
+        }
+        if (isset($row['foto'])) {
+            $foto_copy=$row['foto'];
+            unset($row['foto']);
+        }
+        if (isset($row['archivo'])) {
+            unset($row['archivo']);
+        }
+        $fields     = table::getByname(static::$table);
+        $insert     = database::create_data($fields, $row);
+        $connection = database::instance();
+        $row        = $connection->insert(static::$table, static::$idname, $insert);
+        if (is_int($row) && $row>0) {
+            $last_id = $row;
+            if(isset($foto_copy)){
+                $new_fotos=array();
+                foreach ($foto_copy as $key => $foto) {
+                    $copiar = image::copy($foto, $last_id, $foto['folder'], $foto['subfolder'], $last_id, '');
+                    $new_fotos[]=$copiar['file'][0];
+                    image::regenerar($copiar['file'][0]);
+                }
+                $update=array('id'=>$last_id,'foto'=>functions::encode_json($new_fotos));
+                static::update($update);
+            }
+            if(isset($banner_copy)){
+                $new_banners=array();
+                foreach ($banner_copy as $key => $banner) {
+                    $copiar = image::copy($banner, $last_id, $banner['folder'], $banner['subfolder'], $last_id, '');
+                    $new_banners[]=$copiar['file'][0];
+                    image::regenerar($copiar['file'][0]);
+                }
+                $update=array('id'=>$last_id,'banner'=>functions::encode_json($new_banners));
+                static::update($update);
+            }
+            log::insert_log(static::$table, static::$idname, __FUNCTION__, $insert);
+            return $last_id;
+        } else {
+            return $row;
+        }
+    }

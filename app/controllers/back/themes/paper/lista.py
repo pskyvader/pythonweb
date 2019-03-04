6f35f46a69908733158953d8b0c6321fbb857171
mysitioc <mysitioc@mysitio.cl>
2019-03-04 16:42:50 -0600

    --- Auto-Git Commit ---

--- app/controllers/back/themes/paper/lista.py
+++ app/controllers/back/themes/paper/lista.py
@@ -27,7 +27,6 @@ class lista:
         for fila in row_data:
             td = []
             for v in th:
-                print(v,fila)
                 content = self.field(v, fila)
                 td.append({'content': content, 'content_field': v['field']})
 

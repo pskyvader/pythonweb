eb58f86e394c13773a249e9533091c84cee6938e
mysitioc <mysitioc@mysitio.cl>
2019-03-04 13:16:31 -0600

    --- Auto-Git Commit ---

--- app/controllers/back/themes/paper/lista.py
+++ app/controllers/back/themes/paper/lista.py
@@ -1,51 +1,56 @@
 from core.view import view
 from .head import head
-class lista:
-    metadata  = {'title' : ''}
+from .header import header
+from .aside import aside
+from .footer import footer
 
-    def __init__(self,metadata:dict):
-        for key,value in metadata.items():
+
+class lista:
+    metadata = {'title': ''}
+
+    def __init__(self, metadata: dict):
+        for key, value in metadata.items():
             self.metadata[key] = value
 
-
-    def normal(self,data:dict):
-        ret={'body':''}
-        th       = data['th']
+    def normal(self, data: dict):
+        ret = {'body': ''}
+        th = data['th']
         row_data = data['row']
-        row      = []
-        even     = False
+        row = []
+        even = False
         for fila in row_data:
             td = []
             for v in th:
                 content = self.field(v, fila)
-                td.append({'content' : content, 'content_field' : v['field']})
-            
-            linea = {'even' : even, 'id' : fila[0], 'td' : td, 'order' : fila['orden'] if 'orden' in fila else ''}
+                td.append({'content': content, 'content_field': v['field']})
+
+            linea = {'even': even, 'id': fila[0], 'td': td,
+                     'order': fila['orden'] if 'orden' in fila else ''}
             row.append(linea)
-            even  = not even
-        
-        data['row']         = row
-        data['title']       = self.metadata['title']
+            even = not even
+
+        data['row'] = row
+        data['title'] = self.metadata['title']
         data['is_order'] = 'orden' in th
 
         data = self.pagination(data)
 
         data['delete'] = 'delete' in th
 
-        h = head(cls.metadata)
-        ret_head=h.normal()
-        if ret_head['headers']!='':
+        h = head(self.metadata)
+        ret_head = h.normal()
+        if ret_head['headers'] != '':
             return ret_head
-        ret['body']+=ret_head['body']
+        ret['body'] += ret_head['body']
 
-        header = new header()
-        header->normal()
-        aside = new aside()
-        aside->normal()
+        he = header()
+        ret['body'] += he.normal()['body']
 
-        view::set_array(data)
-        view::render('list')
+        asi = aside()
+        ret['body'] += asi.normal()['body']
 
-        footer = new footer()
-        footer->normal()
-    }
+        view.set_array(data)
+        view.render('list')
+
+        f = footer()
+        ret['body'] += f.normal()['body']

c7fc620d8198696e68dafd856f82cf1dc5b87f9e
mysitioc <mysitioc@mysitio.cl>
2019-03-05 14:46:41 -0600

    --- Auto-Git Commit ---

--- core/app.py
+++ core/app.py
@@ -184,22 +184,6 @@ class app:
         for key in p.keys():
             post[key] = p[key].value
         return post
-    @staticmethod
-    def get_post_form(environ):
-        from cgi import FieldStorage
-        input = environ['wsgi.input']
-        post_form = environ.get('wsgi.post_form')
-        if (post_form is not None and post_form[0] is input):
-            return post_form[2]
-        # This must be done to avoid a bug in cgi.FieldStorage
-        environ.setdefault('QUERY_STRING', '')
-        fs = FieldStorage(fp=input, environ=environ, keep_blank_values=1)
-        new_input = InputProcessed()
-        post_form = (new_input, input, fs)
-        environ['wsgi.post_form'] = post_form
-        environ['wsgi.input'] = new_input
-        return fs
-
     
 
     @staticmethod
@@ -227,9 +211,3 @@ class app:
             return app.url['base']
         else:
             return app.url['admin']
-
-
-class InputProcessed(object):
-    def read(self, *args):
-        raise EOFError('The wsgi.input stream has already been consumed')
-    readline = readlines = __iter__ = read

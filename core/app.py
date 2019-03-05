94c4a22f9dcdd5faf3524e3296b9451546bf015d
mysitioc <mysitioc@mysitio.cl>
2019-03-05 16:42:59 -0600

    --- Auto-Git Commit ---

--- core/app.py
+++ core/app.py
@@ -222,11 +222,27 @@ class app:
                 aux[final_key]=i
                 del var[k]
 
-
-        print('original',var,'aux',aux)
-        var.update(aux)
+        var.dict_merge(aux)
         return var
 
+
+    @staticmethod
+    def dict_merge(dct, merge_dct):
+        import collections
+        """ Recursive dict merge. Inspired by :meth:``dict.update()``, instead of
+        updating only top-level keys, dict_merge recurses down into dicts nested
+        to an arbitrary depth, updating keys. The ``merge_dct`` is merged into
+        ``dct``.
+        :param dct: dict onto which the merge is executed
+        :param merge_dct: dct merged into dct
+        :return: None
+        """
+        for k, v in merge_dct.iteritems():
+            if (k in dct and isinstance(dct[k], dict)
+                    and isinstance(merge_dct[k], collections.Mapping)):
+                app.dict_merge(dct[k], merge_dct[k])
+            else:
+                dct[k] = merge_dct[k]
 
     @staticmethod
     def parse_session():

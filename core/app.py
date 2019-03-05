06699891d5c7bffbda6fb2eda2c78aacd715954e
mysitioc <mysitioc@mysitio.cl>
2019-03-05 14:16:42 -0600

    --- Auto-Git Commit ---

--- core/app.py
+++ core/app.py
@@ -169,13 +169,18 @@ class app:
     def parse_post():
         from cgi import FieldStorage
         from cgi import parse_qs, escape
+
+        
         try:
             request_body_size = int(app.environ.get('CONTENT_LENGTH', 0))
         except (ValueError):
             request_body_size = 0
+        
         request_body = app.environ['wsgi.input'].read(request_body_size)
         d = parse_qs(request_body)
         print(d)
+
+
         post_env = app.environ.copy()
         post_env['QUERY_STRING'] = ''
         p = FieldStorage(

23a106eead92c8076bc07fb3ce9e934864783fcc
mysitioc <mysitioc@mysitio.cl>
2019-03-05 16:05:43 -0600

    --- Auto-Git Commit ---

--- core/app.py
+++ core/app.py
@@ -183,23 +183,34 @@ class app:
         return post
 
     @staticmethod
-    def format_array(var:dict,aux={}):
+    def format_array(var:dict):
         var_copy=var.copy()
+        aux={}
         for k,i in var_copy.items():
             #si existe simbolo de array
             if "[" in k:
                 #separar key principal de key dentro de array
-                final_key,rest=str(k).split('[',2)
+                final_key,rest=k.split('[',2)
                 if rest!='':
                     if final_key not in aux:
                         aux[final_key]={}
                     #comprobar si existe simbolo de cerrado, sino se guarda directamente
                     if rest.find(']')==-1:
                         aux[final_key][rest]=i
+                        aux[final_key]=app.format_array(aux[final_key])
                     #comprobar si existe mas de un valor en sub key, sino se recupera el primer y unico valor
                     elif rest.find('[')==-1:
                         rest=str(rest).split(']',2)[0]
                         aux[final_key][rest]=i
+                        aux[final_key]=app.format_array(aux[final_key])
+                    else:
+                        if rest.find(']')<rest.find('['):
+                            rest1,rest2=str(rest).split(']',2)
+                            aux[final_key][rest1][rest2]=i
+                            aux[final_key]=app.format_array(aux[final_key][rest1])
+                        else:
+                            print('error de formato, formato aceptado: a[b][c][d]')
+
                 else:
                     aux[final_key]=i
                 del var[k]

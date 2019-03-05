205482f690c3c89f5700abe6cee9199303bc8661
mysitioc <mysitioc@mysitio.cl>
2019-03-05 16:19:43 -0600

    --- Auto-Git Commit ---

--- core/app.py
+++ core/app.py
@@ -208,6 +208,8 @@ class app:
                     else:
                         if rest.find(']')<rest.find('['):
                             rest1,rest2=str(rest).split(']',1)
+                            if rest1 not in aux[final_key]:
+                                aux[final_key][rest1]={}
                             aux[final_key][rest1][rest2]=i
                             aux[final_key][rest1]=app.format_array(aux[final_key][rest1])
                         else:

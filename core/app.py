6997198a9ea1be630fc9bc2320262a3abd43e56c
mysitioc <mysitioc@mysitio.cl>
2019-03-05 14:58:41 -0600

    --- Auto-Git Commit ---

--- core/app.py
+++ core/app.py
@@ -177,7 +177,7 @@ class app:
         post = {}
         for key in p.keys():
             value=p[key].value
-            print(json.load(value))
+            print(json.loads(value))
             post[key] = p[key].value
             
         return post

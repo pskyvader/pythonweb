d53b31d54070a77c02116e8cc8c34aa98cb51a34
mysitioc <mysitioc@mysitio.cl>
2019-03-05 16:58:45 -0600

    --- Auto-Git Commit ---

--- core/app.py
+++ core/app.py
@@ -180,6 +180,8 @@ class app:
             post[key] = p[key].value
         post['prueba[a][b]']='[{"aaa":"bbb","c":1}]'
         post=app.format_array(post)
+        post=json.dumps(post)
+        post=json.loads(post)
         print(post)
         return post
 

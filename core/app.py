9622e36ef9b2bf12ef1e652b506abae6d9346324
mysitioc <mysitioc@mysitio.cl>
2019-03-05 16:31:29 -0600

    --- Auto-Git Commit ---

--- core/app.py
+++ core/app.py
@@ -177,8 +177,8 @@ class app:
         post = {}
         for key in p.keys():
             post[key] = p[key].value
-        post['prueba[a][b][c][d][a]']=1
-        post['prueba[a][b][c][d][b]']=1
+        post['prueba[a][b][c][d][1]']=1
+        post['prueba[a][b][c][d][2]']=1
         print(post)
         post=app.format_array(post)
         print(post)

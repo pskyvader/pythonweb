4fb4a807d23a68515307a66ba66b31e81c34b0cb
mysitioc <mysitioc@mysitio.cl>
2019-03-06 16:01:26 -0600

    --- Auto-Git Commit ---

--- app/controllers/back/themes/paper/base.py
+++ app/controllers/back/themes/paper/base.py
@@ -38,7 +38,7 @@ class base:
             method = getattr(cls, fun)
             sig = signature(method)
             params = sig.parameters
-            print(params,inspect.getargspec(method))
+            print(params)
             if len(params) >= 2:
                 ret = method(var)
             elif len(params) == 1:

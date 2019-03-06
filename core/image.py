887fb9eb28348e09f3c3d45f7cf3445854981e66
mysitioc <mysitioc@mysitio.cl>
2019-03-06 12:24:05 -0600

    --- Auto-Git Commit ---

--- core/image.py
+++ core/image.py
@@ -8,8 +8,8 @@ from .functions import functions
 class image:
     types = ["image/webp", "image/bmp", "image/gif", "image/pjpeg", "image/jpeg", "image/svg+xml", "image/png", "video/webm", "video/mp4", "application/zip", "application/x-zip-compressed", "application/octet-stream", "application/postscript", "application/msword", "application/vnd.ms-excel", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "application/vnd.openxmlformats-officedocument.spreadsheetml.template", "application/vnd.openxmlformats-officedocument.presentationml.template",
              "application/vnd.openxmlformats-officedocument.presentationml.slideshow", "application/vnd.openxmlformats-officedocument.presentationml.presentation", "application/vnd.openxmlformats-officedocument.presentationml.slide", "application/vnd.openxmlformats-officedocument.wordprocessingml.document", "application/vnd.openxmlformats-officedocument.wordprocessingml.template", "application/vnd.ms-excel.addin.macroEnabled.12", "application/vnd.ms-excel.sheet.binary.macroEnabled.12", "application/pdf", "application/download"]
-    extensions = ["webp", "bmp", "ico", "gif", "jpeg", "jpg", "svg", "xml", "png", "webm", "mp4", "zip", "doc",
-                  "docx", "dotx", "xls", "xlsx", "xltx", "xlam", "xlsb", "ppt", "pptx", "potx", "ppsx", "sldx", "pdf"]
+    extensions = [".webp", ".bmp", ".ico", ".gif", ".jpeg", ".jpg", ".svg", ".xml", ".png", ".webm", ".mp4", ".zip", ".doc",
+                  ".docx", ".dotx", ".xls", ".xlsx", ".xltx", ".xlam", ".xlsb", ".ppt", ".pptx", ".potx", ".ppsx", ".sldx", ".pdf"]
     upload_dir = ''
     upload_url = ''
 

8cecc0a622dfbe328ed4153aebc9646e06f87031
mysitioc <mysitioc@mysitio.cl>
2019-03-06 15:40:10 -0600

    --- Auto-Git Commit ---

--- app/controllers/back/themes/paper/backup.py
+++ app/controllers/back/themes/paper/backup.py
@@ -233,6 +233,7 @@ class backup(base):
 
     def vaciar_log(self):
         ret = {'body': ''}
+        print(self)
         os.remove(self.dir_backup + '/' + self.archivo_log)
         ret['body'] = "'True'"
         return ret

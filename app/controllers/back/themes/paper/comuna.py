bea9a58e261ef8d6784bae2c1079a5f42d8266c3
mysitioc <mysitioc@mysitio.cl>
2019-03-07 14:30:21 -0600

    --- Auto-Git Commit ---

--- app/controllers/back/themes/paper/comuna.py
+++ app/controllers/back/themes/paper/comuna.py
@@ -5,14 +5,14 @@ from app.models.comuna import comuna as comuna_model
 from app.models.administrador import administrador as administrador_model
 
 #from .detalle import detalle as detalle_class
-#from .lista import lista as lista_class
+from .lista import lista as lista_class
 #from .head import head
 #from .header import header
 #from .aside import aside
 #from .footer import footer
 
 from core.app import app
-#from core.functions import functions
+from core.functions import functions
 #from core.image import image
 
 class comuna(base):

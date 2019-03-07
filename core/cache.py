d9c758cb51ebbd02820926296a9166da493dbc03
mysitioc <mysitioc@mysitio.cl>
2019-03-07 13:38:20 -0600

    --- Auto-Git Commit ---

--- core/cache.py
+++ core/cache.py
@@ -68,6 +68,7 @@ class cache:
             if access(folder, W_OK):
                 name = cache.file_name(url)
                 if name!='':
+                    print(cache.data)
                     f = ''.join(cache.data)
                     f = bytes(f, 'utf-8')
                     f = compress(f)

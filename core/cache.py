7f7cd7e2d9108c46b1ece272da183d35deaa5f27
mysitioc <mysitioc@mysitio.cl>
2019-03-07 13:02:49 -0600

    --- Auto-Git Commit ---

--- core/cache.py
+++ core/cache.py
@@ -44,10 +44,61 @@ class cache:
                 
 
         if app.front and cache.cacheable:
-            directory  = app.get_dir(True) + 'cache/'
-            name = cache.file_name(directory)
-            my_file = Path(directory+name)
+            folder  = app.get_dir(True) + 'cache/'
+            name = cache.file_name(folder)
+            my_file = Path(folder+name)
             if my_file.is_file():
                 return my_file
         
         return ""
+
+
+    @staticmethod
+    def save_cache(url:list):
+        from .app import app
+        from .functions import functions
+        from os import W_OK
+        from os import access
+        ruta    = functions.generar_url(url)
+        current = functions.current_url()
+        if ruta == current and app.front and cache.cacheable:
+            folder = app.get_dir(True) + 'cache/'
+            if access(folder, W_OK):
+            if (is_writable(folder):
+                name = cache.file_name(url)
+                if ('' != name:
+                    file_put_contents(folder . name, implode('', cache.data))
+                }
+            }
+        }
+    }
+
+    @staticmethod
+    def file_name(url:list)
+    {
+        if (!isset(url['url']):
+            return ""
+        }
+
+        name = str_replace('/', '-', url['url'])
+        unset(url['url'])
+        n = explode('.', name, 2)
+        if (isset(n[1]):
+            return ""
+        }
+        foreach (url as key => u:
+            n = "__" . key . "-" . u
+            n = functions.url_amigable(n)
+            name .= n
+        }
+        post = _POST
+        if (isset(post['ajax']):
+            name .= '__ajax'
+            unset(post['ajax'])
+        }
+        if (count(post) > 0:
+            return ""
+        }
+
+        return name
+    }

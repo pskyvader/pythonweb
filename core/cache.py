0ba5281cb72319c9239861cb36c289fdf997b4f1
mysitioc <mysitioc@mysitio.cl>
2019-03-07 12:59:04 -0600

    --- Auto-Git Commit ---

--- core/cache.py
+++ core/cache.py
@@ -29,6 +29,7 @@ class cache:
     def get_cache(url:list):
         from .app import app
         from .functions import functions
+        from pathlib import Path
         ruta    = functions.generar_url(url)
         current = functions.current_url()
         if ruta != current:
@@ -36,20 +37,17 @@ class cache:
         
 
         if cache.cacheable_config==None:
-            config                 = app.getConfig()
-            self.cacheable_config = (isset(config['cache']) ? config['cache'] : true)
-            if (!self.cacheable_config) {
-                self.cacheable = false
-            }
-        }
+            config                 = app.get_config()
+            cache.cacheable_config = config['cache'] if 'cache' in config else True
+            if not cache.cacheable_config:
+                cache.cacheable = False
+                
 
-        if (app._front && self.cacheable) {
-            dir  = app.get_dir(true) . 'cache/'
-            name = self.file_name(url)
-            if ("" != name && file_exists(dir . name)) {
-                return file_get_contents(dir . name)
-            } else {
-                return ""
-            }
-        }
-    }
+        if app.front and cache.cacheable:
+            directory  = app.get_dir(True) + 'cache/'
+            name = cache.file_name(url)
+            my_file = Path(name)
+            if my_file.is_file():
+                return my_file
+        
+        return ""

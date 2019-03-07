8c2058b69fee1c2ab1510befec37c5ce97e58f6a
mysitioc <mysitioc@mysitio.cl>
2019-03-07 12:20:18 -0600

    --- Auto-Git Commit ---

--- core/view.py
+++ core/view.py
@@ -64,7 +64,6 @@ class view:
             loader=FileSystemLoader(theme),
             bytecode_cache=FileSystemBytecodeCache()
         )
-        time=functions.current_time(as_string=False)
         for template, data in template_list:
             template_url = theme + template + "." + view.extension
             my_file = Path(template_url)
@@ -76,11 +75,8 @@ class view:
 
         body = ''
         for template, data in template_list:
-            #template = env.get_template(template + "." + view.extension)
-            #body += template.render(data)
             body += view.render_unit(env, template, data)
 
-        print((functions.current_time(as_string=False)-time) *1000)
 
         if minify:  # and not return_body and cache.is_cacheable():
             body = view.compress(body, 'html')

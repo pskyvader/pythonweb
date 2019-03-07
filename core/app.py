73e1f1ffe1c6443f78b08b8560974491331a8156
mysitioc <mysitioc@mysitio.cl>
2019-03-07 11:12:01 -0600

    --- Auto-Git Commit ---

--- core/app.py
+++ core/app.py
@@ -93,6 +93,7 @@ class app:
         else:
             response = {'error': 404}
 
+        render=False
         if 'headers' not in response:
             response['headers'] = [
                 ('Content-Type', 'text/html; charset=utf-8')
@@ -121,7 +122,11 @@ class app:
         if 'file' in response:
             data_return['file'] = response['file']
 
-        data_return['response_body'] = view.render_multiple(response['body'])
+            
+        if render:
+            data_return['response_body'] = view.render_multiple(response['body'])
+        else:
+            data_return['response_body'] = response['body']
         data_return['headers'] = response['headers']
         for cookie in functions.cookies:
             data_return['headers'].append(('Set-Cookie', cookie))

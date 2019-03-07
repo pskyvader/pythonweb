8c98353f86c601fd65d09b22bcc40e4be471f4b0
mysitioc <mysitioc@mysitio.cl>
2019-03-07 11:55:47 -0600

    --- Auto-Git Commit ---

--- core/view.py
+++ core/view.py
@@ -94,7 +94,7 @@ class view:
                 if isinstance(d, dict) or isinstance(d, list) or isinstance(d, tuple):
                     d = view.render_unit(env, '', d)
         elif isinstance(data, tuple):
-            data = view.render_unit(env, data[0], data[1],True)
+            data = view.render_unit(env, data[0], data[1])
         
         if template!='' and isinstance(data, dict):
             template = env.get_template(template + "." + view.extension)

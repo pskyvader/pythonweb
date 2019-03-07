0dea054dc44605150ed49b1356e2ac7b90160b24
mysitioc <mysitioc@mysitio.cl>
2019-03-07 12:55:04 -0600

    --- Auto-Git Commit ---

--- core/cache.py
+++ core/cache.py
@@ -1,4 +1,4 @@
-from .app import app
+
 
 class cache:
     data             = []
@@ -13,5 +13,42 @@ class cache:
     
     @staticmethod
     def add_cache(content):
+        from .app import app
         if app.front and cache.cacheable:
             cache.data.append(content)
+
+
+    @staticmethod
+    def delete_cache():
+        import shutil
+        direrctory        = app.get_dir(True) + 'cache/'
+        shutil.rmtree(direrctory)
+        
+
+    @staticmethod
+    def get_cache(url:list):
+        from .functions import functions
+        ruta    = functions.generar_url(url)
+        current = functions.current_url()
+        if (ruta != current) {
+            return ""
+        }
+
+        if (null == self.cacheable_config) {
+            config                 = app.getConfig()
+            self.cacheable_config = (isset(config['cache']) ? config['cache'] : true)
+            if (!self.cacheable_config) {
+                self.cacheable = false
+            }
+        }
+
+        if (app._front && self.cacheable) {
+            dir  = app.get_dir(true) . 'cache/'
+            name = self.file_name(url)
+            if ("" != name && file_exists(dir . name)) {
+                return file_get_contents(dir . name)
+            } else {
+                return ""
+            }
+        }
+    }

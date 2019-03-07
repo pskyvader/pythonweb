2279bcd59be237509edf244ae3ec899b10326b07
mysitioc <mysitioc@mysitio.cl>
2019-03-07 12:10:48 -0600

    --- Auto-Git Commit ---

--- core/view.py
+++ core/view.py
@@ -304,22 +304,3 @@ class view:
             from htmlmin import minify
             combine_files = minify(combine_files, True, True, True)
         return combine_files
-
-
-
-class MyCache(BytecodeCache):
-    def __init__(self, directory):
-        self.directory = directory
-
-    def load_bytecode(self, bucket):
-        from os import path
-        filename = path.join(self.directory, bucket.key)
-        if path.exists(filename):
-            with open(filename, 'rb') as f:
-                bucket.load_bytecode(f)
-
-    def dump_bytecode(self, bucket):
-        from os import path
-        filename = path.join(self.directory, bucket.key)
-        with open(filename, 'wb') as f:
-            bucket.write_bytecode(f)

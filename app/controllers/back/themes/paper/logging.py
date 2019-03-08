ee06dc04a940283247822542c404f73d89949a3d
mysitioc <mysitioc@mysitio.cl>
2019-03-08 13:03:25 -0600

    --- Auto-Git Commit ---

--- app/controllers/back/themes/paper/logging.py
+++ app/controllers/back/themes/paper/logging.py
@@ -12,45 +12,14 @@ import socket
 class logging:
     def init(self):
         ret = {'body': ''}
-        app.get
-        theme = view.get_theme()
-        resource = '/'.join(var)
-        resource_url = theme + resource
-        my_file = Path(resource_url)
-        if not my_file.is_file():
-            ret = {'error': 404}
+        resource=app.get_dir(True)+'log.json'
+        ret['headers'] = [ ('Content-Type', 'text/plain; charset=utf-8') ]
+        
+        my_file = Path(resource)
+        if my_file.is_file():
+            ret['file'] = resource
         else:
-            ret['is_file'] = True
-            mime = mimetypes.guess_type(resource_url, False)[0]
-            if mime == None:
-                mime = 'text/plain'
-            extension = mimetypes.guess_extension(mime)
-            expiry_time = datetime.datetime.utcnow() + datetime.timedelta(100)
-            ret['headers'] = [
-                ('Content-Type', mime+'; charset=utf-8'),
-                ('Expires', expiry_time.strftime("%a, %d %b %Y %H:%M:%S GMT")),
-                ('Accept-encoding', 'gzip,deflate'),
-                ('Content-Encoding', 'gzip')
-            ]
-            cache_file = theme+'cache/' + \
-                str(functions.fecha_archivo(resource_url, True)) + \
-                '-'+resource.replace('/', '-')
-            my_file = Path(cache_file)
-            if my_file.is_file():
-                ret['file'] = cache_file
-            else:
-                from gzip import compress
-                test = os.listdir(theme+'cache/')
-                for item in test:
-                    if 'resources' in resource and item.endswith(extension):
-                        os.remove(os.path.join(theme+'cache/', item))
-                    elif item.endswith(resource):
-                        os.remove(os.path.join(theme+'cache/', item))
-                f = open(resource_url, "rb").read()
-                f = compress(f)
-
-                file_write = open(cache_file, 'wb')
-                file_write.write(f)
-                file_write.close()
-                ret['file'] = cache_file
+            file_write = open(resource, 'w+')
+            file_write.close()
+            ret['file'] = resource
         return ret

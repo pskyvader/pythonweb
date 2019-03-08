7e97b7b02be3a3a93de4fa091ca419738fec95fc
mysitioc <mysitioc@mysitio.cl>
2019-03-08 12:11:53 -0600

    --- Auto-Git Commit ---

--- app/controllers/back/themes/paper/error.py
+++ app/controllers/back/themes/paper/error.py
@@ -48,6 +48,7 @@ class error(base):
 
         asi = aside()
         ret['body'] += asi.normal()['body']
+        print(var)
         data = {'error':var}
         ret['body'].append(('404', data))
 

0106297822d9e34c55f4111c4e1e254fd559e0c0
mysitioc <mysitioc@mysitio.cl>
2019-03-08 11:45:38 -0600

    --- Auto-Git Commit ---

--- core/app.py
+++ core/app.py
@@ -98,8 +98,7 @@ class app:
             controller = app.controller_dir + url[0]
             my_file = Path(app.root + controller + '.py')
             if my_file.is_file():
-                current_module = importlib.import_module(
-                    controller.replace("/", "."))
+                current_module = importlib.import_module( controller.replace("/", "."))
                 current_module = getattr(current_module, url[0])
                 current_module = current_module()
                 del url[0]
@@ -126,8 +125,14 @@ class app:
                     response['body'] = ''
             else:
                 data_return['status'] = '404 Not Found'
-                response['body'] = '<html><body>No encontrado ' +  \
-                    str(my_file) + '</body></html>'
+                controller = app.controller_dir + 'error'
+                current_module = importlib.import_module( controller.replace("/", "."))
+                current_module = getattr(current_module, 'error')
+                current_module = current_module()
+                response_error=current_module.index(str(my_file))
+                
+                response['body']= view.render(response_error['body'])
+                #response['body'] = '<html><body>No encontrado ' +   str(my_file) + '</body></html>'
         else:
             data_return['status'] = '200 OK'
 

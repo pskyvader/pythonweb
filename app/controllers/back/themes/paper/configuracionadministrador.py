0d7fd543126df211c19f5e484637744228deab9a
mysitioc <mysitioc@mysitio.cl>
2019-03-08 10:59:07 -0600

    --- Auto-Git Commit ---

--- app/controllers/back/themes/paper/configuracionadministrador.py
+++ app/controllers/back/themes/paper/configuracionadministrador.py
@@ -14,10 +14,13 @@ from .footer import footer
 from core.functions import functions
 #from core.image import image
 
+
 class configuracionadministrador(base):
     url = ['configuracionadministrador']
-    metadata = {'title' : 'Configuracion de administrador','modulo':'configuracionadministrador'}
+    metadata = {'title': 'Configuracion de administrador',
+                'modulo': 'configuracionadministrador'}
     breadcrumb = []
+
     def __init__(self):
         super().__init__(None)
 
@@ -26,7 +29,7 @@ class configuracionadministrador(base):
         '''Controlador de lista_class de elementos base, puede ser sobreescrito en el controlador de cada modulo'''
         ret = {'body': ''}
         # Clase para enviar a controlador de lista_class
-        url_final=cls.url.copy()
+        url_final = cls.url.copy()
         if not administrador_model.verificar_sesion():
             url_final = ['login', 'index'] + url_final
         # verificar sesion o redireccionar a login
@@ -47,18 +50,16 @@ class configuracionadministrador(base):
 
         asi = aside()
         ret['body'] += asi.normal()['body']
-        
-        $vaciar = table_model::getAll(array('truncate' => true), array(), 'tablename');
-        view::set('vaciar', $vaciar);
-        view::set('breadcrumb', $this->breadcrumb);
-        view::set('title', $this->metadata['title']);
-        view::set('save_url', functions::generar_url(array_merge($this->url, array('vaciar'))));
-        view::set('list_url', functions::generar_url($this->url));
-        view::render('configuracion_administrador');
 
+        vaciar = table_model.getAll({'truncate': True}, {}, 'tablename')
+        data = {}
+        data['vaciar'] = vaciar
+        data['breadcrumb'] = cls.breadcrumb
+        data['title'] = cls.metadata['title']
+        data['save_url'] = functions.generar_url(cls.url + ['vaciar'])
+        data['list_url'] = functions.generar_url(cls.url)
 
-
-        ret['body'].append(('home', data))
+        ret['body'].append(('configuracion_administrador', data))
 
         f = footer()
         ret['body'] += f.normal()['body']

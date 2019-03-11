1f65cb372185b878d4d36762bca168f9edad521e
mysitioc <mysitioc@mysitio.cl>
2019-03-11 12:31:05 -0500

    --- Auto-Git Commit ---

--- app/controllers/back/themes/paper/backup.py
+++ app/controllers/back/themes/paper/backup.py
@@ -427,6 +427,7 @@ class backup(base):
     def continuar(self):
         '''Inicio o continuacion de respaldo en modo lento (toma mas tiempo pero consume menos recursos)'''
         ret = {'body': ''}
+        print(app.post)
         lista=json.loads(app.post['lista'])
         respuesta = self.zipData(self,self.base_dir, app.post['archivo_backup'], lista, app.post['total'])
         ret['body'] = json.dumps(respuesta)

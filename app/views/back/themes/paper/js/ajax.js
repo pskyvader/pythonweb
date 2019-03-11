e08fe8f7162652563d98d9a547bbc2d4729d7207
mysitioc <mysitioc@mysitio.cl>
2019-03-11 13:17:06 -0500

    --- Auto-Git Commit ---

--- app/views/back/themes/paper/js/ajax.js
+++ app/views/back/themes/paper/js/ajax.js
@@ -51,6 +51,7 @@ function post(url_post, info, mensaje_inicial, importante, archivo, callback, ex
             data.append(key, value);
         });
     }
+    console.log(data);
     $.ajax({
         url: url_post,
         type: 'POST',

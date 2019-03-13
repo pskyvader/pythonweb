032d8e4e275aea14dbd68debc7f24fc5fc2abc2c
mysitioc <mysitioc@mysitio.cl>
2019-03-13 16:16:28 -0500

    --- Auto-Git Commit ---

--- app/controllers/back/themes/paper/detalle.py
+++ app/controllers/back/themes/paper/detalle.py
@@ -27,11 +27,11 @@ class detalle:
         row_data = data['row']
         row = []
         for k,v in campos.items():
+            print(k)
             content = self.field(v, row_data)
             row.append( {'content': content, 'content_field': v['field'], 'class': 'hidden' if 'hidden' == v['type'] else ''})
 
 
-        print(row)
 
         data['row'] = row
         data['title'] = self.metadata['title']

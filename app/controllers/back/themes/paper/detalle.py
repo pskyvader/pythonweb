ab00646f8bfa198a330f840a5c3ec04e4fd94b4c
mysitioc <mysitioc@mysitio.cl>
2019-03-13 15:54:13 -0500

    --- Auto-Git Commit ---

--- app/controllers/back/themes/paper/detalle.py
+++ app/controllers/back/themes/paper/detalle.py
@@ -298,10 +298,9 @@ class detalle:
                 campos['array'][key]['class'] = ('btn-success' if fila[campos['field']][key] == 'true' else 'btn-danger') if campos['field'] in fila and key in fila[campos['field']] else 'btn-success'
                 campos['array'][key]['icon'] = ('fa-check' if fila[campos['field']][key] == 'true' else 'fa-close') if campos['field'] in fila and key in fila[campos['field']] else 'fa-check'
                 
-            print(list(campos['array'].values()))
             data = {
                 'title_field': campos['title_field'],
-                'array': campos['array'],
+                'array': list(campos['array'].values()),
                 'field': campos['field'],
                 'idparent': idparent,
                 'parent': parent,

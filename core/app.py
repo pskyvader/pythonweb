75f1d538cd6fc141da5ba5bedce550dab7abcf3e
mysitioc <mysitioc@mysitio.cl>
2019-03-13 12:15:25 -0500

    --- Auto-Git Commit ---

--- core/app.py
+++ core/app.py
@@ -111,7 +111,7 @@ class app:
                 response = {'error': 404}
 
         if 'headers' not in response:
-            response['headers'] = [ ( 'Content-Type', 'text/html; charset=utf-8' ) ]
+            response['headers'] = [ ('Content-Type', 'text/html; charset=utf-8') ]
 
         if 'error' in response:
             if response['error'] == 301:

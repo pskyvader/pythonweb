bff64464516c0211a9b56bb9d275ede7a95f6a74
mysitioc <mysitioc@mysitio.cl>
2019-03-13 14:03:25 -0500

    --- Auto-Git Commit ---

--- app/controllers/back/themes/paper/modulo.py
+++ app/controllers/back/themes/paper/modulo.py
@@ -3,8 +3,8 @@ from app.models.modulo import modulo as modulo_model
 
 #from app.models.table import table as table_model
 from app.models.administrador import administrador as administrador_model
-#from app.models.modulo import modulo as modulo_model
 from app.models.moduloconfiguracion import moduloconfiguracion as moduloconfiguracion_model
+from app.models.profile import profile as profile_model
 
 #from .detalle import detalle as detalle_class
 from .lista import lista as lista_class
@@ -118,35 +118,48 @@ class modulo(base):
         return ret
 
 
-    public function detail(var = array())
-    {
-        class = this->class // Clase para enviar a controlador de detalle
-        parent_class = this->parent_class // Clase Padre
-        parent = this->parent // Clase Padre
 
-        url_save = url_list = this->url
-        url_save[] = 'guardar'
-        this->url[] = 'detail'
-        if (isset(var[0]):
-            id = (int) var[0]
-            this->url[] = id
-            this->metadata['title'] = 'Editar '.this->metadata['title'] 
+
+
+    @classmethod
+    def detail(cls, var=[]):
+        '''Controlador de detalle de elementos base, puede ser sobreescrito en el controlador de cada modulo'''
+        ret = {'body': ''}
+        # Clase para enviar a controlador de detalle
+        class_name = cls.class_name
+        parent_class=cls.parent_class
+        parent=cls.parent
+        url_list = cls.url.copy()
+        url_save = cls.url.copy()
+        url_final = cls.url.copy()
+        url_save.append('guardar')
+        url_final.append('detail')
+
+
+        if len(var)>0:
+            id = int(var[0])
+            url_final.append(id)
+            cls.metadata['title'] = 'Editar ' + cls.metadata['title']
         else:
             id = 0
-            this->metadata['title'] = 'Nuevo '.this->metadata['title'] 
-        }
+            cls.metadata['title'] = 'Nuevo ' + cls.metadata['title']
 
-        this->breadcrumb[] = array('url' : functions.generar_url(this->url), 'title' : (this->metadata['title']), 'active' : 'active')
+        cls.breadcrumb.append({'url': functions.generar_url( url_final), 'title': cls.metadata['title'], 'active': 'active'})
+
 
         if not administrador_model.verificar_sesion():
-            this->url = array_merge(array('login', 'index'), this->url)
-        }
-        functions.url_redirect(this->url) //verificar sesion o redireccionar a login
+            url_final = ['login', 'index'] + url_final
+        # verificar sesion o redireccionar a login
+        url_return = functions.url_redirect(url_final)
+        if url_return != '':
+            ret['error'] = 301
+            ret['redirect'] = url_return
+            return ret
 
-        this->metadata['title'] = parent['titulo'] . ' - ' . this->metadata['title']
+        cls.metadata['title'] = parent['titulo'] + ' - ' + cls.metadata['title']
+        # cabeceras y campos que se muestran en el detalle:
+        # titulo,campo de la tabla a usar, tipo (ver archivo detalle.py funcion "field")
 
-        /* cabeceras y campos que se muestran en el detalle:
-        titulo,campo de la tabla a usar, tipo (ver archivo detalle.php funcion "field") */
 
         ta = profile.getAll(array('estado' : true))
         tipos_administrador = array()
@@ -199,6 +212,105 @@ class modulo(base):
             'aside' : array('title_field' : 'Aside', 'field' : 'aside', 'type' : 'active', 'required' : true),
             'hijos' : array('title_field' : 'Contiene hijos', 'field' : 'hijos', 'type' : 'active', 'required' : true),
         )
+
+
+
+        # controlador de detalle
+        detalle = detalle_class(cls.metadata)
+        configuracion = detalle.configuracion(cls.metadata['modulo'])
+        
+        if 'error' in configuracion:
+            ret['error']=configuracion['error']
+            ret['redirect']=configuracion['redirect']
+            return ret
+
+        row = class_name.getById(id) if id != 0 else []
+        if cls.contiene_tipos:
+            configuracion['campos']['tipo'] = {
+                'title_field': 'tipo', 'field': 'tipo', 'type': 'hidden', 'required': True}
+            row['tipo'] = get['tipo']
+
+        if cls.contiene_hijos and 'idpadre' in configuracion['campos']:
+            categorias = class_name.getAll()
+            for c in categorias:
+                if c[0] == id:
+                    del c
+                    break
+
+            raiz = {0: 0, 'titulo': 'Raíz', 'idpadre': [-1]}
+            categorias = raiz+categorias
+            configuracion['campos']['idpadre']['parent'] = functions.crear_arbol( categorias, -1)
+        elif cls.contiene_hijos or 'idpadre' in configuracion['campos']:
+            configuracion['campos']['idpadre'] = {
+                'title_field': 'idpadre', 'field': 'idpadre', 'type': 'hidden', 'required': True}
+            if id == 0:
+                if 'idpadre' in get:
+                    row['idpadre'] = json.dumps([get['idpadre']])
+                else:
+                    row['idpadre'] = json.dumps([0])
+        else:
+            if 'idpadre' in configuracion['campos']:
+                del configuracion['campos']['idpadre']
+
+        if cls.class_parent != None:
+            class_parent = cls.class_parent
+            idparent = class_parent.idname
+
+            is_array = True
+            fields = table.getByname(class_name.table)
+            if idparent in fields and fields[idparent]['tipo'] != 'longtext':
+                is_array = False
+
+            if idparent in configuracion['campos']:
+                categorias = class_parent.getAll()
+                if is_array:
+                    configuracion['campos'][idparent]['parent'] = functions.crear_arbol(
+                        categorias)
+                else:
+                    configuracion['campos'][idparent]['parent'] = categorias
+
+            else:
+                configuracion['campos'][idparent] = {
+                    'title_field': idparent, 'field': idparent, 'type': 'hidden', 'required': True}
+                if id == 0:
+                    if idparent in get:
+                        if is_array:
+                            row[idparent] = json.dumps([get[idparent]])
+                        else:
+                            row[idparent] = int(get[idparent])
+                    else:
+                        if is_array:
+                            row[idparent] = json.dumps([0])
+                        else:
+                            row[idparent] = 0
+                else:
+                    if is_array:
+                        row[idparent] = json.dumps(row[idparent])
+                    else:
+                        row[idparent] = row[idparent]
+
+        # informacion para generar la vista del detalle
+        data = {
+            'breadcrumb': cls.breadcrumb,
+            'campos': configuracion['campos'],
+            'row': row,
+            'id': id if id != 0 else '',
+            'current_url': functions.generar_url(url_final),
+            'save_url': functions.generar_url(url_save),
+            'list_url': functions.generar_url(url_list),
+        }
+
+        ret=detalle.normal(data)
+        return ret
+
+
+
+
+
+
+
+
+
 
         detalle = new detalle(this->metadata) //controlador de detalle
         row = (id != 0) ? (class.getById(id)) : array()

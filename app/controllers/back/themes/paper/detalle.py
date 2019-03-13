89d732cc588fe2bab3353afc0c504ffe075b28b3
mysitioc <mysitioc@mysitio.cl>
2019-03-13 16:43:59 -0500

    --- Auto-Git Commit ---

--- app/controllers/back/themes/paper/detalle.py
+++ app/controllers/back/themes/paper/detalle.py
@@ -299,11 +299,12 @@ class detalle:
         elif campos['type'] == 'multiple_active_array':
             array_campos=[]
             for key in campos['array'].keys():
-                if campos['field'] in fila and key in fila[campos['field']]:
-                    print(fila[campos['field']][key])
-                else:
-                    print(fila)
+                if campos['field'] in fila:
                     print(fila[campos['field']])
+                    if key in fila[campos['field']]:
+                        print(fila[campos['field']][key])
+                    else:
+                        print('key',key)
                 campos['array'][key]['active'] = str(fila[campos['field']][key]) if campos['field'] in fila and key in fila[campos['field']] else 'true'
                 campos['array'][key]['class'] = ('btn-success' if fila[campos['field']][key] == 'true' else 'btn-danger') if campos['field'] in fila and key in fila[campos['field']] else 'btn-success'
                 campos['array'][key]['icon'] = ('fa-check' if fila[campos['field']][key] == 'true' else 'fa-close') if campos['field'] in fila and key in fila[campos['field']] else 'fa-check'

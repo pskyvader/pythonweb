c507cc63e59c944edf893a0dcf19c2f838ea8e37
mysitioc <mysitioc@mysitio.cl>
2019-03-13 13:03:11 -0500

    --- Auto-Git Commit ---

--- app/models/administrador.py
+++ app/models/administrador.py
@@ -149,7 +149,6 @@ class administrador(base_model):
     def logout():
         prefix_site = app.prefix_site
         session = app.session
-        print(session)
         del session[administrador.idname + prefix_site]
         del session["email" + prefix_site]
         del session["nombre" + prefix_site]

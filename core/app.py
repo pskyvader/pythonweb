f3c2f01f7e0fb3674e9556ea9629c56dc20e49fe
mysitioc <mysitioc@mysitio.cl>
2019-03-14 12:12:16 -0500

    --- Auto-Git Commit ---

--- core/app.py
+++ core/app.py
@@ -223,6 +223,7 @@ class app:
             p = FieldStorage( fp=post_env2['wsgi.input'], environ=post_env, keep_blank_values=True )
             try:
                 for key in p.keys():
+                    print(type(p[key]))
                     post[key] = p[key].value
             except Exception as error:
                 print('Error al obtener post: ' + repr(error) + repr(p)+ app.environ['PATH_INFO'])

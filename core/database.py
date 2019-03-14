95f1748370955866a60ea66b2a88b908a9cb9cce
mysitioc <mysitioc@mysitio.cl>
2019-03-14 13:05:32 -0500

    --- Auto-Git Commit ---

--- core/database.py
+++ core/database.py
@@ -144,6 +144,7 @@ class database():
 
     def update(self, table, idname, set_query, where, delete_cache=True):
         set_query = self.process_multiple(set_query)
+        print(set_query)
         image = []
         if 'image' in set_query:
             image = set_query['image']

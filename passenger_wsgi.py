57130334e09c9ebfd02f223cd1314e3b4ebf1cd7
mysitioc <mysitioc@mysitio.cl>
2019-03-14 15:48:06 -0500

    --- Auto-Git Commit ---

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -7,8 +7,8 @@ sys.path.insert(0, os.path.dirname(__file__))
 
 
 def application2(environ, start_response):
-    #from datetime import datetime
-    #init_time = datetime.now()
+    from datetime import datetime
+    init_time = datetime.now()
     
     app_web = app(os.path.dirname(__file__))
     main_data = app_web.init(environ)
@@ -33,7 +33,7 @@ def application2(environ, start_response):
             print('no filewrapper')
             return file_wrapper(f, 1024)
     else:
-        #print('total', (datetime.now()-init_time))
+        print('total', (datetime.now()-init_time))
         return [ret]
 
 

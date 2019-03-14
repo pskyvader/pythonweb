25267284127b38b881701de3e20064d70ef7a874
mysitioc <mysitioc@mysitio.cl>
2019-03-14 16:02:22 -0500

    --- Auto-Git Commit ---

--- core/app.py
+++ core/app.py
@@ -28,6 +28,8 @@ class app:
     def init(self, environ):
         from .cache import cache
         from .functions import functions
+        from datetime import datetime
+        init_time = datetime.now()
 
         app.environ = environ
         data_return = {}
@@ -152,6 +154,9 @@ class app:
         if 'file' in response:
             data_return['file'] = response['file']
 
+        if data_return['status']=='200 OK':
+            print('antes de render', (datetime.now()-init_time).total_seconds()*1000)
+            init_time=datetime.now()
 
         if isinstance(response['body'], list):
             data_return['response_body'] = view.render(response['body'])
@@ -162,6 +167,8 @@ class app:
         data_return['headers'] = response['headers']
         for cookie in functions.cookies:
             data_return['headers'].append(('Set-Cookie', cookie))
+        if data_return['status']=='200 OK':
+            print('total', (datetime.now()-init_time).total_seconds()*1000)
         return data_return
 
     @staticmethod

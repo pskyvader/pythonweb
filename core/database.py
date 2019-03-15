6bdaf13ea1ba63d41da43815f7b4849bdf7e14ee
mysitioc <mysitioc@mysitio.cl>
2019-03-15 15:33:17 -0500

    --- Auto-Git Commit ---

--- core/database.py
+++ core/database.py
@@ -63,7 +63,6 @@ class database():
         return rows
 
     def get_last_insert_id(self):
-        print(self._connection.insert_id())
         return self._connection.insert_id()
 
     def get(self, table: str, idname: str, where: dict, condiciones={}, select=""):

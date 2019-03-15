f846dce4ac82baba9d88787082bd76027c580f0b
mysitioc <mysitioc@mysitio.cl>
2019-03-15 10:47:00 -0500

    --- Auto-Git Commit ---

--- app/controllers/back/themes/paper/static_file.py
+++ app/controllers/back/themes/paper/static_file.py
@@ -5,16 +5,21 @@ import os
 
 class static_file:
     def init(self,var=[]):
-        print(var)
-        resource=app.get_dir(True)+'log.json'
-        ret = {'headers': [ ('Content-Type', 'application/json; charset=utf-8') ], 'body': ''}
-        
-        my_file = Path(resource)
-        ret['is_file'] = True
-        if my_file.is_file():
-            ret['file'] = resource
+        if 0 in var:
+            resource=app.get_dir(True)+var[0]
+            ret = {'headers': [ ('Content-Type', 'application/json; charset=utf-8') ], 'body': ''}
+            
+            my_file = Path(resource)
+            ret['is_file'] = True
+            if my_file.is_file():
+                ret['file'] = resource
+            else:
+                file_write = open(resource, 'w+')
+                file_write.close()
+                ret['file'] = resource
+            return ret
         else:
-            file_write = open(resource, 'w+')
-            file_write.close()
-            ret['file'] = resource
-        return ret
+            ret = {
+                'error': 404
+            }
+            return ret

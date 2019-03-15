4c18f106f2f8328443994fd8b9cf0a5e0202cec1
mysitioc <mysitioc@mysitio.cl>
2019-03-15 14:08:08 -0500

    --- Auto-Git Commit ---

--- app/models/base_model.py
+++ app/models/base_model.py
@@ -34,7 +34,7 @@ class base_model:
             del where['idpadre']
 
         if 'order' not in condiciones and 'orden' in fields:
-            print(cls.table,fields['orden'])
+            print(cls,fields['orden'])
             condiciones['order'] = 'orden ASC'
 
         if 'palabra' in condiciones:

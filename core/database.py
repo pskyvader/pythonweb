40261a7a415f1b0133dbf8c24010f08ea14eefd5
mysitioc <mysitioc@mysitio.cl>
2019-03-15 13:29:37 -0500

    --- Auto-Git Commit ---

--- core/database.py
+++ core/database.py
@@ -431,7 +431,9 @@ class database():
                 if key != "image" and key != "file":
                     if all(isinstance(item, int) for item in row.keys()):
                         row=list(row.values())
+                    print('antes',key,row)
                     data[key] = json.dumps(row)
+                    print('despues',key,data[key])
                 else:
                     data[key] = row
             del data['multiple']

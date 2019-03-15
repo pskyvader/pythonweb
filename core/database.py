ef3441c9124a7d4a6fc002859006cbd35d9846d9
mysitioc <mysitioc@mysitio.cl>
2019-03-15 15:05:46 -0500

    --- Auto-Git Commit ---

--- core/database.py
+++ core/database.py
@@ -429,6 +429,7 @@ class database():
                 if key != "image" and key != "file":
                     if all(isinstance(item, int) for item in row.keys()):
                         row=list(row.values())
+                    print(row)
                     data[key] = json.dumps(row,ensure_ascii=False)
                 else:
                     data[key] = row

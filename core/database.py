1d78bd0776a414a6e9c34470109124a08ee57992
mysitioc <mysitioc@mysitio.cl>
2019-03-15 14:30:08 -0500

    --- Auto-Git Commit ---

--- core/database.py
+++ core/database.py
@@ -99,7 +99,9 @@ class database():
             sql += " LIMIT " + str(condiciones['limit'])
             if 'limit2' in condiciones and condiciones['limit2'] > 0:
                 sql += " , " + str(condiciones['limit2'])
+        print(sql)
         row = self.consulta(sql, True)
+        print(row)
         return row
 
     def insert(self, table, idname, insert, delete_cache=True):

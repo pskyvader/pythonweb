0d6167e67f45c74a0360ae27a6b0a2ec0dd4e2d3
mysitioc <mysitioc@mysitio.cl>
2019-03-15 11:25:31 -0500

    --- Auto-Git Commit ---

--- app/views/back/themes/paper/js/custom.js
+++ app/views/back/themes/paper/js/custom.js
@@ -68,7 +68,7 @@ function register_sw() {
         //console.log('sw');
         navigator.serviceWorker.register(path + 'sw.js').then(function(registration) {
             // Registration was successful
-            //console.log('ServiceWorker registration successful with scope: ', registration.scope);
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
         }).catch(function(err) {
             // registration failed :(
             console.log('ServiceWorker registration failed: ', err);

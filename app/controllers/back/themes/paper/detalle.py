d213df37089b574837e2c80027928dd1a93f8418
mysitioc <mysitioc@mysitio.cl>
2019-03-15 14:54:46 -0500

    --- Auto-Git Commit ---

--- app/controllers/back/themes/paper/detalle.py
+++ app/controllers/back/themes/paper/detalle.py
@@ -66,7 +66,6 @@ class detalle:
 
         campos = {}
         for m in modulo['detalle']:
-            print(m['field'])
             if 'true' == m['estado'][tipo_admin]:
                 campos[m['field']] = {'title_field': m['titulo'], 'field': m['field'], 'type': m['tipo'], 'required': 'true' == m['required'], 'help': m['texto_ayuda']}
 

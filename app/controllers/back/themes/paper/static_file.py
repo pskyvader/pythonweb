e9d44ca02d95e0faa1dd8ec76cf60658375c96b9
mysitioc <mysitioc@mysitio.cl>
2019-03-15 10:50:45 -0500

    --- Auto-Git Commit ---

--- app/controllers/back/themes/paper/static_file.py
+++ app/controllers/back/themes/paper/static_file.py
@@ -5,14 +5,17 @@ import mimetypes
 
 
 class static_file:
-    def init(self,var=[]):
-        if len(var)>0:
-            resource_url=app.get_dir(True)+var[0]
+    def init(self, var=[]):
+        if len(var) > 0:
+            resource_url = app.get_dir(True)+var[0]
             mime = mimetypes.guess_type(resource_url, False)[0]
             if mime == None:
                 mime = 'text/plain'
-            ret = {'headers': [ 'Content-Type', mime+'; charset=utf-8' ], 'body': ''}
-            
+            ret = {
+                'headers': ['Content-Type', mime+'; charset=utf-8'],
+                'body': ''
+            }
+
             my_file = Path(resource_url)
             ret['is_file'] = True
             if my_file.is_file():

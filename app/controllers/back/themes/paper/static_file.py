6d364ff9b22452f7eb14ab56c3575217d65a5265
mysitioc <mysitioc@mysitio.cl>
2019-03-15 10:49:00 -0500

    --- Auto-Git Commit ---

--- app/controllers/back/themes/paper/static_file.py
+++ app/controllers/back/themes/paper/static_file.py
@@ -1,22 +1,26 @@
 from core.app import app
 from pathlib import Path
 import os
+import mimetypes
 
 
 class static_file:
     def init(self,var=[]):
         if 0 in var:
-            resource=app.get_dir(True)+var[0]
-            ret = {'headers': [ ('Content-Type', 'application/json; charset=utf-8') ], 'body': ''}
+            resource_url=app.get_dir(True)+var[0]
+            mime = mimetypes.guess_type(resource_url, False)[0]
+            if mime == None:
+                mime = 'text/plain'
+            ret = {'headers': [ 'Content-Type', mime+'; charset=utf-8' ], 'body': ''}
             
-            my_file = Path(resource)
+            my_file = Path(resource_url)
             ret['is_file'] = True
             if my_file.is_file():
-                ret['file'] = resource
+                ret['file'] = resource_url
             else:
-                file_write = open(resource, 'w+')
+                file_write = open(resource_url, 'w+')
                 file_write.close()
-                ret['file'] = resource
+                ret['file'] = resource_url
             return ret
         else:
             ret = {

61f68ad6003befa0cb94f8cdd3082ee19c309a3c
mysitioc <mysitioc@mysitio.cl>
2019-03-18 11:13:15 -0500

    --- Auto-Git Commit ---

--- core/image.py
+++ core/image.py
@@ -470,7 +470,6 @@ class image:
         if len(fotos) > 0:
             portada = fotos[0]
             for f in fotos:
-                print(f)
                 if (isinstance(f['portada'], str) and 'true' == f['portada']) or (isinstance(f['portada'], bool) and f['portada']):
                     portada = f
                     break

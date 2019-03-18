c930de3d25535a71c4f8d7690748bf7ed3233491
mysitioc <mysitioc@mysitio.cl>
2019-03-18 10:57:00 -0500

    --- Auto-Git Commit ---

--- app/controllers/back/themes/paper/pedido.py
+++ app/controllers/back/themes/paper/pedido.py
@@ -305,7 +305,6 @@ class pedido(base):
                 portada = image.portada(lp['foto'])
                 thumb_url = image.generar_url(portada, 'cart')
                 lp = {'titulo': lp['titulo'], 'idproducto': lp['idproducto'], 'foto': thumb_url, 'precio': lp['precio_final'], 'stock': lp['stock']}
-                print(thumb_url)
 
             configuracion['campos']['direcciones']['lista_productos'] = lista_productos
 

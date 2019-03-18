13aefd9173796ce519a6075b7c43675f61558c5d
mysitioc <mysitioc@mysitio.cl>
2019-03-18 10:16:14 -0500

    --- Auto-Git Commit ---

--- app/controllers/back/themes/paper/base.py
+++ app/controllers/back/themes/paper/base.py
@@ -68,8 +68,7 @@ class base:
                 tipo = 0
 
             modulo = modulo_model.getAll( {'idmoduloconfiguracion': moduloconfiguracion[0], 'tipo': tipo})
-
-            print(modulo)
+            
             cls.contiene_hijos = modulo[0]['hijos'] if 'hijos' in modulo[0] else False
             cls.metadata['title'] = modulo[0]['titulo']
 

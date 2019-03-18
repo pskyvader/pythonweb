349a370ebc21949bcc9703465ffef943b0200c36
mysitioc <mysitioc@mysitio.cl>
2019-03-18 13:43:41 -0500

    --- Auto-Git Commit ---

--- core/database.py
+++ core/database.py
@@ -126,6 +126,7 @@ class database():
 
         for key, value in insert.items():
             sql += ","
+            
             sql += value if (value.lower == "true" or value.lower == "True" or value == "false" or value == "False") else "'" + str(value).replace("'", "\\'") + "'"
 
         sql += ")"

d6f3271494f04e90ae150d845e8c1eb1adf3dabc
mysitioc <mysitioc@mysitio.cl>
2019-03-19 12:05:04 -0500

    --- Auto-Git Commit ---

--- app/controllers/back/themes/paper/update.py
+++ app/controllers/back/themes/paper/update.py
@@ -145,9 +145,7 @@ class update(base):
 
     def download(self, url, path):
         import urllib.request
-        #open(path, 'w+').close()
         final_path, result = urllib.request.urlretrieve(url, path)
-        print(final_path, result)
         return True
 
     def update_file(self):

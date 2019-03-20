80b5fdf5aa938f0fc03da2193c4046e540da836c
mysitioc <mysitioc@mysitio.cl>
2019-03-20 15:42:38 -0500

    --- Auto-Git Commit ---

--- core/image.py
+++ core/image.py
@@ -374,7 +374,7 @@ class image:
             else:
                 new_im = Image.new( 'RGBA', (ancho_maximo, alto_maximo), (255, 255, 255, 0))
                 box = (x, y, miniatura_ancho+x, miniatura_alto+y)
-                new_im.paste(im, (box))
+                new_im.paste(im.resize((miniatura_ancho, miniatura_alto)), (box))
 
         foto_recorte = image.nombre_archivo(foto, etiqueta, '', True)
         foto_webp = image.nombre_archivo(foto, etiqueta, 'webp', True)

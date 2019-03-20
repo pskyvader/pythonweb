0a7852561f80d29e54fe7d5ab38b8fa968d40c29
mysitioc <mysitioc@mysitio.cl>
2019-03-20 15:51:53 -0500

    --- Auto-Git Commit ---

--- core/image.py
+++ core/image.py
@@ -370,8 +370,9 @@ class image:
             if ancho >= miniatura_ancho or alto >= miniatura_alto:
                 new_im = Image.new( 'RGBA', (miniatura_ancho, miniatura_alto), (255, 255, 255, 0))
                 box = (x, y)
-                print(box,etiqueta)
-                new_im.paste(im.resize((miniatura_ancho, miniatura_alto)), (box))
+                im=im.resize((miniatura_ancho, miniatura_alto))
+                print(box,etiqueta,im.size)
+                new_im.paste(im, (box))
             else:
                 new_im = Image.new( 'RGBA', (ancho_maximo, alto_maximo), (255, 255, 255, 0))
                 box = (x, y, miniatura_ancho+x, miniatura_alto+y)

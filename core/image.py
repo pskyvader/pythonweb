58d5099d44fd48ba0f76a2c2bf03fdb95dc92b00
mysitioc <mysitioc@mysitio.cl>
2019-03-20 15:04:22 -0500

    --- Auto-Git Commit ---

--- core/image.py
+++ core/image.py
@@ -360,6 +360,7 @@ class image:
 
         if tipo == "recortar":
             box = (x, y, ancho_maximo-x, alto_maximo-y)
+            print(box)
             new_im = im.crop(box)
             new_im.thumbnail((miniatura_ancho, miniatura_alto))
         elif "rellenar" == tipo:

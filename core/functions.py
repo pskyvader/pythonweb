d86541c5563587d23d51fbf5af3f8d5086ded4ad
mysitioc <mysitioc@mysitio.cl>
2019-03-21 14:29:10 -0500

    --- Auto-Git Commit ---

--- core/functions.py
+++ core/functions.py
@@ -249,6 +249,7 @@ class functions():
                 tree['root'][id] = tree['children'][id]
             else:
                 tree['children'][node['idpadre'][0]]['children'][id] = tree['children'][id]
+        print(tree)
         return tree['root']
 
     @staticmethod

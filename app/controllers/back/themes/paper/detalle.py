01fed8748706d2dd68c6896a675e619d00f5f039
mysitioc <mysitioc@mysitio.cl>
2019-03-21 14:46:40 -0500

    --- Auto-Git Commit ---

--- app/controllers/back/themes/paper/detalle.py
+++ app/controllers/back/themes/paper/detalle.py
@@ -453,7 +453,6 @@ class detalle:
                 }
                 for children in campos['parent'].values():
                     data['children'].append(self.field(campos, fila, '', children[0], 1))
-                print(data)
             else:
                 parent = campos['parent']
                 checked = True if 0 == idparent else False

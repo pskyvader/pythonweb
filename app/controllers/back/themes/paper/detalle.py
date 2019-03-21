6eaf4c3b565fbb0be8886e7759280a98e78a8076
mysitioc <mysitioc@mysitio.cl>
2019-03-21 12:45:07 -0500

    --- Auto-Git Commit ---

--- app/controllers/back/themes/paper/detalle.py
+++ app/controllers/back/themes/paper/detalle.py
@@ -451,8 +451,8 @@ class detalle:
                     'children': [],
                     'count': count if count > 0 else '',
                 }
+                print(campos)
                 for children in campos['parent']:
-                    print(campos, fila, '', children, 1)
                     data['children'] += self.field(campos, fila, '', children[0], 1)
 
             else:

d9b67669e794b307aded8e0760b9d934833adbe5
mysitioc <mysitioc@mysitio.cl>
2019-03-21 15:05:11 -0500

    --- Auto-Git Commit ---

--- app/controllers/back/themes/paper/base.py
+++ app/controllers/back/themes/paper/base.py
@@ -252,9 +252,8 @@ class base:
                 if c[0] == id:
                     del c
                     break
-            raiz = {0: 0, 'titulo': 'Raíz', 'idpadre': [-1]}
-            raiz.update(enumerate(categorias))
-            categorias = raiz
+            raiz = [{0: 0, 'titulo': 'Raíz', 'idpadre': [-1]}]
+            categorias = raiz+categorias
             configuracion['campos']['idpadre']['parent'] = functions.crear_arbol( categorias, -1)
         elif cls.contiene_hijos or 'idpadre' in configuracion['campos']:
             configuracion['campos']['idpadre'] = {

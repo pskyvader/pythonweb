8b2bf5f8216dfdb8eb598de3f9ab6dc84617175b
mysitioc <mysitioc@mysitio.cl>
2019-03-21 13:38:23 -0500

    --- Auto-Git Commit ---

--- core/view.py
+++ core/view.py
@@ -59,9 +59,10 @@ class view:
                     if k=='children':
                         print(d,data[k])
         elif isinstance(data, list):
-            #for d in data:
-                #if isinstance(d, dict) or isinstance(d, list) or isinstance(d, tuple):
-                    #d = view.render_unit(env, '', d)
+            pass
+            for d in data:
+                if isinstance(d, dict) or isinstance(d, list) or isinstance(d, tuple):
+                    d = view.render_unit(env, '', d)
         elif isinstance(data, tuple):
             data = view.render_unit(env, data[0], data[1])
 

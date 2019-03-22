5798c17ab7fcdb91ee1c1fc8e0b855168b43aa8a
mysitioc <mysitioc@mysitio.cl>
2019-03-22 10:38:13 -0500

    --- Auto-Git Commit ---

--- app/controllers/back/themes/paper/base.py
+++ app/controllers/back/themes/paper/base.py
@@ -378,8 +378,8 @@ class base:
                 where[class_parent.idname] = get[class_parent.idname]
 
         select = ""
-        respuesta['body'] = json.dumps(lista_class.excel(
-            cls.class_name, where, select, cls.metadata['title']), ensure_ascii=False)
+        excel_data=lista_class.excel(cls.class_name, where, select, cls.metadata['title'])
+        respuesta['body'] = json.dumps(excel_data, ensure_ascii=False)
         return respuesta
 
     @classmethod

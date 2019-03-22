4a828f83f8fa9788bac5496bc8d8e95c982fcf59
mysitioc <mysitioc@mysitio.cl>
2019-03-22 14:37:25 -0500

    --- Auto-Git Commit ---

---
+++ app/controllers/front/themes/mdbootstrap/websocket.py
@@ -0,0 +1,35 @@
+import asyncio
+import websockets
+
+
+class websocket:
+    USERS = set()
+
+    def init(self, var=[]):
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(websockets.serve(self.handler, 'http://example.com/admin/', 5678))
+        loop.run_forever()
+
+
+    async def notify(self, message):
+        if self.USERS:       # asyncio.wait doesn't accept an empty list
+            await asyncio.wait([user.send(message) for user in self.USERS])
+
+    async def producer_handler(self, websocket, path):
+        while True and self.USERS:
+            if self.message!=None:
+                message=self.message
+                self.message=None
+                await self.notify(message)
+
+
+    async def handler(self, websocket, path):
+        print('inicio')
+        # Register.
+        self.USERS.add(websocket)
+        producer_task = asyncio.ensure_future( self.producer_handler(websocket, path))
+        done, pending = await asyncio.wait([producer_task], return_when=asyncio.FIRST_COMPLETED)
+        for task in pending:
+            task.cancel()
+        
+        print('fin')

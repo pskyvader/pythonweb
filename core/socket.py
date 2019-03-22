a93763f15f5f3c3579caa98697f0930d4412eb20
mysitioc <mysitioc@mysitio.cl>
2019-03-22 13:19:38 -0500

    --- Auto-Git Commit ---

--- core/socket.py
+++ core/socket.py
@@ -11,10 +11,11 @@ class socket:
     USERS = set()
     producer_function=None
 
-    def __init__(self,function):
+    def __init__(self,producer_function):
         self.loop = asyncio.get_event_loop()
         self.loop.run_until_complete( websockets.serve(self.handler, 'localhost', 6789))
         self.loop.run_forever()
+        self.producer_function=producer_function
 
     async def notify(self, message):
         if self.USERS:       # asyncio.wait doesn't accept an empty list

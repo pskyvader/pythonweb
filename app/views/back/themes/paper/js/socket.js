26f188aee5f3b87d3901d55f0b33ffbbd0c2af82
mysitioc <mysitioc@mysitio.cl>
2019-03-22 14:27:10 -0500

    --- Auto-Git Commit ---

--- app/views/back/themes/paper/js/socket.js
+++ app/views/back/themes/paper/js/socket.js
@@ -3,7 +3,7 @@ websocket=null
 function log_socket(){
     ws=path.replace("https", "ws");
     ws=ws.replace("http", "ws");
-    websocket = new WebSocket(path + ":5678/");
+    websocket = new WebSocket(ws + ":5678/");
     console.log(websocket);
     websocket.onmessage = function(event) {
         data=event.data

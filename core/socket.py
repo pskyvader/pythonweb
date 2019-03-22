69ebea7ed544fe494d80e348dd76dfec56ef88f5
mysitioc <mysitioc@mysitio.cl>
2019-03-22 13:24:08 -0500

    --- Auto-Git Commit ---

--- core/socket.py
+++ core/socket.py
@@ -35,8 +35,7 @@ class socket:
     async def handler(self, websocket, path):
         # Register.
         self.USERS.add(websocket)
-        producer_task = asyncio.ensure_future(
-            self.producer_handler(websocket, path))
+        producer_task = asyncio.ensure_future( self.producer_handler(websocket, path))
         done, pending = await asyncio.wait([producer_task], return_when=asyncio.FIRST_COMPLETED)
         for task in pending:
             task.cancel()

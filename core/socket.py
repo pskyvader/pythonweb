9791b04a5fc0dd9c4177fb8d49dba7079c7e4a6d
mysitioc <mysitioc@mysitio.cl>
2019-03-22 12:03:21 -0500

    --- Auto-Git Commit ---

--- core/socket.py
+++ core/socket.py
@@ -1,18 +1,59 @@
 import asyncio
+import json
+import logging
 import websockets
 
 class socket:
-    
-    async def hello(websocket, path):
-        name = await websocket.recv()
-        print(f"< {name}")
 
-        greeting = f"Hello {name}!"
+    logging.basicConfig()
 
-        await websocket.send(greeting)
-        print(f"> {greeting}")
+    STATE = {'value': 0}
 
-    start_server = websockets.serve(hello, 'localhost', 8765)
+    USERS = set()
 
-    asyncio.get_event_loop().run_until_complete(start_server)
+    def state_event():
+        return json.dumps({'type': 'state', **STATE})
+
+    def users_event():
+        return json.dumps({'type': 'users', 'count': len(USERS)})
+
+    async def notify_state():
+        if USERS:       # asyncio.wait doesn't accept an empty list
+            message = state_event()
+            await asyncio.wait([user.send(message) for user in USERS])
+
+    async def notify_users():
+        if USERS:       # asyncio.wait doesn't accept an empty list
+            message = users_event()
+            await asyncio.wait([user.send(message) for user in USERS])
+
+    async def register(websocket):
+        USERS.add(websocket)
+        await notify_users()
+
+    async def unregister(websocket):
+        USERS.remove(websocket)
+        await notify_users()
+
+    async def counter(websocket, path):
+        # register(websocket) sends user_event() to websocket
+        await register(websocket)
+        try:
+            await websocket.send(state_event())
+            async for message in websocket:
+                data = json.loads(message)
+                if data['action'] == 'minus':
+                    STATE['value'] -= 1
+                    await notify_state()
+                elif data['action'] == 'plus':
+                    STATE['value'] += 1
+                    await notify_state()
+                else:
+                    logging.error(
+                        "unsupported event: {}", data)
+        finally:
+            await unregister(websocket)
+
+    asyncio.get_event_loop().run_until_complete(
+        websockets.serve(counter, 'localhost', 6789))
     asyncio.get_event_loop().run_forever()

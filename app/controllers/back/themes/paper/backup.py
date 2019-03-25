b6bef3c9a323d509a5be1a29c8ae855e474e953b
mysitioc <mysitioc@mysitio.cl>
2019-03-25 14:23:27 -0500

    --- Auto-Git Commit ---

--- app/controllers/back/themes/paper/backup.py
+++ app/controllers/back/themes/paper/backup.py
@@ -31,7 +31,7 @@ class backup(base):
     no_restore = ['backup/']
     sock=None
     host =app.root_url
-    port =8888
+    port =8001
 
     def __init__(self):
         backup.base_dir = app.get_dir(True)

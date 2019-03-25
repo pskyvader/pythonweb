2ef8c2db4d1987a04f87ba4d2ba9280e9865c1b4
mysitioc <mysitioc@mysitio.cl>
2019-03-25 13:29:10 -0500

    --- Auto-Git Commit ---

--- app/controllers/back/themes/paper/backup.py
+++ app/controllers/back/themes/paper/backup.py
@@ -339,9 +339,9 @@ class backup(base):
         c = configuracion_administrador()
         c.json(False)
         respuesta = {'exito': True, 'mensaje': ''}
-        print(self.sock)
+        print('socket',self.sock)
         self.sock.connect((self.host,self.port))
-        print(self.sock)
+        print('socket',self.sock)
 
         my_file = Path(self.dir_backup)
         if my_file.is_dir():

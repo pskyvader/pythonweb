4aaed639b8edc6b40d6b0a4e6f4728dd1c470749
mysitioc <mysitioc@mysitio.cl>
2019-03-25 12:14:29 -0500

    --- Auto-Git Commit ---

--- app/views/back/themes/paper/js/socket.js
+++ app/views/back/themes/paper/js/socket.js
@@ -4,7 +4,7 @@ function log_socket(){
     ws=path.replace("https", "ws");
     ws=ws.replace("http", "ws");
     ws=ws.replace("admin/", "");
-    websocket = new WebSocket(ws + ":80/");
+    websocket = new WebSocket(ws + ":80/hello-tornado");
     console.log(websocket);
     websocket.onmessage = function(event) {
         data=event.data

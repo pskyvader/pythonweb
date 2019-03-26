5ad33dc997880e8324514042c4ea1e8bb81febc1
mysitioc <mysitioc@mysitio.cl>
2019-03-26 11:19:27 -0500

    --- Auto-Git Commit ---

--- core/app.py
+++ core/app.py
@@ -243,8 +243,9 @@ class app:
         try:
             for key in p.keys():
                 if isinstance(p[key], FieldStorage):
-
-                    print(key,p[key].file)
+                    if not key in post:
+                        post[key]=[]
+                    post[key].append(p[key].file)
 
                 if isinstance(p[key], MiniFieldStorage):
                     post[key] = p[key].value

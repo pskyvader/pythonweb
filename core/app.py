ebaf2ef635fb3ed056c2aec54b58f01ca66236d5
mysitioc <mysitioc@mysitio.cl>
2019-03-26 13:55:00 -0500

    --- Auto-Git Commit ---

--- core/app.py
+++ core/app.py
@@ -246,12 +246,14 @@ class app:
                 if isinstance(p[key], FieldStorage) and p[key].file:
                     tmpfile=p[key].file.read()
                     name=escape(p[key].filename)
-                    if not key in post:
-                        post[key]=[]
+                    if not 'files' in post:
+                        post['files']={}
+                    if not key in post['files']:
+                        post['files'][key]=[]
                     mime = mimetypes.guess_type(name, False)[0]
                     if mime == None:
                         mime = 'text/plain'
-                    post[key].append({'name':name,'type':mime, 'tmp_name':tmpfile})
+                    post['files'][key].append({'name':name,'type':mime, 'tmp_name':tmpfile})
                 elif isinstance(p[key], MiniFieldStorage):
                     post[key] = p[key].value
                 elif isinstance(p[key], list):

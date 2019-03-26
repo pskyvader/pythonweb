c3d8c89225ebbb35642381e63d9e027734dd7939
mysitioc <mysitioc@mysitio.cl>
2019-03-26 14:39:16 -0500

    --- Auto-Git Commit ---

--- core/functions.py
+++ core/functions.py
@@ -269,32 +269,32 @@ class functions():
 
     @staticmethod
     def get_content_type_by_filename(file_name):
-    mime_type = ""
-    mime_map = {}
-    mime_map["js"] = "application/javascript"
-    mime_map["xlsx"] = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-    mime_map["xltx"] = "application/vnd.openxmlformats-officedocument.spreadsheetml.template"
-    mime_map["potx"] = "application/vnd.openxmlformats-officedocument.presentationml.template"
-    mime_map["ppsx"] = "application/vnd.openxmlformats-officedocument.presentationml.slideshow"
-    mime_map["pptx"] = "application/vnd.openxmlformats-officedocument.presentationml.presentation"
-    mime_map["sldx"] = "application/vnd.openxmlformats-officedocument.presentationml.slide"
-    mime_map["docx"] = "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
-    mime_map["dotx"] = "application/vnd.openxmlformats-officedocument.wordprocessingml.template"
-    mime_map["xlam"] = "application/vnd.ms-excel.addin.macroEnabled.12"
-    mime_map["xlsb"] = "application/vnd.ms-excel.sheet.binary.macroEnabled.12"
-    mime_map["apk"] = "application/vnd.android.package-archive"
-    try:
-        suffix = ""
-        name = os.path.basename(file_name)
-        suffix = name.split('.')[-1]
-        if suffix in mime_map.keys():
-            mime_type = mime_map[suffix] 
-        else:
-            import mimetypes
-            mimetypes.init()
-            mime_type = mimetypes.types_map["." + suffix]
-    except Exception:
-        mime_type = 'application/octet-stream'
-    if not mime_type:
-        mime_type = 'application/octet-stream'
-    return mime_type 
+        mime_type = ""
+        mime_map = {}
+        mime_map["js"] = "application/javascript"
+        mime_map["xlsx"] = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+        mime_map["xltx"] = "application/vnd.openxmlformats-officedocument.spreadsheetml.template"
+        mime_map["potx"] = "application/vnd.openxmlformats-officedocument.presentationml.template"
+        mime_map["ppsx"] = "application/vnd.openxmlformats-officedocument.presentationml.slideshow"
+        mime_map["pptx"] = "application/vnd.openxmlformats-officedocument.presentationml.presentation"
+        mime_map["sldx"] = "application/vnd.openxmlformats-officedocument.presentationml.slide"
+        mime_map["docx"] = "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+        mime_map["dotx"] = "application/vnd.openxmlformats-officedocument.wordprocessingml.template"
+        mime_map["xlam"] = "application/vnd.ms-excel.addin.macroEnabled.12"
+        mime_map["xlsb"] = "application/vnd.ms-excel.sheet.binary.macroEnabled.12"
+        mime_map["apk"] = "application/vnd.android.package-archive"
+        try:
+            suffix = ""
+            name = os.path.basename(file_name)
+            suffix = name.split('.')[-1]
+            if suffix in mime_map.keys():
+                mime_type = mime_map[suffix] 
+            else:
+                import mimetypes
+                mimetypes.init()
+                mime_type = mimetypes.types_map["." + suffix]
+        except Exception:
+            mime_type = 'application/octet-stream'
+        if not mime_type:
+            mime_type = 'application/octet-stream'
+        return mime_type 

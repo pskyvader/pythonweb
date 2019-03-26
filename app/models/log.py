42341d5bd72c7e76f90f170588f864a9cc449fe9
mysitioc <mysitioc@mysitio.cl>
2019-03-26 10:27:56 -0500

    --- Auto-Git Commit ---

--- app/models/log.py
+++ app/models/log.py
@@ -10,43 +10,6 @@ class log(base_model):
     table = 'log'
     delete_cache = False
     @classmethod
-    def getAll(cls, where={}, condiciones={}, select=""):
-        return_total = None
-        connection = database.instance()
-        fields     = table.getByname(cls.table)
-
-        if 'order' not in condiciones and 'orden' in fields:
-            condiciones['order'] = 'fecha DESC'
-
-        if 'palabra' in condiciones:
-            condiciones['buscar'] = {}
-            if 'tabla' in fields:
-                condiciones['buscar']['tabla'] = condiciones['palabra']
-            if 'accion' in fields:
-                condiciones['buscar']['accion'] = condiciones['palabra']
-            if 'administrador' in fields:
-                condiciones['buscar']['administrador'] = condiciones['palabra']
-
-            if len(condiciones['buscar']) == 0:
-                del condiciones['buscar']
-
-        if select == 'total':
-            return_total = True
-
-        row = connection.get(cls.table, cls.idname, where, condiciones, select)
-        
-        if not return_total:
-            for v in row:
-                v['fecha']=v[4]=v['fecha'].strftime("%Y-%m-%d %H:%M:%S")
-
-
-
-        if return_total != None:
-            return len(row)
-        else:
-            return row
-
-    @classmethod
     def insert(cls, set_query: dict,  loggging=True):
         fields     = table.getByname(cls.table)
         insert = database.create_data(fields, set_query)

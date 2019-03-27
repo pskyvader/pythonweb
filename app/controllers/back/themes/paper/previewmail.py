a3743c005ab4afc12f162935e8af80104393d229
mysitioc <mysitioc@mysitio.cl>
2019-03-27 11:36:16 -0500

    --- Auto-Git Commit ---

--- app/controllers/back/themes/paper/previewmail.py
+++ app/controllers/back/themes/paper/previewmail.py
@@ -6,14 +6,14 @@ from core.view import view
 class previewmail():
     @classmethod
     def index(cls, var):
-        nombre='pablo'
-        nombre_sitio='sitio python'
-        password='123456789'
+        nombre = 'pablo'
+        nombre_sitio = 'sitio python'
+        password = '123456789'
         body_email = {
             'body': view.get_theme() + 'mail/recuperar_password.html',
             'titulo': "Recuperación de contraseña",
             'cabecera': "Estimado " + nombre + ", se ha solicitado la recuperación de contraseña en " + nombre_sitio,
-                        'campos': {'Contraseña (sin espacios)': password},
-                        'campos_largos': {},
+            'campos': {'Contraseña (sin espacios)': password},
+            'campos_largos': {},
         }
         body = email.body_email(body_email)

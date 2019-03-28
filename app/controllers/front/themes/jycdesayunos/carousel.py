2eed3558748c5fdfbe717ad35d2f18f7d048aed9
mysitioc <mysitioc@mysitio.cl>
2019-03-28 11:34:52 -0500

    --- Auto-Git Commit ---

--- app/controllers/front/themes/jycdesayunos/carousel.py
+++ app/controllers/front/themes/jycdesayunos/carousel.py
@@ -32,82 +32,45 @@ class carousel:
 
 
 
+            carousel = []
+            for key, c in row_carousel:
+                foto = image.generar_url(c, 'foto1')
+                if foto!='':
 
+                    srcset = self.srcset(c)
 
-                    
-
-                portada = image.portada(b["foto"])
-                foto = image.generar_url(portada, 'foto1')
-                if foto != '':
-                    thumb.append({'id': key, 'active': (key == 0)})
-
-                    srcset = self.srcset(portada)
-
-                    banner.append({
-                        'srcset': srcset,
-                        'title': b['titulo'],
-                        'active': (key == 0),
+                    carousel.append({
+                        'id'       : key,
+                        'srcset'   : srcset,
+                        'title'    : titulo,
+                        'active'   : (0 == key),
                         'data': 'data-' if (key != 0) else '',
-                        'foto': foto,
-                        'texto1': b['texto1'],
-                        'texto2': b['texto2'],
-                        'link': functions.ruta(b['link']),
-                        'background': image.generar_url(portada, 'color'),
+                        'foto'     : foto,
+                        'original' : image.generar_url(c, '')
                     })
 
-            data = {}
+            data={}
+                    
             data['thumb'] = thumb
-            data['banner'] = banner
-            ret['body'].append(('banner', data))
+            data['carousel'] = carousel
+            
+            ret['body'].append(('carousel', data))
         return ret
 
 
 
-
-                   
-                }
-            }
-
-            carousel = array()
-            foreach (row_carousel as key : c) {
-                if (isset(c)) {
-                    foto = image::generar_url(c, 'foto1')
-                } else {
-                    foto = ''
-                }
-                if ('' != foto) {
-
-                    srcset = this->srcset(c)
-
-                    carousel[] = array(
-                        'id'       : key,
-                        'srcset'   : srcset,
-                        'title'    : titulo,
-                        'active'   : (0 == key) ? 'active' : '',
-                        'data'     : (0 != key) ? 'data-' : '',
-                        'foto'     : foto,
-                        'original' : image::generar_url(c, ''),
-                    )
-                }
-            }
-            view::set('thumb', thumb)
-            view::set('carousel', carousel)
-            view::render('carousel')
-        }
-    }
-
     public function srcset(foto_base)
     {
-        images = self::sizes
+        images = self.sizes
         srcset = array()
         foreach (images as k : size) {
-            foto = image::generar_url(foto_base, size['foto'], 'webp')
+            foto = image.generar_url(foto_base, size['foto'], 'webp')
             if ('' != foto) {
                 srcset[] = array('media' : '(min-width: ' . size['size'] . 'px)', 'url' : foto, 'type' : 'image/webp')
             }
         }
         foreach (images as k : size) {
-            foto = image::generar_url(foto_base, size['foto'])
+            foto = image.generar_url(foto_base, size['foto'])
             if ('' != foto) {
                 srcset[] = array('media' : '(min-width: ' . size['size'] . 'px)', 'url' : foto, 'type' : 'image/jpg')
             }

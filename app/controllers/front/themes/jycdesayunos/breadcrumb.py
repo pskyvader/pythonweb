c6c91f98c1c9cc67c369eeb5bcee91dc4ce52d4c
mysitioc <mysitioc@mysitio.cl>
2019-03-28 11:18:37 -0500

    --- Auto-Git Commit ---

---
+++ app/controllers/front/themes/jycdesayunos/breadcrumb.py
@@ -0,0 +1,35 @@
+from core.functions import functions
+
+
+from app.models.seo import seo as seo_model
+
+
+class breadcrumb:
+    def normal(breadcrumb=[]):
+        seo=seo_model.getById(1)
+        b = [
+            {'url' : functions.generar_url(array(seo['url'])), 'title' :seo['titulo']},
+        ]
+        b=array_merge(b,breadcrumb)
+        foreach (b as key : bread) {
+            b[key]['is_active']=false
+            b[key]['active']=''
+        }
+        last=array_pop(b)
+        last['is_active']=true
+        last['active']='active'
+        b[]=last
+
+        view.set('breadcrumb', b)
+        last=array_pop(b)
+        view.set('titulo', last['title'])
+        if(count(b)>1){
+            last=array_pop(b)
+            view.set('subtitulo', last['title'])
+            view.set('is_subtitulo', true)
+        }else{
+            view.set('subtitulo', '')
+            view.set('is_subtitulo', false)
+        }
+        view.render('breadcrumb')
+    }

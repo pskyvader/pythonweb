3952c76c8a2c662ac9791451dbf5176193b9fde2
mysitioc <mysitioc@mysitio.cl>
2019-03-28 11:36:07 -0500

    --- Auto-Git Commit ---

--- app/controllers/front/themes/jycdesayunos/carousel.py
+++ app/controllers/front/themes/jycdesayunos/carousel.py
@@ -1,4 +1,6 @@
 from core.image import image
+
+
 class carousel:
     sizes = [
         {'foto': 'foto1', 'size': '1200'},
@@ -7,74 +9,66 @@ class carousel:
         {'foto': 'foto4', 'size': '0'},
     ]
 
-
-    def normal(self, row_carousel=[],titulo=''):
+    def normal(self, row_carousel=[], titulo=''):
         ret = {'body': []}
         if len(row_carousel) > 0:
             thumb = []
-            
+
             for key, c in row_carousel:
                 foto = image.generar_url(c, 'thumb_carousel')
 
-
-                if foto!='':
+                if foto != '':
                     foto_w = image.generar_url(c, 'thumb_carousel', 'webp')
-                    if foto_w!='':
-                        srcset = [{'url' : foto_w, 'type' : 'image/webp'}]
-                    
+                    if foto_w != '':
+                        srcset = [{'url': foto_w, 'type': 'image/webp'}]
+
                     thumb.append({
-                        'srcset' : srcset,
-                        'id'     : key,
-                        'title'  : titulo,
-                        'active' : (0 == key),
-                        'foto'   : foto,
+                        'srcset': srcset,
+                        'id': key,
+                        'title': titulo,
+                        'active': (0 == key),
+                        'foto': foto,
                     })
-
-
 
             carousel = []
             for key, c in row_carousel:
                 foto = image.generar_url(c, 'foto1')
-                if foto!='':
+                if foto != '':
 
                     srcset = self.srcset(c)
 
                     carousel.append({
-                        'id'       : key,
-                        'srcset'   : srcset,
-                        'title'    : titulo,
-                        'active'   : (0 == key),
+                        'id': key,
+                        'srcset': srcset,
+                        'title': titulo,
+                        'active': (0 == key),
                         'data': 'data-' if (key != 0) else '',
-                        'foto'     : foto,
-                        'original' : image.generar_url(c, '')
+                        'foto': foto,
+                        'original': image.generar_url(c, '')
                     })
 
-            data={}
-                    
+            data = {}
+
             data['thumb'] = thumb
             data['carousel'] = carousel
-            
+
             ret['body'].append(('carousel', data))
         return ret
 
-
-
-    public function srcset(foto_base)
-    {
+    def srcset(self, foto_base):
         images = self.sizes
-        srcset = array()
-        foreach (images as k : size) {
+        srcset = []
+
+        for size in images:
             foto = image.generar_url(foto_base, size['foto'], 'webp')
-            if ('' != foto) {
-                srcset[] = array('media' : '(min-width: ' . size['size'] . 'px)', 'url' : foto, 'type' : 'image/webp')
-            }
-        }
-        foreach (images as k : size) {
+            if foto != '':
+                srcset.append(
+                    {'media': '(min-width: ' + size['size'] + 'px)', 'url': foto, 'type': 'image/webp'})
+
+        for size in images:
             foto = image.generar_url(foto_base, size['foto'])
-            if ('' != foto) {
-                srcset[] = array('media' : '(min-width: ' . size['size'] . 'px)', 'url' : foto, 'type' : 'image/jpg')
-            }
-        }
+            if foto != '':
+                srcset.append(
+                    {'media': '(min-width: ' + size['size'] + 'px)', 'url': foto, 'type': 'image/jpg'})
+
         return srcset
-    }
-}

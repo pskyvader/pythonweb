ba60cd7eb77f8ceb1e96c56df1d0fe6f5e505b11
mysitioc <mysitioc@mysitio.cl>
2019-03-28 11:24:22 -0500

    --- Auto-Git Commit ---

--- app/controllers/front/themes/jycdesayunos/breadcrumb.py
+++ app/controllers/front/themes/jycdesayunos/breadcrumb.py
@@ -6,30 +6,29 @@ from app.models.seo import seo as seo_model
 
 class breadcrumb:
     def normal(breadcrumb=[]):
-        seo=seo_model.getById(1)
+        ret = {'body': []}
+        seo = seo_model.getById(1)
         b = [
-            {'url' : functions.generar_url(array(seo['url'])), 'title' :seo['titulo']},
+            {'url': functions.generar_url(
+                array(seo['url'])), 'title':seo['titulo']},
         ]
-        b=b+breadcrumb
-        foreach (b as key : bread) {
-            b[key]['is_active']=false
-            b[key]['active']=''
-        }
-        last=array_pop(b)
-        last['is_active']=true
-        last['active']='active'
-        b[]=last
+        b = b+breadcrumb
+        for bread in b:
+            if b.index(bread) == len(b)-1:
+                bread['active'] = True
+            else:
+                bread['active'] = False
 
-        view.set('breadcrumb', b)
-        last=array_pop(b)
-        view.set('titulo', last['title'])
-        if(count(b)>1){
-            last=array_pop(b)
-            view.set('subtitulo', last['title'])
-            view.set('is_subtitulo', true)
-        }else{
-            view.set('subtitulo', '')
-            view.set('is_subtitulo', false)
-        }
-        view.render('breadcrumb')
-    }
+        data = {}
+        data['breadcrumb'] = b.copy()
+        last = b.pop()
+        data['titulo'] = last['title']
+
+        if len(b) > 1:
+            last = b.pop()
+            data['subtitulo'] = last['title']
+        else:
+            data['subtitulo'] = ''
+
+        ret['body'].append(('breadcrumb', data))
+        return ret

de947e8debf07f8bbeb9f41359e0de9b93e06214
mysitioc <mysitioc@mysitio.cl>
2019-03-28 13:40:45 -0500

    --- Auto-Git Commit ---

--- app/controllers/front/themes/jycdesayunos/cmscategory.py
+++ app/controllers/front/themes/jycdesayunos/cmscategory.py
@@ -12,13 +12,12 @@ from .header import header
 from .banner import banner
 from .breadcrumb import breadcrumb
 from .footer import footer
+from .carousel import carousel
 
 
 class cmscategory(base):
     def __init__(self):
         super().__init__(app.get["idseo"])
-
-
 
     def index(self):
         ret = {"body": []}
@@ -39,9 +38,7 @@ class cmscategory(base):
         ret["body"] += he.normal()["body"]
 
         ba = banner()
-        ret["body"] += ba.individual(
-            self.seo["banner"], self.metadata["title"]
-        )["body"]
+        ret["body"] += ba.individual(self.seo["banner"], self.metadata["title"])["body"]
 
         bc = breadcrumb()
         ret["body"] += bc.normal(self.breadcrumb)["body"]
@@ -54,46 +51,32 @@ class cmscategory(base):
             var["idpadre"] = 0
 
         row = seccioncategoria_model.getAll(var)
-        sidebar = []
-        for s in row:
-            sidebar.append(
-                {
-                    "title": s["titulo"],
-                    "active": "",
-                    "url": functions.url_seccion([self.url[0], "detail"], s),
-                }
-            )
-
+        categories = self.lista(row)
         data = {}
-
-        data["title_category"] = self.seo["titulo"]
-        data["sidebar"] = sidebar
-
-        data["description"] = ""
+        data["list"] = categories
         ret["body"].append(("cms-sidebar", data))
+
         f = footer()
         ret["body"] += f.normal()["body"]
         return ret
 
 
-
-
-
-
-
-
-
-
-
-
-    def index(self):
+    def detail(self, var=[]):
         ret = {"body": []}
-        self.meta(self.seo)
+        if len(var) > 0:
+            id = functions.get_idseccion(var[0])
+            categoria = seccioncategoria_model.getById(id)
+            if 0 in categoria:
+                self.url = functions.url_seccion([self.url[0], "detail"], categoria, True)
+                self.breadcrumb.append( {"url": functions.generar_url(self.url), "title": categoria["titulo"]} )
+
         url_return = functions.url_redirect(self.url)
         if url_return != "":
             ret["error"] = 301
             ret["redirect"] = url_return
             return ret
+
+        self.meta(categoria)
 
         h = head(self.metadata)
         ret_head = h.normal()
@@ -105,43 +88,69 @@ class cmscategory(base):
         ret["body"] += he.normal()["body"]
 
         ba = banner()
-        ret["body"] += ba.individual(
-            self.seo["banner"], self.metadata["title"], self.seo["subtitulo"]
-        )["body"]
+        ret["body"] += ba.individual(self.seo["banner"], self.seo["titulo"])["body"]
 
         bc = breadcrumb()
         ret["body"] += bc.normal(self.breadcrumb)["body"]
+
+
+        data={}
+        data['title']= categoria['titulo']
+        data['description']= categoria['descripcion']
+        ret["body"].append(("title-text", data.copy()))
 
         var = {}
         if self.seo["tipo_modulo"] != 0:
             var["tipo"] = self.seo["tipo_modulo"]
 
         if self.modulo["hijos"]:
-            var["idpadre"] = 0
+            var["idpadre"] = categoria[0]
 
-        row = seccion_model.getAll(var)
-        sidebar = []
-        for s in row:
-            sidebar.append(
-                {
-                    "title": s["titulo"],
-                    "active": "",
-                    "url": functions.url_seccion([self.url[0], "detail"], s),
-                }
-            )
-
+        row = seccioncategoria_model.getAll(var)
+        categories = self.lista(row)
         data = {}
+        data["list"] = categories
+        ret["body"].append(("grid-border-3", data.copy()))
 
-        data["title_category"] = self.seo["titulo"]
-        data["sidebar"] = sidebar
 
-        data["description"] = ""
-        ret["body"].append(("cms-sidebar", data))
+
+
+
+
+        var = {}
+        if self.seo["tipo_modulo"] != 0:
+            var["tipo"] = self.seo["tipo_modulo"]
+
+        if self.modulo["hijos"]:
+            var[seccioncategoria_model.idname] = categoria[0]
+
+        
+        row = seccion_model.getAll(var)
+
+        if len(row) > 0:
+            ret["body"].append(("title", {'title':'Secciones'}))
+            secciones = self.lista(row, 'sub', 'lista')
+            ret["body"].append(("grid-3", {'list':secciones}))
+
         f = footer()
         ret["body"] += f.normal()["body"]
         return ret
 
-    def detail(self, var=[]):
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    def sub(self, var=[]):
         ret = {"body": []}
         if len(var) > 0:
             id = functions.get_idseccion(var[0])
@@ -170,52 +179,21 @@ class cmscategory(base):
         ret["body"] += he.normal()["body"]
 
         ba = banner()
-        ret["body"] += ba.individual(self.seo["banner"], self.seo["subtitulo"])["body"]
+        ret["body"] += ba.individual(self.seo["banner"], self.seo["titulo"])["body"]
 
         bc = breadcrumb()
         ret["body"] += bc.normal(self.breadcrumb)["body"]
 
-        var = {}
-        if self.seo["tipo_modulo"] != 0:
-            var["tipo"] = self.seo["tipo_modulo"]
-
-        if self.modulo["hijos"]:
-            var["idpadre"] = 0
-
-        row = seccion_model.getAll(var)
-        sidebar = []
-        for s in row:
-            sidebar.append(
-                {
-                    "title": s["titulo"],
-                    "active": "",
-                    "url": functions.url_seccion([self.url[0], "detail"], s),
-                }
-            )
-
-        extra = ""
-        if len(seccion["archivo"]) > 0:
-            files = []
-            for a in seccion["archivo"]:
-                files.append(
-                    {
-                        "title": a["url"],
-                        "size": functions.file_size(file.generar_dir(a, "")),
-                        "url": file.generar_url(a, ""),
-                    }
-                )
-            data = {}
-            data["files"] = files
-            data["title"] = "Archivos"
-            extra = ("file", data.copy())
 
         data = {}
-        data["sidebar"] = sidebar
-        data["title_category"] = self.seo["titulo"]
         data["title"] = seccion["titulo"]
         data["description"] = seccion["descripcion"]
-        data["extra"] = extra
-        ret["body"].append(("cms-sidebar", data))
+        ret["body"].append(("title-text", data))
+
+        ret["body"].append(("title", {'title':'Galería'}))
+
+        car =  carousel()
+        $carousel->normal($seccion['foto'], $seccion['titulo']);
 
         f = footer()
         ret["body"] += f.normal()["body"]

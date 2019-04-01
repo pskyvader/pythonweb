e3794b5a6d52c576723e682dd7c43a5f35bb4b37
mysitioc <mysitioc@mysitio.cl>
2019-04-01 13:28:57 -0500

    --- Auto-Git Commit ---

--- app/controllers/front/themes/jycdesayunos/product.py
+++ app/controllers/front/themes/jycdesayunos/product.py
@@ -65,29 +65,40 @@ class product(base):
     
 
     def category(self,var = []):
+        ret = {"body": []}
         if len(var)>0:
             id        = functions.get_idseccion(var[0])
             categoria = productocategoria_model.getById(id)
-            if (isset(categoria[0])) {
-                this.url          = functions.url_seccion(array(this.url[0], 'category'), categoria, true)
-                this.breadcrumb[] = array('url' => functions.generar_url(this.url), 'title' => categoria['titulo'])
-            }
-        }
-        functions.url_redirect(this.url)
-        this.meta(categoria)
+            if len(categoria)>0:
+                self.url          = functions.url_seccion([self.url[0], 'category'], categoria, True)
+                self.breadcrumb.append({'url' : functions.generar_url(self.url), 'title' : categoria['titulo']})
+            
+        
+        url_return = functions.url_redirect(self.url)
+        if url_return != "":
+            ret["error"] = 301
+            ret["redirect"] = url_return
+            return ret
+        self.meta(categoria)
 
-        head = new head(this.metadata)
-        head.normal()
 
-        header = new header()
-        header.normal()
+        h = head(self.metadata)
+        ret_head = h.normal()
+        if ret_head["headers"] != "":
+            return ret_head
+        ret["body"] += ret_head["body"]
 
-        banner = new banner()
-        banner.individual(this.seo['banner'], this.seo['titulo'],this.metadata['title'])
+        he = header()
+        ret["body"] += he.normal()["body"]
 
-        #breadcrumb = new breadcrumb()
-        #breadcrumb.normal(this.breadcrumb)
-        pl           = new product_list() #product_list.php
+        ba = banner()
+        ret["body"] += ba.individual(self.seo["banner"], self.seo["titulo"],self.metadata['title'])["body"]
+
+        bc = breadcrumb()
+        ret["body"] += bc.normal(self.breadcrumb)["body"]
+
+
+        pl           = product_list() #product_list.php
         product_list = pl.product_list(categoria) #Lista de productos, renderiza vista
         sidebar      = pl.sidebar(categoria) # genera sidebar, renderiza vista
         pl.orden_producto() # genera lista de filtros
@@ -95,13 +106,17 @@ class product(base):
         pl.pagination() # genera paginador
         pl.is_search() # Genera texto de busqueda, si existe
 
-        view.set('product_list', product_list)
-        view.set('sidebar', sidebar)
-        view.render('product/category')
 
-        footer = new footer()
-        footer.normal()
-    }
+        data={}
+        data['product_list']= product_list
+        data['sidebar']= sidebar
+        
+        ret["body"].append(("product/category", data))
+
+        f = footer()
+        ret["body"] += f.normal()["body"]
+        return ret
+        
 
     public function detail(var = array())
     {
@@ -109,38 +124,38 @@ class product(base):
             id       = functions.get_idseccion(var[0])
             producto = producto_model.getById(id)
             if (isset(producto[0])) {
-                this.url          = functions.url_seccion(array(this.url[0], 'detail'), producto, true)
-                this.breadcrumb[] = array('url' => functions.generar_url(this.url), 'title' => producto['titulo'])
+                self.url          = functions.url_seccion(array(self.url[0], 'detail'), producto, True)
+                self.breadcrumb[] = array('url' : functions.generar_url(self.url), 'title' : producto['titulo'])
             }
         }
-        functions.url_redirect(this.url)
-        this.meta(producto)
+        functions.url_redirect(self.url)
+        self.meta(producto)
 
-        head = new head(this.metadata)
+        head = new head(self.metadata)
         head.normal()
 
         header = new header()
         header.normal()
 
         banner = new banner()
-        banner.individual(this.seo['banner'], producto['titulo'])
+        banner.individual(self.seo['banner'], producto['titulo'])
 
         #breadcrumb = new breadcrumb()
-        #breadcrumb.normal(this.breadcrumb)
+        #breadcrumb.normal(self.breadcrumb)
         pl      = new product_list() #product_list.php
         sidebar = pl.sidebar() # genera sidebar, renderiza vista
-        pd      = new product_detail(producto, this.url)
+        pd      = new product_detail(producto, self.url)
         tabs    = pd.tabs()
         pd.galeria()
         pd.resumen()
 
         view.set('sidebar', sidebar)
         view.set('tabs', tabs)
-        view.set('url', functions.generar_url(this.url))
-        if(this.metadata['image']!=''){
-            view.set('imagen_portada', this.metadata['image'])
+        view.set('url', functions.generar_url(self.url))
+        if(self.metadata['image']!=''){
+            view.set('imagen_portada', self.metadata['image'])
         }else{
-            view.set('imagen_portada', this.metadata['logo'])
+            view.set('imagen_portada', self.metadata['logo'])
         }
 
         view.render('product/detail')

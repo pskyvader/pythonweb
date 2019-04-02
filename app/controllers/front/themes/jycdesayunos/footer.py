33c41807a26e4c847fc7eab9f3e0d18e45335df9
mysitioc <mysitioc@mysitio.cl>
2019-04-02 12:31:55 -0500

    --- Auto-Git Commit ---

--- app/controllers/front/themes/jycdesayunos/footer.py
+++ app/controllers/front/themes/jycdesayunos/footer.py
@@ -76,13 +76,12 @@ class footer:
 
             l = {"title": "Productos", "links": [], "size": 3}
             row = productocategoria_model.getAll({"tipo": 1, "idpadre": 0})
+            print(row)
             seo = seo_model.getById(8)
             for productos in row:
                 l["links"].append(
                     {
-                        "url": functions.url_seccion(
-                            [seo["url"], "category"], productos
-                        ),
+                        "url": functions.url_seccion( [seo["url"], "category"], productos ),
                         "title": productos["titulo"],
                     }
                 )

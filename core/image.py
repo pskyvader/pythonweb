857783f80e8b6148221b7d36c0d68fafc00c9fed
mysitioc <mysitioc@mysitio.cl>
2019-04-02 11:47:38 -0500

    --- Auto-Git Commit ---

--- core/image.py
+++ core/image.py
@@ -385,6 +385,7 @@ class image:
             box = (x, y, ancho_maximo+x, alto_maximo+y)
             im.thumbnail((miniatura_ancho, miniatura_alto), Image.ANTIALIAS)
             print((miniatura_ancho, miniatura_alto),box)
+            new_im = im.crop
             #new_im = im.crop(box)
         elif "rellenar" == tipo:
             if "png" == imagen_tipo:

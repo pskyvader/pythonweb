6f3a1a76b3798b9ddd73c3158b60d0bbd8f48940
mysitioc <mysitioc@mysitio.cl>
2019-04-02 12:18:24 -0500

    --- Auto-Git Commit ---

--- core/image.py
+++ core/image.py
@@ -412,8 +412,8 @@ class image:
                 else:
                     new_im = Image.new( 'RGB', (ancho_maximo, alto_maximo), (255, 255, 255))
 
-                box = (x, y, ancho_maximo+x, alto_maximo+y)
-                #box = (x, y)
+                #box = (x, y, ancho_maximo+x, alto_maximo+y)
+                box = (x, y)
                 im=im.resize((miniatura_ancho, miniatura_alto), Image.ANTIALIAS)
                 new_im.paste(im, (box))
 

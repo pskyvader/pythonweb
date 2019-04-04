0b6d605fe66e15246b106ecadcef78e1a1bf72d8
mysitioc <mysitioc@mysitio.cl>
2019-04-04 08:52:09 -0500

    --- Auto-Git Commit ---

--- app/controllers/front/themes/jycdesayunos/product.py
+++ app/controllers/front/themes/jycdesayunos/product.py
@@ -98,7 +98,7 @@ class product(base):
         bc = breadcrumb()
         ret["body"] += bc.normal(self.breadcrumb)["body"]
 
-        pl = product_list()  # product_list.php
+        pl = product_list()  # product_list.py
         prod_list = pl.product_list(categoria)  # Lista de productos, renderiza vista
         sidebar = pl.sidebar(categoria)  # genera sidebar, renderiza vista
         pl.orden_producto()  # genera lista de filtros

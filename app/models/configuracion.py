498a7596ea0c6b74e73081c606b65641b0605f8e
mysitioc <mysitioc@mysitio.cl>
2019-04-07 16:07:14 -0500

    --- Auto-Git Commit ---

--- app/models/configuracion.py
+++ app/models/configuracion.py
@@ -3,28 +3,34 @@ from .base_model import base_model
 
 
 class configuracion(base_model):
-    idname = 'idconfiguracion'
-    table = 'configuracion'
+    idname = "idconfiguracion"
+    table = "configuracion"
 
     @classmethod
-    def getByVariable(cls, variable: str):
-        where = {'variable': variable}
-        condicion = {'limit': 1}
+    def getByVariable(cls, variable: str, default=None):
+        where = {"variable": variable}
+        condicion = {"limit": 1}
         connection = database.instance()
         row = connection.get(cls.table, cls.idname, where, condicion)
-        return row[0]['valor'] if (len(row) == 1) else False
+        if len(row) == 1:
+            return row[0]["valor"]
+        else:
+            if default == None:
+                return False
+            else:
+                configuracion.setByVariable(variable, default)
+                return default
 
     @classmethod
     def setByVariable(cls, variable: str, valor: str):
-        where = {'variable': variable}
-        condicion = {'limit': 1}
+        where = {"variable": variable}
+        condicion = {"limit": 1}
         connection = database.instance()
         row = connection.get(cls.table, cls.idname, where, condicion)
 
         if len(row) == 0:
-            row = cls.insert({'variable': variable, 'valor': valor})
+            row = cls.insert({"variable": variable, "valor": valor})
         else:
-            row = cls.update(
-                {'variable': variable, 'valor': valor, 'id': row[0][0]})
+            row = cls.update({"variable": variable, "valor": valor, "id": row[0][0]})
 
         return row

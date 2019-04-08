e4ccf2cb82e2f558ee3d54d7a527522687e651c7
mysitioc <mysitioc@mysitio.cl>
2019-04-08 13:34:42 -0500

    --- Auto-Git Commit ---

--- app/controllers/front/themes/jycdesayunos/order.py
+++ app/controllers/front/themes/jycdesayunos/order.py
@@ -320,7 +320,7 @@ class order(base):
                     h["selected"] = True
                 else:
                     h["selected"] = False
-            d["horarios_entrega"]=d["horarios_entrega"].values()
+            d["horarios_entrega"]=d["horarios_entrega"].copy().values()
             direcciones.append(d)
 
         sidebar = order.sidebar(carro)

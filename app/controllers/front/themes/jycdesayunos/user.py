f8e787ecc1c10709f001c6a046c42519efdb5b85
mysitioc <mysitioc@mysitio.cl>
2019-04-08 13:09:25 -0500

    --- Auto-Git Commit ---

--- app/controllers/front/themes/jycdesayunos/user.py
+++ app/controllers/front/themes/jycdesayunos/user.py
@@ -630,8 +630,6 @@ class user(base):
             if len(var) > 0:
                 pedido = pedido_model.getByCookie(var[0], False)
                 # Podria desaparecer si se necesita que cualquier pedido sea publico
-                print(pedido)
-                print(pedido["idusuario"])
                 if ( "idusuario" in pedido and pedido["idusuario"] == app.session[usuario_model.idname + app.prefix_site] ):
                     self.url.append("pedido")
                     self.url.append(var[0])

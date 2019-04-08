a70e7bd46d49e10a00389711e626cbe4f634e5d6
mysitioc <mysitioc@mysitio.cl>
2019-04-08 13:28:26 -0500

    --- Auto-Git Commit ---

--- app/controllers/front/themes/jycdesayunos/order.py
+++ app/controllers/front/themes/jycdesayunos/order.py
@@ -296,7 +296,7 @@ class order(base):
 
 
             hora_entrega=fecha_entrega =  0 if dp["fecha_entrega"]=='0000-00-00 00:00:00' else datetime.strptime(dp["fecha_entrega"], "%Y-%m-%d %H:%M:%S")
-            fecha_entrega =  "" if fecha_entrega < functions.current_time(as_string=False) else functions.formato_fecha(fecha_entrega, "%F") 
+            fecha_entrega =  "" if fecha_entrega.timestamp() < functions.current_time(as_string=False) else functions.formato_fecha(fecha_entrega, "%F") 
 
             hora_entrega = (
                 ""

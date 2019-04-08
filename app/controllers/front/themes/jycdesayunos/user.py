9b2c8a2823bf46fa67cd4d8afdecf8bd9c4019d6
mysitioc <mysitioc@mysitio.cl>
2019-04-08 12:45:37 -0500

    --- Auto-Git Commit ---

--- app/controllers/front/themes/jycdesayunos/user.py
+++ app/controllers/front/themes/jycdesayunos/user.py
@@ -429,6 +429,7 @@ class user(base):
                 else:
                     campos_opcionales.append(m)
 
+        print(campos_opcionales)
         data["campos_requeridos"] = campos_requeridos
         data["campos_opcionales"] = campos_opcionales
         data["title"] = direccion["titulo"] if direccion != None else "Nueva dirección"

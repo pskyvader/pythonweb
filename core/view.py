66e47b8321809eca9120d43ff566624872fd31fe
mysitioc <mysitioc@mysitio.cl>
2019-04-29 11:27:52 -0500

    --- Auto-Git Commit ---

--- core/view.py
+++ core/view.py
@@ -43,8 +43,8 @@ class view:
         #custom filter
         def phone_number(number):
             """Convert a 10 character string into (xxx) xxx-xxxx."""
-            first = number[-8:]
-            second = number[-4:8]
+            first = number[-8:4]
+            second = number[-4:4]
             third = number[-1:4]
             return '(' + first + ')' + ' ' + second + '-' + third
         env.filters['phone_number'] = phone_number

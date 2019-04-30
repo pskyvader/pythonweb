5f98a531d3d57b9052f27f60cc216ad4b2e5cdf8
mysitioc <mysitioc@mysitio.cl>
2019-04-30 12:08:55 -0500

    --- Auto-Git Commit ---

--- core/app.py
+++ core/app.py
@@ -259,16 +259,6 @@ class app:
         return get
 
     @staticmethod
-    def fix(file):
-        # wrap 'func' to convert its return value to bytes using the specified encoding
-        def wrap(func, encoding):
-            def read(*args, **kwargs):
-                return bytes(func(*args, **kwargs), encoding)
-            return read
-        file.read = wrap(file.read, 'ascii')
-
-
-    @staticmethod
     def parse_post():
         from cgi import FieldStorage
         from cgi import parse_qs
@@ -278,8 +268,7 @@ class app:
             post_env = app.environ.copy()
             post_env["QUERY_STRING"] = ""
             post_env["CONTENT_LENGTH"] = int(app.environ.get("CONTENT_LENGTH", 0))
-            app.fix(post_env['wsgi.input'])
-            input_post=post_env["wsgi.input"].fileno
+            input_post=post_env["wsgi.input"]
             p = FieldStorage(
                 fp=input_post, environ=post_env, keep_blank_values=False
             )

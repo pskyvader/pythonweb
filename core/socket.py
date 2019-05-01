e677380aec75da6eda715cb407314de9f5c511e8
mysitioc <mysitioc@mysitio.cl>
2019-05-01 15:06:32 -0500

    --- Auto-Git Commit ---

--- core/socket.py
+++ core/socket.py
@@ -4,11 +4,16 @@ from websocket import create_connection
 class socket:
     sock = None
     host = "ws://socket.mysitio.cl:8000/ws"
+    
     @staticmethod
     def send(msg):
-        if socket.sock==None:
-            socket.sock = create_connection(socket.host)
-        socket.sock.send(msg)
+        try:
+            if socket.sock==None:
+                socket.sock = create_connection(socket.host)
+            socket.sock.send(msg)
+        except Exception:
+            socket.sock=None
+
     @staticmethod
     def close():
         if socket.sock!=None:

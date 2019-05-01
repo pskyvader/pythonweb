06f09b82d7c3b363b097b1426c95f043ebc9d61c
mysitioc <mysitioc@mysitio.cl>
2019-05-01 15:12:18 -0500

    --- Auto-Git Commit ---

--- core/socket.py
+++ core/socket.py
@@ -4,6 +4,8 @@ from websocket import create_connection
 class socket:
     sock = None
     host = "ws://socket.mysitio.cl:8000/ws"
+    url="http://socket.mysitio.cl"
+    intento=False
 
     @staticmethod
     def send(msg):
@@ -13,6 +15,19 @@ class socket:
             socket.sock.send(msg)
         except Exception:
             socket.sock=None
+            if not socket.intento:
+                socket.intento=True
+                socket.create()
+
+
+    @staticmethod
+    def create():
+        import urllib.request
+        url=socket.url
+        try:
+            response = urllib.request.urlopen(url,timeout:100)
+        except:
+            pass
 
     @staticmethod
     def close():
